40f9c4ef601c4bf364ea2abd3352a25334f2a67d
danilo neves cruz <cruzdanilo@gmail.com>
2023-11-13 20:12:20 -0300

    ✨ webauthn: implement for android

--- webauthn/android/src/main/java/expo/modules/webauthn/ExpoWebauthn.kt
+++ webauthn/android/src/main/java/expo/modules/webauthn/ExpoWebauthn.kt
@@ -1,10 +1,31 @@
 package expo.modules.webauthn
 
+import androidx.credentials.CreatePublicKeyCredentialRequest
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetPasswordOption
+import androidx.credentials.GetPublicKeyCredentialOption
+import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.functions.Coroutine
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ExpoWebauthn : Module() {
   override fun definition() = ModuleDefinition {
     Name("ExpoWebauthn")
+
+    AsyncFunction("create") Coroutine { requestJSON: String ->
+      val activity = appContext.currentActivity ?: throw Exceptions.MissingActivity()
+      CredentialManager.create(activity)
+        .createCredential(activity, CreatePublicKeyCredentialRequest(requestJSON))
+    }
+
+    AsyncFunction("get") Coroutine { requestJSON: String ->
+      val activity = appContext.currentActivity ?: throw Exceptions.MissingActivity()
+      CredentialManager.create(activity).getCredential(
+        activity,
+        GetCredentialRequest(listOf(GetPublicKeyCredentialOption(requestJSON), GetPasswordOption()))
+      )
+    }
   }
 }
