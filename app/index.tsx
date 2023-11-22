2246d58a541533c532a24166f7734bca3831933a
danilo neves cruz <cruzdanilo@gmail.com>
2023-11-22 15:35:41 -0300

    ✨ turnkey: store sub-org and wallet data

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { ApiKeyStamper } from "@turnkey/api-key-stamper";
 import { TurnkeyClient, createActivityPoller, getWebAuthnAttestation } from "@turnkey/http";
+import { WebauthnStamper } from "@turnkey/webauthn-stamper";
 import { deviceName } from "expo-device";
 import React, { useCallback } from "react";
 import { Platform } from "react-native";
@@ -7,35 +8,48 @@ import { Button, Text, XStack, YStack } from "tamagui";
 import { UAParser } from "ua-parser-js";
 import { useBlockNumber } from "wagmi";
 
+import useTurnkeyStore from "../hooks/useTurnkeyStore";
 import base64URLEncode from "../utils/base64URLEncode";
 import generateRandomBuffer from "../utils/generateRandomBuffer";
 import handleError from "../utils/handleError";
 
+if (!process.env.EXPO_PUBLIC_TURNKEY_ORGANIZATION_ID) throw new Error("missing turnkey organization id");
+if (!process.env.EXPO_PUBLIC_TURNKEY_API_PUBLIC_KEY) throw new Error("missing turnkey api public key");
+if (!process.env.EXPO_PUBLIC_TURNKEY_API_PRIVATE_KEY) throw new Error("missing turnkey api private key");
+
 const rpId = __DEV__ && Platform.OS === "web" ? "localhost" : "exactly.app";
+const apiPublicKey = process.env.EXPO_PUBLIC_TURNKEY_API_PUBLIC_KEY;
+const apiPrivateKey = process.env.EXPO_PUBLIC_TURNKEY_API_PRIVATE_KEY;
+const organizationId = process.env.EXPO_PUBLIC_TURNKEY_ORGANIZATION_ID;
 
 export default function Home() {
   const { data: blockNumber } = useBlockNumber();
+  const { connect: connectTurnkey } = useTurnkeyStore();
 
   const create = useCallback(() => {
+    const name = `exactly, ${new Date().toISOString()}`;
     const challenge = generateRandomBuffer();
     getWebAuthnAttestation({
       publicKey: {
         rp: { id: rpId, name: "exactly" },
-        user: { id: challenge, name: "exactly account", displayName: "exactly account" },
+        user: { id: challenge, name, displayName: name },
         pubKeyCredParams: [{ alg: -7, type: "public-key" }],
         authenticatorSelection: { requireResidentKey: true, residentKey: "required", userVerification: "required" },
         challenge,
       },
     })
       .then(async (attestation) => {
-        console.log("attestation", attestation); // eslint-disable-line no-console
-        if (!process.env.EXPO_PUBLIC_TURNKEY_ORGANIZATION_ID) throw new Error("missing turnkey organization id");
-        const client = turnkeyClient();
+        const client = new TurnkeyClient(
+          { baseUrl: "https://api.turnkey.com" },
+          new ApiKeyStamper({ apiPublicKey, apiPrivateKey }),
+        );
         const activityPoller = createActivityPoller({ client, requestFn: client.createSubOrganization });
-        const completedActivity = await activityPoller({
+        const {
+          result: { createSubOrganizationResultV4 },
+        } = await activityPoller({
           type: "ACTIVITY_TYPE_CREATE_SUB_ORGANIZATION_V4",
           timestampMs: String(Date.now()),
-          organizationId: process.env.EXPO_PUBLIC_TURNKEY_ORGANIZATION_ID,
+          organizationId,
           parameters: {
             subOrganizationName: attestation.credentialId,
             rootQuorumThreshold: 1,
@@ -65,40 +79,42 @@ export default function Home() {
             },
           },
         });
-        console.log(completedActivity); // eslint-disable-line no-console
+        if (!createSubOrganizationResultV4?.wallet?.addresses[0]) throw new Error("sub-org creation failed");
+        connectTurnkey(
+          createSubOrganizationResultV4.subOrganizationId,
+          createSubOrganizationResultV4.wallet.addresses[0],
+        );
       })
       .catch(handleError);
-  }, []);
+  }, [connectTurnkey]);
 
-  const get = useCallback(() => {
-    navigator.credentials
-      .get({ publicKey: { rpId, userVerification: "required", challenge: generateRandomBuffer() } })
-      .then((credential) => {
-        console.log("credential", credential); // eslint-disable-line no-console
+  const connect = useCallback(() => {
+    new TurnkeyClient({ baseUrl: "https://api.turnkey.com" }, new WebauthnStamper({ rpId }))
+      .getWhoami({ organizationId })
+      .then(async ({ organizationId: subOrgId }) => {
+        const client = new TurnkeyClient(
+          { baseUrl: "https://api.turnkey.com" },
+          new ApiKeyStamper({ apiPublicKey, apiPrivateKey }),
+        );
+        const {
+          wallets: [wallet],
+        } = await client.getWallets({ organizationId: subOrgId });
+        if (!wallet) throw new Error("no wallet");
+        const { accounts } = await client.getWalletAccounts({ organizationId: subOrgId, walletId: wallet.walletId });
+        const account = accounts.find(({ curve }) => curve === "CURVE_SECP256K1");
+        if (!account) throw new Error("no ethereum account");
+        connectTurnkey(subOrgId, account.address);
       })
       .catch(handleError);
-  }, []);
+  }, [connectTurnkey]);
 
   return (
     <XStack flex={1} alignItems="center" space>
       <YStack flex={1} alignItems="center" space>
         <Text textAlign="center">block number: {blockNumber && String(blockNumber)}</Text>
         <Button onPress={create}>create</Button>
-        <Button onPress={get}>get</Button>
+        <Button onPress={connect}>connect</Button>
       </YStack>
     </XStack>
-  );
-}
-
-function turnkeyClient() {
-  if (!process.env.EXPO_PUBLIC_TURNKEY_API_PUBLIC_KEY || !process.env.EXPO_PUBLIC_TURNKEY_API_PRIVATE_KEY) {
-    throw new Error("missing turnkey api keys");
-  }
-  return new TurnkeyClient(
-    { baseUrl: "https://api.turnkey.com" },
-    new ApiKeyStamper({
-      apiPublicKey: process.env.EXPO_PUBLIC_TURNKEY_API_PUBLIC_KEY,
-      apiPrivateKey: process.env.EXPO_PUBLIC_TURNKEY_API_PRIVATE_KEY,
-    }),
   );
 }
