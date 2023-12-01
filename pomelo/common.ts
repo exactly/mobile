fd448b1b460e7547dcb83627bd7fc259e267c9e5
sebipap <sebipaps@gmail.com>
2023-12-01 10:13:27 -0300

    ➖ axios

--- pomelo/common.ts
+++ pomelo/common.ts
@@ -1,24 +1,21 @@
-import axios, { isAxiosError } from "axios";
-
 import type { AccessTokenResponse, ErrorPayload } from "./types";
 import { pomeloAudience, pomeloBaseUrl, pomeloClientId, pomeloClientSecret } from "../utils/constants";
 
-const instance = axios.create({
-  baseURL: pomeloBaseUrl,
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-});
-
 async function accessToken() {
-  const {
-    data: { access_token },
-  } = await instance.post<AccessTokenResponse>("/oauth/token", {
-    client_id: pomeloClientId,
-    client_secret: pomeloClientSecret,
-    audience: pomeloAudience,
-    grant_type: "client_credentials",
+  const response = await fetch(`${pomeloBaseUrl}/oauth/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      client_id: pomeloClientId,
+      client_secret: pomeloClientSecret,
+      audience: pomeloAudience,
+      grant_type: "client_credentials",
+    }),
   });
+  if (!response.ok) throw new Error(`Error Fetching Access Token: ${response.status}`);
+  const { access_token } = (await response.json()) as AccessTokenResponse;
   return access_token;
 }
 
@@ -28,37 +25,38 @@ export default async function request<Response>(
   body?: object,
   headers?: Record<string, string>,
 ) {
-  try {
-    const { data } = await instance<Response>({
-      method,
-      url,
-      data: body,
-      headers: { ...headers, Authorization: `Bearer ${await accessToken()}` },
-    });
-    return data;
-  } catch (error) {
-    if (isAxiosError(error)) {
-      switch (error.response?.status) {
-        case 400: {
-          const {
-            error: { details },
-          } = error.response.data as ErrorPayload;
-          throw new Error(`Invalid Request: ${details.map(({ detail }) => detail).join(", ")}`);
-        }
-        case 401: {
-          throw new Error("Unauthorized");
-        }
-        case 403: {
-          throw new Error("Forbidden");
-        }
-        case 404: {
-          const {
-            error: { details },
-          } = error.response.data as ErrorPayload;
-          throw new Error(`Not Found: ${details.map(({ detail }) => detail).join(", ")}`);
-        }
-      }
+  const response = await fetch(pomeloBaseUrl + url, {
+    method,
+    headers: {
+      ...headers,
+      "Content-Type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${await accessToken()}`,
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (response.ok) return response.json() as Response;
+
+  const {
+    error: { details },
+  } = (await response.json()) as ErrorPayload;
+
+  const detailsText = details.map(({ detail }) => detail).join(", ");
+  switch (response.status) {
+    case 400: {
+      throw new Error(`Invalid Request: ${detailsText}`);
     }
-    throw new Error("Unexpected Error"); // TODO: report to Sentry
+    case 401: {
+      throw new Error("Unauthorized");
+    }
+    case 403: {
+      throw new Error("Forbidden");
+    }
+    case 404: {
+      throw new Error(`Not Found: ${detailsText}`);
+    }
+    default: {
+      throw new Error(`Unexpected Error: ${response.status}`); // TODO: report to Sentry;
+    }
   }
 }
