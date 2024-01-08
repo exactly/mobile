6b52ad9380f2875f5c99f7a99666d3de32895b24
sebipap <sebipaps@gmail.com>
2024-01-08 17:07:55 -0300

    🍱 add exactly assets and set up project to parse SVGs

--- assets/types.d.ts
+++ assets/types.d.ts
@@ -2,3 +2,9 @@ declare module "*.otf" {
   const value: FontSource;
   export default value;
 }
+declare module "*.svg" {
+  import type React from "react";
+  import type { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
