3b26bf96cb0d3e5ad28b785c8f2c9db8428754c4
danilo neves cruz <cruzdanilo@gmail.com>
2024-01-15 18:06:18 -0300

    🏷️ metro: annotate config type

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+/** @type {import('metro-config').InputConfigT} */
 module.exports = {
   ...defaultConfig,
   resolver: {
