0f56866f3e610d9a6ebe59d9091feb913c57db21
danilo neves cruz <cruzdanilo@gmail.com>
2024-01-16 22:38:43 -0300

    ♻️ app: use wagmi generated hooks

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,18 @@
-import Auditor from "@exactly/protocol/deployments/goerli/Auditor.json";
-import MarketUSDC from "@exactly/protocol/deployments/goerli/MarketUSDC.json";
-import MarketWETH from "@exactly/protocol/deployments/goerli/MarketWETH.json";
 import { ApiKeyStamper } from "@turnkey/api-key-stamper";
 import { TurnkeyClient, createActivityPoller, getWebAuthnAttestation } from "@turnkey/http";
 import { deviceName } from "expo-device";
 import React, { useCallback } from "react";
 import { Button, Spinner, Text, XStack, YStack } from "tamagui";
 import { UAParser } from "ua-parser-js";
-import { formatEther, getAddress, parseUnits, zeroAddress } from "viem";
-import {
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useReadContract,
-  useSimulateContract,
-  useWaitForTransactionReceipt,
-  useWriteContract,
-} from "wagmi";
+import { formatEther, parseUnits, zeroAddress } from "viem";
+import { useAccount, useConnect, useDisconnect, useWaitForTransactionReceipt, useWriteContract } from "wagmi";
 
+import {
+  marketWethAddress,
+  useReadAuditorAccountLiquidity,
+  useSimulateAuditorEnterMarket,
+  useSimulateMarketUsdcBorrow,
+} from "../common/wagmi";
 import base64URLEncode from "../utils/base64URLEncode";
 import { rpId, turnkeyAPIPrivateKey, turnkeyAPIPublicKey, turnkeyOrganizationId } from "../utils/constants";
 import generateRandomBuffer from "../utils/generateRandomBuffer";
@@ -31,40 +26,16 @@ export default function Home() {
   } = useConnect();
   const { address } = useAccount();
   const { disconnect } = useDisconnect();
-  const { data: enterWETHSimulation } = useSimulateContract({
-    abi: Auditor.abi,
-    address: getAddress(Auditor.address),
-    functionName: "enterMarket",
-    args: [MarketWETH.address],
+  const { data: enterWETHSimulation } = useSimulateAuditorEnterMarket({
+    args: [marketWethAddress],
     query: { enabled: !!address },
   });
-  const { data: borrowUSDCSimulation } = useSimulateContract({
-    abi: MarketUSDC.abi,
-    address: getAddress(MarketUSDC.address),
-    functionName: "borrow",
-    args: [parseUnits("1", 6), address, address],
+  const { data: borrowUSDCSimulation } = useSimulateMarketUsdcBorrow({
+    args: address && [parseUnits("1", 6), address, address],
     query: { enabled: !!address },
   });
-  const { data: accountLiquidity } = useReadContract({
-    abi: [
-      {
-        name: "accountLiquidity",
-        type: "function",
-        stateMutability: "view",
-        inputs: [
-          { internalType: "address", name: "account", type: "address" },
-          { internalType: "contract Market", name: "marketToSimulate", type: "address" },
-          { internalType: "uint256", name: "withdrawAmount", type: "uint256" },
-        ],
-        outputs: [
-          { internalType: "uint256", name: "sumCollateral", type: "uint256" },
-          { internalType: "uint256", name: "sumDebtPlusEffects", type: "uint256" },
-        ],
-      },
-    ],
-    address: getAddress(Auditor.address),
-    functionName: "accountLiquidity",
-    args: [address ?? zeroAddress, zeroAddress, 0n],
+  const { data: accountLiquidity } = useReadAuditorAccountLiquidity({
+    args: address && [address, zeroAddress, 0n],
     query: { enabled: !!address },
   });
   const { writeContract, data: txHash, isPending: isSending } = useWriteContract();
