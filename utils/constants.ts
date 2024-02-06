30a8b38cb0ed3d398c00f72e3624e3b3ea265119
sebipap <sebipaps@gmail.com>
2024-02-06 15:19:29 -0300

    🛂 auth: implement passkey authentication

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,9 +4,11 @@ export const rpId = __DEV__ && Platform.OS === "web" ? "localhost" : "exactly.ap
 
 if (!process.env.EXPO_PUBLIC_ALCHEMY_API_KEY) throw new Error("missing alchemy api key");
 if (!process.env.EXPO_PUBLIC_ALCHEMY_GAS_POLICY_ID) throw new Error("missing alchemy gas policy");
+if (!process.env.EXPO_PUBLIC_SERVER_URL) throw new Error("missing server url");
 
 export const alchemyAPIKey = process.env.EXPO_PUBLIC_ALCHEMY_API_KEY;
 export const alchemyGasPolicyId = process.env.EXPO_PUBLIC_ALCHEMY_GAS_POLICY_ID;
 export const oneSignalAppId = process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID;
+export const serverURL = process.env.EXPO_PUBLIC_SERVER_URL;
 
 export { optimismSepolia as chain } from "@alchemy/aa-core";
