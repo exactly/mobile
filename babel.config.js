b8b0fcf2cc43b1742d9f6d8d54ec9b11545f8c47
danilo neves cruz <cruzdanilo@gmail.com>
2024-06-18 00:53:05 -0300

    🔧 babel: remove legacy plugins

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,6 @@ module.exports = function config(api) {
   /** @type {(ever: boolean) => void} */ (/** @type {unknown} */ (api.cache))(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "transform-inline-environment-variables",
-      ["@babel/plugin-transform-private-methods", { loose: true }],
-      ["@tamagui/babel-plugin", { components: ["tamagui"], config: "tamagui.config.ts", logTimings: true }],
-    ],
+    plugins: [["@tamagui/babel-plugin", { components: ["tamagui"], config: "tamagui.config.ts", logTimings: true }]],
   };
 };
