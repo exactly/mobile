1c107d38afc3e3d304d06ff9550db8e2cf40822b
danilo neves cruz <cruzdanilo@gmail.com>
2024-06-20 16:36:22 -0300

    ⚰️ app: remove legacy constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,3 @@
-import { Platform } from "react-native";
-
-export const rpId = __DEV__ && Platform.OS === "web" ? "localhost" : "exactly.app";
-
 if (!process.env.EXPO_PUBLIC_ALCHEMY_API_KEY) throw new Error("missing alchemy api key");
 if (!process.env.EXPO_PUBLIC_ALCHEMY_GAS_POLICY_ID) throw new Error("missing alchemy gas policy");
 if (!process.env.EXPO_PUBLIC_SERVER_URL) throw new Error("missing server url");
@@ -10,5 +6,3 @@ export const alchemyAPIKey = process.env.EXPO_PUBLIC_ALCHEMY_API_KEY;
 export const alchemyGasPolicyId = process.env.EXPO_PUBLIC_ALCHEMY_GAS_POLICY_ID;
 export const oneSignalAppId = process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID;
 export const serverURL = process.env.EXPO_PUBLIC_SERVER_URL;
-
-export { optimismSepolia as chain } from "@alchemy/aa-core";
