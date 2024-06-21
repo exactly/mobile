a37491614feda00af9b5dc0b4efff8c3cc90220b
danilo neves cruz <cruzdanilo@gmail.com>
2024-06-20 21:06:40 -0300

    🔧 app: set default server url

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,7 @@
 if (!process.env.EXPO_PUBLIC_ALCHEMY_API_KEY) throw new Error("missing alchemy api key");
 if (!process.env.EXPO_PUBLIC_ALCHEMY_GAS_POLICY_ID) throw new Error("missing alchemy gas policy");
-if (!process.env.EXPO_PUBLIC_SERVER_URL) throw new Error("missing server url");
 
+export const serverURL = process.env.EXPO_PUBLIC_SERVER_URL ?? "https://server.exactly.app";
 export const alchemyAPIKey = process.env.EXPO_PUBLIC_ALCHEMY_API_KEY;
 export const alchemyGasPolicyId = process.env.EXPO_PUBLIC_ALCHEMY_GAS_POLICY_ID;
 export const oneSignalAppId = process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID;
-export const serverURL = process.env.EXPO_PUBLIC_SERVER_URL;
