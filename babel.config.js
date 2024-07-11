dfaa356fb034fd5e0021bd4500ade8a1e4b1d9c9
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-11 20:15:20 -0300

    🔧 babel: setup import attributes

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,9 @@ module.exports = function config(api) {
   /** @type {(ever: boolean) => void} */ (/** @type {unknown} */ (api.cache))(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["@tamagui/babel-plugin", { components: ["tamagui"], config: "tamagui.config.ts", logTimings: true }]],
+    plugins: [
+      "@babel/plugin-syntax-import-attributes",
+      ["@tamagui/babel-plugin", { components: ["tamagui"], config: "tamagui.config.ts", logTimings: true }],
+    ],
   };
 };
