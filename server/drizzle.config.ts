842deeda4f0053ca591d2dbd5174f5ad23c9e046
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-11 21:24:50 -0300

    🔧 server: use drizzle url from vercel env file

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,9 +1,14 @@
+import { parse } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+import { readFileSync } from "node:fs";
+import path from "node:path";
 
 export default defineConfig({
   schema: "database/schema.ts",
   dialect: "postgresql",
-  dbCredentials: { url: process.env.POSTGRES_URL ?? "" },
+  dbCredentials: {
+    url: parse(readFileSync(path.resolve(__dirname, "../.vercel/.env.development.local"))).POSTGRES_URL ?? "",
+  },
   verbose: true,
   strict: true,
 });
