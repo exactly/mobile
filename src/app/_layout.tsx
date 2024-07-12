798e82c457577b6341e29283593d1496a741d923
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-12 18:41:52 -0300

    ✨ app: add new root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,16 @@
 import "../utils/polyfill";
 
 import { createAlchemyPublicRpcClient } from "@alchemy/aa-alchemy";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { ReactNativeTracing, ReactNavigationInstrumentation, init, wrap } from "@sentry/react-native";
-import InterBold from "@tamagui/font-inter/otf/Inter-Bold.otf"; // eslint-disable-line import/no-unresolved
-import Inter from "@tamagui/font-inter/otf/Inter-Medium.otf"; // eslint-disable-line import/no-unresolved
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { isRunningInExpoGo } from "expo";
 import { type FontSource, useFonts } from "expo-font";
 import { Slot, SplashScreen, useNavigationContainerRef } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect } from "react";
+import { useColorScheme } from "react-native";
 import { TamaguiProvider } from "tamagui";
 import { WagmiProvider, createConfig, createStorage, custom } from "wagmi";
 
@@ -19,6 +18,11 @@ import { alchemyAPIKey, chain } from "@exactly/common/constants";
 
 import metadata from "../../package.json";
 import tamaguiConfig from "../../tamagui.config";
+import BDOGroteskBold from "../assets/fonts/BDOGrotesk-Bold.otf";
+import BDOGroteskRegular from "../assets/fonts/BDOGrotesk-Regular.otf";
+import IBMPlexMonoBold from "../assets/fonts/IBMPlexMono-Bold.otf";
+import IBMPlexMonoRegular from "../assets/fonts/IBMPlexMono-Regular.otf";
+import IBMPlexMonoSemiBold from "../assets/fonts/IBMPlexMono-SemiBold.otf";
 import alchemyConnector from "../utils/alchemyConnector";
 import handleError from "../utils/handleError";
 import useOneSignal from "../utils/useOneSignal";
@@ -52,10 +56,14 @@ const queryClient = new QueryClient();
 
 export default wrap(function RootLayout() {
   const navigationContainer = useNavigationContainerRef();
+  const colorScheme = useColorScheme();
   const [loaded, error] = useFonts({
-    Inter: Inter as FontSource,
-    InterBold: InterBold as FontSource,
-    ...FontAwesome.font,
+    BDOGroteskBold: BDOGroteskBold as FontSource,
+    BDOGroteskRegular: BDOGroteskRegular as FontSource,
+    IBMPlexMonoBold: IBMPlexMonoBold as FontSource,
+    IBMPlexMonoRegular: IBMPlexMonoRegular as FontSource,
+    IBMPlexMonoSemiBold: IBMPlexMonoSemiBold as FontSource,
+    unset: BDOGroteskRegular as FontSource,
   });
 
   useOneSignal();
@@ -78,11 +86,13 @@ export default wrap(function RootLayout() {
     <>
       <StatusBar translucent={false} />
       <TamaguiProvider config={tamaguiConfig}>
-        <WagmiProvider config={wagmiConfig}>
-          <QueryClientProvider client={queryClient}>
-            <Slot />
-          </QueryClientProvider>
-        </WagmiProvider>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <WagmiProvider config={wagmiConfig}>
+            <QueryClientProvider client={queryClient}>
+              <Slot />
+            </QueryClientProvider>
+          </WagmiProvider>
+        </ThemeProvider>
       </TamaguiProvider>
     </>
   );
