5cbc67e084d2ceb82e3e9db669629f55a6537f98
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-16 12:10:56 -0300

    🔧 app: add more tamagui components to eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,10 @@ module.exports = {
     "no-console": "warn",
     "no-restricted-imports": ["error", { patterns: ["./server/"] }],
     "no-shadow": "off", // @typescript-eslint/no-shadow
-    "react-native/no-raw-text": ["error", { skip: ["Button", "Heading", "SizableText"] }],
+    "react-native/no-raw-text": [
+      "error",
+      { skip: ["Button", "Link", "SubmitButton", "LinkButton", "Heading", "SizableText"] },
+    ],
     "unicorn/filename-case": "off", // use default export name
     "unicorn/number-literal-case": "off", // incompatible with prettier
     "unicorn/prefer-top-level-await": "off", // unsupported in react-native
