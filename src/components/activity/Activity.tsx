7065b8853f8d92bb5897c83bdf314d66313d9e43
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-17 17:35:50 -0300

    ⚗️ app: add start onboarding button for testing

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -1,16 +1,36 @@
+import { router } from "expo-router";
 import React from "react";
-import { Text } from "tamagui";
+import { ms } from "react-native-size-matters";
+import { Button, Text, View } from "tamagui";
 
 import BaseLayout from "../shared/BaseLayout";
 import SafeView from "../shared/SafeView";
 
+const pressStyle = { backgroundColor: "$interactiveBaseBrandDefault", opacity: 0.9 };
+
+const startOnboarding = () => {
+  router.push("onboarding");
+};
+
 const Activity = () => {
   return (
     <SafeView>
-      <BaseLayout>
-        <Text fontSize={40} fontFamily="$mono" fontWeight={700}>
-          Activity
-        </Text>
+      <BaseLayout flex={1}>
+        <View gap={ms(40)}>
+          <Text fontSize={40} fontFamily="$mono" fontWeight={700}>
+            Activity
+          </Text>
+          <Button
+            variant="outlined"
+            backgroundColor="$interactiveBaseBrandDefault"
+            color="$textInteractiveBaseBrandDefault"
+            onPress={startOnboarding}
+            fontWeight={600}
+            pressStyle={pressStyle}
+          >
+            Start Onboarding
+          </Button>
+        </View>
       </BaseLayout>
     </SafeView>
   );
