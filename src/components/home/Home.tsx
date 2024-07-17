1d49491616aeb96242e23bf7148d018429833845
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-17 13:00:13 -0300

    🔥 app: remove start onboarding button

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,15 +1,10 @@
-import { router } from "expo-router";
 import React from "react";
-import { Button, View } from "tamagui";
+import { View } from "tamagui";
 
 import Balance from "./Balance";
 import BaseLayout from "../shared/BaseLayout";
 import ProfileHeader from "../shared/ProfileHeader";
 import SafeView from "../shared/SafeView";
-
-const startOnboarding = () => {
-  router.push("onboarding");
-};
 
 const Home = () => {
   return (
@@ -18,15 +13,6 @@ const Home = () => {
         <View gap={40}>
           <ProfileHeader />
           <Balance />
-          <Button
-            variant="outlined"
-            backgroundColor="$interactiveBaseBrandDefault"
-            color="$textInteractiveBaseBrandDefault"
-            onPress={startOnboarding}
-            fontWeight={600}
-          >
-            Start Onboarding
-          </Button>
         </View>
       </BaseLayout>
     </SafeView>
