a1941ba455b78a699f7f68e1900561d128239ccc
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-18 15:08:30 -0300

    💫 app: create animated spinner

---
+++ src/components/shared/Spinner.tsx
@@ -0,0 +1,28 @@
+import { SpinnerGap } from "phosphor-react-native";
+import React from "react";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import { ms } from "react-native-size-matters";
+import { View } from "tamagui";
+
+const AnimatedSpinner = Animated.createAnimatedComponent(SpinnerGap);
+
+export default function Spinner() {
+  const rotation = useSharedValue(0);
+  const rStyle = useAnimatedStyle(() => {
+    rotation.value += 5;
+    const rotationValue = `${(rotation.value % 360).toString()}deg`;
+    return { transform: [{ rotate: rotationValue }] };
+  });
+  return (
+    <View
+      backgroundColor="$backgroundMild"
+      width={ms(88)}
+      height={ms(88)}
+      justifyContent="center"
+      alignItems="center"
+      borderRadius={100}
+    >
+      <AnimatedSpinner style={rStyle} />
+    </View>
+  );
+}
