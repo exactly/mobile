081e9d2a8c03a33e52853d5109c1afb6bc71a2a2
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-19 11:17:18 -0300

    ✨ app: create spending limits button

---
+++ src/components/card/SpendingLimitButton.tsx
@@ -0,0 +1,61 @@
+import { PencilLine } from "phosphor-react-native";
+import React from "react";
+import { ms } from "react-native-size-matters";
+import { View, Text, useTheme } from "tamagui";
+
+interface SpendingLimitButtonProperties {
+  title: string;
+  amount: number;
+  limit: number;
+  currency?: string;
+}
+
+const SpendingLimitButton = ({ title, amount, limit, currency }: SpendingLimitButtonProperties) => {
+  const theme = useTheme();
+  return (
+    <View
+      flexDirection="row"
+      flex={1}
+      backgroundColor="$backgroundBrandSoft"
+      width="100%"
+      borderRadius={10}
+      height={ms(46)}
+      alignItems="center"
+    >
+      <View
+        position="absolute"
+        width="70%"
+        height={ms(46)}
+        backgroundColor="$interactiveBaseBrandDefault"
+        borderRadius={10}
+        borderTopRightRadius={0}
+        borderBottomRightRadius={0}
+        opacity={0.1}
+      />
+
+      <View flexDirection="row" justifyContent="space-between" flex={1}>
+        <View flexDirection="row" gap={ms(5)} alignItems="center" padding={ms(10)}>
+          <Text color="$uiprimary" fontSize={ms(15)}>
+            {title}
+          </Text>
+          <PencilLine size={ms(15)} color={theme.iconPrimary.get() as string} />
+        </View>
+        <View flexDirection="row" justifyContent="space-around" alignItems="center" gap={ms(5)}>
+          <Text color="$uiBrandSecondary" fontSize={ms(15)} fontWeight="bold">
+            {currency}
+            {amount}
+          </Text>
+          <Text color="$uiBrandSecondary" fontSize={ms(15)}>
+            /
+          </Text>
+          <Text color="$uiBrandSecondary" fontSize={ms(15)}>
+            {currency}
+            {limit}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default SpendingLimitButton;
