53c4a87be4ce57801aaffa95d514d20bc6c1c9ba
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-20 17:03:20 -0300

    🎨 app: update wagmi config in home screen

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,20 +1,48 @@
+import { readContract, getAccount } from "@wagmi/core";
 import { CaretRight } from "phosphor-react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { ms } from "react-native-size-matters";
 import { ScrollView, Text, View, useTheme } from "tamagui";
+import { zeroAddress } from "viem";
 
-import Balance from "./Balance.js";
-import HomeActions from "./HomeActions.js";
-import LatestActivity from "./LatestActivity.js";
-import AlertBadge from "../shared/AlertBadge.js";
-import BaseLayout from "../shared/BaseLayout.js";
-import InfoPreview from "../shared/InfoPreview.js";
-import ProfileHeader from "../shared/ProfileHeader.js";
-import SafeView from "../shared/SafeView.js";
+import Balance from "./Balance";
+import HomeActions from "./HomeActions";
+import LatestActivity from "./LatestActivity";
+import type { PreviewerData } from "../../stores/usePreviewerStore";
+import usePreviewerStore from "../../stores/usePreviewerStore";
+import handleError from "../../utils/handleError";
+import wagmiConfig from "../../utils/wagmi";
+import AlertBadge from "../shared/AlertBadge";
+import BaseLayout from "../shared/BaseLayout";
+import InfoPreview from "../shared/InfoPreview";
+import ProfileHeader from "../shared/ProfileHeader";
+import SafeView from "../shared/SafeView";
+import { previewerAbi, previewerAddress } from "../../generated/contracts";
 
 export default function Home() {
   const theme = useTheme();
+  const { data, setPreviewerData } = usePreviewerStore();
+
+  useEffect(() => {
+    const fetchPreviewer = async () => {
+      try {
+        const previewerData: PreviewerData = await readContract(wagmiConfig, {
+          address: previewerAddress,
+          abi: previewerAbi,
+          functionName: "exactly",
+          args: [getAccount(wagmiConfig).address || zeroAddress],
+        });
+
+        setPreviewerData(previewerData);
+      } catch (error) {
+        handleError(error);
+      }
+    };
+
+    fetchPreviewer().catch(handleError);
+  }, [setPreviewerData]);
+
   return (
     <SafeView paddingBottom={0}>
       <BaseLayout>
