75cdfb82377636bb92934fd6554b94234b3927e8
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-22 14:14:39 -0300

    💄 app: improve tab bar style

--- src/components/shared/TabBar.tsx
+++ src/components/shared/TabBar.tsx
@@ -3,56 +3,60 @@ import React from "react";
 import { ms, vs } from "react-native-size-matters";
 import { View, Text, ButtonIcon, useTheme } from "tamagui";
 
+import SafeView from "./SafeView.js";
 import StyledPressable from "./StyledPressable.js";
 
 export default function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
   const theme = useTheme();
   return (
-    <View
-      display="flex"
-      flexDirection="row"
-      alignItems="center"
-      justifyContent="center"
-      height={vs(83)}
-      backgroundColor="$backgroundSoft"
-      borderColor="$borderNeutralSoft"
-      borderTopWidth={0.5}
-    >
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key] || { options: undefined };
-        if (!options) throw new Error("No navigation button options found");
+    <SafeView flex={0} paddingTop={0}>
+      <View
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-around"
+        height={vs(63)}
+        backgroundColor="$backgroundSoft"
+        borderColor="$borderNeutralSoft"
+        borderTopWidth={0.5}
+        gap={ms(10)}
+      >
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key] || { options: undefined };
+          if (!options) throw new Error("No navigation button options found");
 
-        const label = options.title;
-        const icon = options.tabBarIcon;
-        const isFocused = state.index === index;
+          const label = options.title;
+          const icon = options.tabBarIcon;
+          const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: "tabPress",
-            target: route.key,
-            canPreventDefault: true,
-          });
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params);
-          }
-        };
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
+            }
+          };
 
-        return (
-          <StyledPressable key={route.key} role="button" onPress={onPress} flex={1} padding={ms(10)}>
-            <View display="flex" flexDirection="column" gap={ms(2)} alignContent="center" alignItems="center" flex={1}>
-              <ButtonIcon>
-                {icon?.({
-                  focused: isFocused,
-                  color: isFocused ? (theme.textBrandPrimary.val as string) : (theme.textSecondary.val as string),
-                  size: vs(24),
-                })}
-              </ButtonIcon>
-              <Text color={isFocused ? theme.textBrandPrimary : theme.textSecondary}>{label}</Text>
-            </View>
-          </StyledPressable>
-        );
-      })}
-    </View>
+          return (
+            <StyledPressable key={route.key} role="button" onPress={onPress} flex={1} padding={ms(10)}>
+              <View display="flex" flexDirection="column" gap={ms(2)} alignContent="center" alignItems="center">
+                <ButtonIcon>
+                  {icon?.({
+                    focused: isFocused,
+                    color: isFocused ? (theme.textBrandPrimary.val as string) : (theme.textSecondary.val as string),
+                    size: vs(24),
+                  })}
+                </ButtonIcon>
+                <Text color={isFocused ? theme.textBrandPrimary : theme.textSecondary}>{label}</Text>
+              </View>
+            </StyledPressable>
+          );
+        })}
+      </View>
+    </SafeView>
   );
 }
