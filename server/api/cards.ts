c19f455b017c1bf4ab1129429167348463188ae8
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-23 17:54:43 -0300

    ✨ server: implement card creation endpoint

--- server/api/cards.ts
+++ server/api/cards.ts
@@ -1,5 +1,7 @@
+import { CreateCardParameters } from "@exactly/common/types.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { eq } from "drizzle-orm";
+import { parse } from "valibot";
 
 import database, { cards } from "../database/index.js";
 import auth from "../middleware/auth.js";
@@ -7,7 +9,7 @@ import cors from "../middleware/cors.js";
 import { createCard } from "../utils/cryptomate.js";
 
 export default cors(
-  auth(async function handler({ method }: VercelRequest, response: VercelResponse, credentialId: string) {
+  auth(async function handler({ method, body }: VercelRequest, response: VercelResponse, credentialId: string) {
     switch (method) {
       case "GET":
         return response
@@ -20,7 +22,7 @@ export default cors(
           .end();
       case "POST":
         try {
-          const card = await createCard("Satoshi Nakamoto");
+          const card = await createCard(parse(CreateCardParameters, body));
           return response.json(card);
         } catch (error) {
           return response.status(500).end(error instanceof Error ? error.message : error);
--- server/utils/cryptomate.ts
+++ server/utils/cryptomate.ts
@@ -1,13 +1,30 @@
+import type { CreateCardParameters } from "@exactly/common/types.js";
+import * as v from "valibot";
+
 if (!process.env.CRYPTOMATE_URL || !process.env.CRYPTOMATE_API_KEY) throw new Error("missing cryptomate vars");
 const baseURL = process.env.CRYPTOMATE_URL;
 const apiKey = process.env.CRYPTOMATE_API_KEY;
 
 // eslint-disable-next-line import/prefer-default-export
-export function createCard(name: string) {
-  return request("/card", { card_holder_name: name, approval_method: "WEBHOOK" });
+export function createCard({ cardholder, email, phone, limits }: CreateCardParameters) {
+  return request(
+    CreateCardResponse,
+    "/cards/virtual-cards/create",
+    v.parse(CreateCardRequest, {
+      card_holder_name: cardholder,
+      email,
+      phone_country_code: phone.countryCode,
+      phone: phone.number,
+      approval_method: "WEBHOOK",
+      daily_limit: limits.daily,
+      weekly_limit: limits.weekly,
+      monthly_limit: limits.monthly,
+    }),
+  );
 }
 
-async function request<T = unknown>(
+async function request<TInput, TOutput, TIssue extends v.BaseIssue<unknown>>(
+  schema: v.BaseSchema<TInput, TOutput, TIssue>,
   url: `/${string}`,
   body?: unknown,
   method: "GET" | "POST" = body === undefined ? "GET" : "POST",
@@ -18,5 +35,27 @@ async function request<T = unknown>(
     body: body ? JSON.stringify(body) : undefined,
   });
   if (!response.ok) throw new Error(`${String(response.status)} ${await response.text()}`);
-  return response.json() as Promise<T>;
+  return v.parse(schema, await response.json());
 }
+
+const CreateCardRequest = v.object({
+  card_holder_name: v.string(),
+  email: v.pipe(v.string(), v.email()),
+  phone_country_code: v.pipe(v.string(), v.regex(/^\d{2}$/)),
+  phone: v.pipe(v.string(), v.regex(/^\d+$/)),
+  approval_method: v.literal("WEBHOOK"),
+  daily_limit: v.number(),
+  weekly_limit: v.number(),
+  monthly_limit: v.number(),
+});
+
+const CreateCardResponse = v.object({
+  id: v.string(),
+  card_holder_name: v.string(),
+  type: v.literal("Virtual"),
+  last4: v.pipe(v.string(), v.regex(/^\d{4}$/)),
+  status: v.literal("ACTIVE"),
+  daily_limit: v.number(),
+  weekly_limit: v.number(),
+  monthly_limit: v.number(),
+});
