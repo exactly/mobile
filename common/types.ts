eeca171353abd013bfa3675991e437ba72c2d0e0
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-23 17:49:21 -0300

    🏷️ common: add type for card creation parameters

--- common/types.ts
+++ common/types.ts
@@ -1,4 +1,4 @@
-import { type InferOutput, custom, object, pipe, regex, string } from "valibot";
+import { type InferOutput, custom, email, number, object, pipe, regex, string } from "valibot";
 import { type Hash as ViemHash, isHash } from "viem";
 
 export const Base64URL = pipe(string(), regex(/^[\w-]+$/));
@@ -7,8 +7,17 @@ export const Hash = custom<ViemHash>(isHash as (hash: unknown) => hash is ViemHa
 
 export const Passkey = object({ credentialId: Base64URL, x: Hash, y: Hash });
 
+export const CreateCardParameters = object({
+  cardholder: string(),
+  email: pipe(string(), email()),
+  phone: object({ countryCode: pipe(string(), regex(/^\d{2}$/)), number: pipe(string(), regex(/^\d+$/)) }),
+  limits: object({ daily: number(), weekly: number(), monthly: number() }),
+});
+
 /* eslint-disable @typescript-eslint/no-redeclare */
 export type Base64URL = InferOutput<typeof Base64URL>;
 export type Hash = InferOutput<typeof Hash>;
 export type Passkey = InferOutput<typeof Passkey>;
+
+export type CreateCardParameters = InferOutput<typeof CreateCardParameters>;
 /* eslint-enable @typescript-eslint/no-redeclare */
