803efdd57bbbc6711078b34966d1d27efc840d94
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-23 15:50:38 -0300

    🎨 app: add mock payment button

--- src/components/payments/Payments.tsx
+++ src/components/payments/Payments.tsx
@@ -1,16 +1,36 @@
 import React from "react";
-import { Text } from "tamagui";
+import { ms } from "react-native-size-matters";
+import { Button, Text, View } from "tamagui";
 
 import BaseLayout from "../shared/BaseLayout";
 import SafeView from "../shared/SafeView";
+
+const pressStyle = { backgroundColor: "$interactiveBaseBrandDefault", opacity: 0.9 };
+
+const pay = () => {
+  /// TODO implement payment logic
+};
 
 export default function Payments() {
   return (
     <SafeView>
       <BaseLayout flex={1}>
-        <Text fontSize={40} fontFamily="$mono" fontWeight={700}>
-          Payments
-        </Text>
+        <View gap={ms(40)}>
+          <Text fontSize={40} fontFamily="$mono" fontWeight={700}>
+            Payments
+          </Text>
+          <Button
+            borderRadius="$r2"
+            variant="outlined"
+            backgroundColor="$interactiveBaseBrandDefault"
+            color="$interactiveOnBaseBrandDefault"
+            onPress={pay}
+            fontWeight={600}
+            pressStyle={pressStyle}
+          >
+            Pay Now
+          </Button>
+        </View>
       </BaseLayout>
     </SafeView>
   );
