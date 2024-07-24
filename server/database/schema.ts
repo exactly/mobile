ccf48792767e8b3c84f4b59bc6b2085a3dfb7747
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-24 12:02:09 -0300

    🗃️ server: add kyc field to credentials

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { customType, integer, jsonb, pgTable, text } from "drizzle-orm/pg-core";
+import { boolean, customType, integer, jsonb, pgTable, text } from "drizzle-orm/pg-core";
 
 const bytea = customType<{ data: Uint8Array; driverData: string }>({ dataType: () => "bytea" });
 
@@ -8,6 +8,7 @@ export const credentials = pgTable("credentials", {
   publicKey: bytea("public_key").notNull(),
   transports: text("transports").array(),
   counter: integer("counter").notNull(),
+  kyc: boolean("kyc").default(false),
 });
 
 export const cards = pgTable("cards", {
