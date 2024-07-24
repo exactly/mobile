cc8e61b5cc7f74f25c768f73f87e681e7518191a
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-24 16:50:42 -0300

    🎨 app: move passkey utils to a separate component

---
+++ src/components/payments/PasskeyUtils.tsx
@@ -0,0 +1,79 @@
+import { useQuery } from "@tanstack/react-query";
+import * as Clipboard from "expo-clipboard";
+import React from "react";
+import { ms } from "react-native-size-matters";
+import { View, Text, Spinner, Button } from "tamagui";
+
+import handleError from "../../utils/handleError";
+import loadPasskey from "../../utils/loadPasskey";
+const pressStyle = { backgroundColor: "$interactiveBaseBrandDefault", opacity: 0.9 };
+
+export default function PasskeyUtils() {
+  const {
+    data: passkey,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery({ queryKey: ["passkey"], queryFn: loadPasskey, enabled: false, staleTime: 1 });
+
+  function getPasskey() {
+    refetch().catch(handleError);
+  }
+
+  const copy = () => {
+    if (!passkey?.credentialId) return;
+    Clipboard.setStringAsync(passkey.credentialId).catch(handleError);
+  };
+  return (
+    <View gap={ms(10)}>
+      <Text fontSize={ms(16)} color="$uiNeutralPrimary" fontWeight="bold">
+        Passkey
+      </Text>
+      {isLoading && <Spinner backgroundColor="$interactiveBaseBrandDefault" />}
+      {error && (
+        <Text color="$uiErrorPrimary" fontWeight="bold">
+          {error.message}
+        </Text>
+      )}
+      {passkey?.credentialId && (
+        <View borderRadius="$r4" borderWidth={2} borderColor="$borderNeutralSoft" padding={ms(10)}>
+          <Text textAlign="center" fontSize={ms(14)} fontFamily="$mono" width="100%" fontWeight="bold">
+            {passkey.credentialId}
+          </Text>
+        </View>
+      )}
+
+      <View flexDirection="row" gap={ms(10)}>
+        <Button
+          borderRadius="$r2"
+          variant="outlined"
+          backgroundColor="$interactiveBaseBrandDefault"
+          color="$interactiveOnBaseBrandDefault"
+          onPress={getPasskey}
+          fontWeight="bold"
+          pressStyle={pressStyle}
+          padding={ms(10)}
+          flex={1}
+        >
+          Refresh
+        </Button>
+
+        {passkey?.credentialId && (
+          <Button
+            borderRadius="$r2"
+            variant="outlined"
+            backgroundColor="$interactiveBaseBrandSoftDefault"
+            color="$interactiveOnBaseBrandSoft"
+            onPress={copy}
+            fontWeight="bold"
+            pressStyle={pressStyle}
+            padding={ms(10)}
+            flex={1}
+          >
+            Copy
+          </Button>
+        )}
+      </View>
+    </View>
+  );
+}
--- src/components/payments/Payments.tsx
+++ src/components/payments/Payments.tsx
@@ -1,92 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
-import * as Clipboard from "expo-clipboard";
 import React from "react";
 import { ms } from "react-native-size-matters";
-import { Button, Spinner, Text, View } from "tamagui";
+import { ScrollView, Text, View } from "tamagui";
 
-import handleError from "../../utils/handleError";
-import loadPasskey from "../../utils/loadPasskey";
+import PasskeyUtils from "./PasskeyUtils";
 import BaseLayout from "../shared/BaseLayout";
 import SafeView from "../shared/SafeView";
 
-const pressStyle = { backgroundColor: "$interactiveBaseBrandDefault", opacity: 0.9 };
-
 export default function Payments() {
-  const {
-    data: passkey,
-    isLoading,
-    error,
-    refetch,
-  } = useQuery({ queryKey: ["passkey"], queryFn: loadPasskey, enabled: false, staleTime: 1 });
-
-  function getPasskey() {
-    refetch().catch(handleError);
-  }
-
-  const copy = () => {
-    if (!passkey?.credentialId) return;
-    Clipboard.setStringAsync(passkey.credentialId).catch(handleError);
-  };
-
   return (
     <SafeView>
-      <BaseLayout flex={1}>
-        <View gap={ms(20)}>
-          <Text fontSize={40} fontFamily="$mono" fontWeight="bold">
-            Payments
-          </Text>
-          <View gap={ms(10)}>
-            <Text fontSize={ms(16)} color="$uiNeutralPrimary" fontWeight="bold">
-              Passkey
+      <ScrollView>
+        <BaseLayout flex={1}>
+          <View gap={ms(20)}>
+            <Text fontSize={40} fontFamily="$mono" fontWeight="bold">
+              Payments
             </Text>
-            {isLoading && <Spinner backgroundColor="$interactiveBaseBrandDefault" />}
-            {error && (
-              <Text color="$uiErrorPrimary" fontWeight="bold">
-                {error.message}
-              </Text>
-            )}
-            {passkey?.credentialId && (
-              <View borderRadius="$r4" borderWidth={2} borderColor="$borderNeutralSoft" padding={ms(10)}>
-                <Text textAlign="center" fontSize={ms(14)} fontFamily="$mono" width="100%" fontWeight="bold">
-                  {passkey.credentialId}
-                </Text>
-              </View>
-            )}
-
-            <View flexDirection="row" gap={ms(10)}>
-              <Button
-                borderRadius="$r2"
-                variant="outlined"
-                backgroundColor="$interactiveBaseBrandDefault"
-                color="$interactiveOnBaseBrandDefault"
-                onPress={getPasskey}
-                fontWeight="bold"
-                pressStyle={pressStyle}
-                padding={ms(10)}
-                flex={1}
-              >
-                Refresh
-              </Button>
-
-              {passkey?.credentialId && (
-                <Button
-                  borderRadius="$r2"
-                  variant="outlined"
-                  backgroundColor="$interactiveBaseBrandSoftDefault"
-                  color="$interactiveOnBaseBrandSoft"
-                  onPress={copy}
-                  fontWeight="bold"
-                  pressStyle={pressStyle}
-                  padding={ms(10)}
-                  flex={1}
-                >
-                  Copy
-                </Button>
-              )}
-            </View>
+            <PasskeyUtils />
           </View>
-        </View>
-      </BaseLayout>
+        </BaseLayout>
+      </ScrollView>
     </SafeView>
   );
 }
