f7e231daf6a577a0e83823ab5ca721d092ab1a88
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-24 14:37:22 -0300

    ✨ app: implement card details

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,18 +1,52 @@
-import { ArrowRight, Calculator, CaretRight, Info, Plus } from "phosphor-react-native";
+import { useQuery } from "@tanstack/react-query";
+import { ArrowRight, Calculator, CaretRight, Eye, Info, Plus, Snowflake } from "phosphor-react-native";
 import React from "react";
 import { Pressable } from "react-native";
 import { ms } from "react-native-size-matters";
-import { ScrollView, Text, View, useTheme } from "tamagui";
+import { Button, ScrollView, Switch, Text, View, styled, useTheme, Spinner } from "tamagui";
 
-import CardActions from "./CardActions";
+import CardDetails from "./CardDetails";
 import LatestActivity from "./LatestActivity";
 import SpendingLimitButton from "./SpendingLimitButton";
+import handleError from "../../utils/handleError";
+import { createCard, getPAN } from "../../utils/server";
 import BaseLayout from "../shared/BaseLayout";
 import InfoPreview from "../shared/InfoPreview";
 import SafeView from "../shared/SafeView";
 
+const StyledAction = styled(View, {
+  flex: 1,
+  minHeight: ms(140),
+  borderWidth: 1,
+  padding: ms(16),
+  borderRadius: 10,
+  backgroundColor: "$backgroundSoft",
+  borderColor: "$borderNeutralSoft",
+  justifyContent: "space-between",
+});
+
+const handleCreateCard = () => {
+  createCard({
+    cardholder: "Vitalik Buterin",
+    email: "vb@gmail.com",
+    phone: { countryCode: "55", number: "988887777" },
+    limits: { daily: 1000, weekly: 3000, monthly: 5000 },
+  })
+    .then()
+    .catch(handleError);
+};
+
 export default function Card() {
   const theme = useTheme();
+  const {
+    data: uri,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery({ queryKey: ["pan"], queryFn: getPAN, enabled: false, staleTime: 1000 * 60 });
+  const getCard = () => {
+    refetch().catch(handleError);
+  };
   return (
     <SafeView paddingBottom={0}>
       <ScrollView>
@@ -27,7 +61,59 @@ export default function Card() {
               </Pressable>
             </View>
 
-            <CardActions />
+            {isLoading && <Spinner color="$interactiveBaseBrandDefault" />}
+            {!isLoading && uri && <CardDetails uri={uri} />}
+            {error && (
+              <Text color="$uiErrorPrimary" fontWeight="bold">
+                {error.message}
+              </Text>
+            )}
+
+            <Button
+              borderRadius="$r2"
+              variant="outlined"
+              backgroundColor="$interactiveBaseBrandDefault"
+              color="$interactiveOnBaseBrandDefault"
+              onPress={handleCreateCard}
+              fontWeight={600}
+            >
+              Create Card
+            </Button>
+
+            <View flexDirection="row" justifyContent="space-between" gap={ms(10)}>
+              <StyledAction>
+                <Pressable onPress={getCard}>
+                  <View gap={ms(10)}>
+                    <Eye size={ms(24)} color={theme.backgroundBrand.get()} weight="bold" />
+                    <Text color="$uiPrimary" fontSize={ms(15)}>
+                      Details
+                    </Text>
+                    <Text color="$interactiveBaseBrandDefault" fontSize={ms(15)} fontWeight="bold">
+                      Reveal
+                    </Text>
+                  </View>
+                </Pressable>
+              </StyledAction>
+
+              <StyledAction>
+                <Pressable>
+                  <View gap={ms(10)}>
+                    <Snowflake size={ms(24)} color={theme.backgroundBrand.get()} weight="bold" />
+                    <Text color="$uiPrimary" fontSize={ms(15)}>
+                      Freeze
+                    </Text>
+                    <Switch
+                      size={ms(24)}
+                      backgroundColor="$backgroundMild"
+                      maxWidth="50%"
+                      borderColor={theme.borderNeutralSoft}
+                    >
+                      <Switch.Thumb animation="quicker" backgroundColor="$backgroundSoft" shadowColor="$uiPrimary" />
+                    </Switch>
+                  </View>
+                </Pressable>
+              </StyledAction>
+            </View>
 
             <InfoPreview
               title="Installments"
