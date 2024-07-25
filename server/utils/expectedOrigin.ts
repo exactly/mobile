084826d3ec93dd1cd6a1b8133c5187a4140abc99
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-25 18:12:57 -0300

    ✨ server: read android origin from env

--- server/utils/expectedOrigin.ts
+++ server/utils/expectedOrigin.ts
@@ -2,7 +2,7 @@ import { ORIGIN } from "../middleware/cors.js";
 
 export default function expectedOrigin(userAgent?: string) {
   if (userAgent?.match(/okhttp\/\d+\.\d+\.\d+/)) {
-    return "android:apk-key-hash:-sYXRdwJA3hvue3mKpYrOZ9zSPC7b4mbgzJmdZEDO5w";
+    return process.env.ANDROID_ORIGIN || "android:apk-key-hash:-sYXRdwJA3hvue3mKpYrOZ9zSPC7b4mbgzJmdZEDO5w";
   }
   return ORIGIN;
 }
