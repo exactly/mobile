eafff2afc73b9b42b41f2615a1d76ecf0c2ebfe4
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-25 15:20:36 -0300

    🎨 app: set query client defaults for passkey

--- src/utils/queryClient.ts
+++ src/utils/queryClient.ts
@@ -1,4 +1,11 @@
 import { QueryClient } from "@tanstack/react-query";
 
+import loadPasskey from "./loadPasskey";
+
 const queryClient = new QueryClient();
+queryClient.setQueryDefaults(["passkey"], {
+  retry: false,
+  staleTime: Infinity,
+  queryFn: loadPasskey,
+});
 export default queryClient;
