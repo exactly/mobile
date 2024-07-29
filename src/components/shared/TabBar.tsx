e18572af9392f67f8f787fde0a450077b7f0d85c
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-29 16:24:23 -0300

    ♻️ app: refactor tab bar navigation

--- src/components/shared/TabBar.tsx
+++ src/components/shared/TabBar.tsx
@@ -1,5 +1,6 @@
 import type { BottomTabBarProps } from "@react-navigation/bottom-tabs";
-import React from "react";
+import type { NavigationRoute } from "@sentry/react-native/dist/js/tracing/reactnavigation";
+import React, { useCallback } from "react";
 import { ms } from "react-native-size-matters";
 import { useTheme, ToggleGroup, ButtonIcon, Text } from "tamagui";
 
@@ -7,54 +8,55 @@ import SafeView from "./SafeView";
 
 export default function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
   const theme = useTheme();
+  const onPress = useCallback(
+    (route: NavigationRoute, focused: boolean) => {
+      const event = navigation.emit({
+        type: "tabPress",
+        target: route.key,
+        canPreventDefault: true,
+      });
+      if (!focused && !event.defaultPrevented) {
+        navigation.navigate(route.name, route.params);
+      }
+    },
+    [navigation],
+  );
   return (
     <SafeView flex={0} paddingTop={0}>
       <ToggleGroup
-        type="single"
+        backgroundColor="$backgroundSoft"
         borderRadius={0}
         borderTopColor="$borderNeutralSoft"
         borderTopWidth={1}
-        backgroundColor="$backgroundSoft"
+        type="single"
       >
         {state.routes.map((route, index) => {
           const { options } = descriptors[route.key] || { options: undefined };
           if (!options) throw new Error("no navigation button options found");
-
           const label = options.title;
           const icon = options.tabBarIcon;
-          const isFocused = state.index === index;
-
-          const onPress = () => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
-
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name, route.params);
-            }
-          };
-
+          const focused = state.index === index;
           return (
             <ToggleGroup.Item
               key={route.key}
               backgroundColor="transparent"
               borderWidth={0}
               flex={1}
-              onPress={onPress}
+              onPress={() => {
+                onPress(route, focused);
+              }}
               padding={ms(10)}
               role="button"
               value="center"
             >
               <ButtonIcon>
                 {icon?.({
-                  focused: isFocused,
-                  color: isFocused ? theme.uiBrandPrimary.val : theme.uiNeutralSecondary.val,
+                  focused,
+                  color: focused ? theme.uiBrandPrimary.val : theme.uiNeutralSecondary.val,
                   size: 24,
                 })}
               </ButtonIcon>
-              <Text textAlign="center" color={isFocused ? theme.uiBrandPrimary : theme.uiNeutralSecondary}>
+              <Text textAlign="center" color={focused ? theme.uiBrandPrimary : theme.uiNeutralSecondary}>
                 {label}
               </Text>
             </ToggleGroup.Item>
