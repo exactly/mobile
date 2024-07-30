e12f48d699617bb6150e71c06ec39f4ee94fe008
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-07-30 15:46:17 -0300

    ✨ app: render card on web platform

--- src/components/card/CardDetails.tsx
+++ src/components/card/CardDetails.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import { Platform } from "react-native";
 import { WebView } from "react-native-webview";
 import { Spinner, styled } from "tamagui";
 
 const StyledWebView = styled(WebView, {});
 
+const style = {
+  height: "100%",
+  width: "100%",
+  backgroundColor: "transparent",
+};
+
 export default function CardDetails({ uri }: { uri: string }) {
   const [webViewHeight, setWebViewHeight] = useState(0);
-  return (
+  return Platform.OS === "web" ? (
+    <iframe src={uri} style={style} />
+  ) : (
     <StyledWebView
       onMessage={(event) => {
         setWebViewHeight(Number(event.nativeEvent.data));
