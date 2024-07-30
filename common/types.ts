f0afbb78b64f488c815a6f0b0bdeada50859c081
danilo neves cruz <cruzdanilo@gmail.com>
2024-07-30 20:38:45 -0300

    ✨ common: add hex validation

--- common/types.ts
+++ common/types.ts
@@ -1,9 +1,11 @@
 import { type InferOutput, custom, email, number, object, pipe, regex, string } from "valibot";
-import { type Hash as ViemHash, isHash } from "viem";
+import { type Hash as ViemHash, type Hex as ViemHex, isHash, isHex } from "viem";
 
 export const Base64URL = pipe(string(), regex(/^[\w-]+$/));
 
 export const Hash = custom<ViemHash>(isHash as (hash: unknown) => hash is ViemHash);
+
+export const Hex = custom<ViemHex>(isHex);
 
 export const Passkey = object({ credentialId: Base64URL, x: Hash, y: Hash });
 
@@ -17,7 +19,7 @@ export const CreateCardParameters = object({
 /* eslint-disable @typescript-eslint/no-redeclare */
 export type Base64URL = InferOutput<typeof Base64URL>;
 export type Hash = InferOutput<typeof Hash>;
+export type Hex = InferOutput<typeof Hex>;
 export type Passkey = InferOutput<typeof Passkey>;
-
 export type CreateCardParameters = InferOutput<typeof CreateCardParameters>;
 /* eslint-enable @typescript-eslint/no-redeclare */
