9dd9646f4d3338ad18d96f023a95a6c9759b872d
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-08-06 18:41:52 -0300

    💄 app: fix success screen styles

--- src/components/onboarding/Success.tsx
+++ src/components/onboarding/Success.tsx
@@ -1,48 +1,55 @@
 import { ArrowRight } from "@tamagui/lucide-icons";
 import { router } from "expo-router";
 import React from "react";
-import { ms } from "react-native-size-matters";
-import { Text, View } from "tamagui";
+import { StyleSheet } from "react-native";
+import { View } from "tamagui";
 
 import AccountCreatedBlob from "../../assets/images/account-created-blob.svg";
 import AccountCreatedImage from "../../assets/images/account-created.svg";
 import ActionButton from "../shared/ActionButton";
-import BaseLayout from "../shared/BaseLayout";
 import SafeView from "../shared/SafeView";
+import Text from "../shared/Text";
 
 export default function Success() {
   return (
-    <SafeView>
-      <BaseLayout flex={1}>
-        <View flex={1}>
-          <View alignItems="center" alignContent="center" justifyContent="center" height="100%" flex={1}>
-            <View position="absolute">
-              <AccountCreatedBlob />
-            </View>
-            <View position="absolute">
-              <AccountCreatedImage />
-            </View>
+    <SafeView
+      backgroundColor="$backgroundSoft"
+      padding="$s5"
+      justifyContent="space-between"
+      alignItems="stretch"
+      gap="$s5"
+    >
+      <View justifyContent="center" alignItems="center" flexGrow={1} flexShrink={1}>
+        <View width="100%" aspectRatio={1} justifyContent="center" alignItems="center" flexShrink={1}>
+          <View width="100%" height="100%" aspectRatio={1}>
+            <AccountCreatedBlob width="100%" height="100%" />
           </View>
-
-          <View flexDirection="column" paddingVertical={ms(10)} paddingHorizontal={ms(20)} gap={ms(40)}>
-            <Text fontSize={ms(28)} fontWeight={700} color="$interactiveBaseBrandDefault" textAlign="center">
-              Account created successfully!
-            </Text>
-
-            <View flexDirection="column" gap={ms(10)} paddingBottom={ms(30)}>
-              <ActionButton
-                onPress={() => {
-                  // TODO Implement account setup
-                  router.replace("/(app)");
-                }}
-                iconAfter={<ArrowRight color="$interactiveOnBaseBrandDefault" />}
-              >
-                Start account setup
-              </ActionButton>
-            </View>
+          <View width="100%" height="100%" aspectRatio={1} style={StyleSheet.absoluteFill}>
+            <AccountCreatedImage width="100%" height="100%" />
           </View>
         </View>
-      </BaseLayout>
+        <View gap="$s5" justifyContent="center">
+          <Text emphasized title brand centered>
+            Account created successfully!
+          </Text>
+        </View>
+      </View>
+
+      <View alignItems="stretch" alignSelf="stretch">
+        <View flexDirection="row" alignSelf="stretch">
+          <ActionButton
+            marginTop="$s4"
+            marginBottom="$s5"
+            onPress={() => {
+              // TODO Implement account setup
+              router.replace("/(app)");
+            }}
+            iconAfter={<ArrowRight color="$interactiveOnBaseBrandDefault" />}
+          >
+            Start account setup
+          </ActionButton>
+        </View>
+      </View>
     </SafeView>
   );
 }
