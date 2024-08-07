5e2193edf0256f03cad753bcd0de3d2fed9940c8
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-08-07 14:52:58 -0300

    💄 app: update passkey creation screens styles

--- src/components/onboarding/Passkeys.tsx
+++ src/components/onboarding/Passkeys.tsx
@@ -55,72 +55,70 @@ export default function Passkeys() {
   }, [connect, connector, data, isSuccess]);
 
   return (
-    <SafeView
-      backgroundColor="$backgroundSoft"
-      padding="$s5"
-      justifyContent="space-between"
-      alignItems="stretch"
-      gap="$s5"
-    >
-      <View position="absolute" top="$s7" right="$s5" zIndex={1}>
-        <Pressable onPress={close}>
-          <X size={ms(25)} color="$uiNeutralSecondary" />
-        </Pressable>
-      </View>
-      <View justifyContent="center" alignItems="center" flexGrow={1} flexShrink={1}>
-        <View width="100%" aspectRatio={1} justifyContent="center" alignItems="center" flexShrink={1}>
-          <View width="100%" height="100%" aspectRatio={1}>
-            <PasskeysBlob width="100%" height="100%" />
-          </View>
-          <View width="100%" height="100%" aspectRatio={1} style={StyleSheet.absoluteFill}>
-            <PasskeysImage width="100%" height="100%" />
-          </View>
-        </View>
-
-        <View gap="$s5" justifyContent="center">
-          <Text emphasized title brand centered>
-            A secure and easy way to access your account
-          </Text>
-          <Text secondary fontSize={ms(13)} color="$uiBaseSecondary" textAlign="center">
-            To keep your account secure, Exa App uses passkeys, a passwordless authentication method protected by your
-            device biometric verification.
-          </Text>
-        </View>
-      </View>
-
-      <View alignItems="stretch" alignSelf="stretch">
-        <View flexDirection="row" alignSelf="stretch" justifyContent="center">
-          <Text fontSize={ms(11)} color="$uiNeutralPlaceholder">
-            By continuing, I accept the
-          </Text>
-          <Text fontSize={ms(11)} color="$interactiveBaseBrandDefault">
-            {" "}
-            Terms & Conditions
-          </Text>
-        </View>
-
-        <View flexDirection="row" alignSelf="stretch">
-          <ActionButton
-            flex={1}
-            marginTop="$s4"
-            marginBottom="$s5"
-            isLoading={isPending || isConnecting}
-            loadingContent="Creating account..."
-            iconAfter={<Key size={ms(20)} color="$interactiveOnBaseBrandDefault" fontWeight="bold" />}
-            onPress={() => {
-              createAccount();
-            }}
-          >
-            Create account
-          </ActionButton>
-        </View>
-
-        <View flexDirection="row" justifyContent="center">
-          <Pressable onPress={learnMore}>
-            <Text textAlign="center" fontSize={ms(13)} fontWeight="bold" color="$interactiveBaseBrandDefault">
-              Learn more about passkeys
-            </Text>
+    <SafeView backgroundColor="$backgroundSoft" justifyContent="space-between" alignItems="stretch" gap="$s5">
+      <View flex={1} padding="$s5">
+        <View position="absolute" right="$s5" zIndex={1}>
+          <Pressable onPress={close}>
+            <X size={ms(25)} color="$uiNeutralSecondary" />
           </Pressable>
+        </View>
+        <View justifyContent="center" alignItems="center" flexGrow={1} flexShrink={1}>
+          <View width="100%" aspectRatio={1} justifyContent="center" alignItems="center" flexShrink={1}>
+            <View width="100%" height="100%" aspectRatio={1}>
+              <PasskeysBlob width="100%" height="100%" />
+            </View>
+            <View width="100%" height="100%" aspectRatio={1} style={StyleSheet.absoluteFill}>
+              <PasskeysImage width="100%" height="100%" />
+            </View>
+          </View>
+
+          <View gap="$s5" justifyContent="center">
+            <Text emphasized title brand centered>
+              A secure and easy way to access your account
+            </Text>
+            <Text secondary fontSize={ms(13)} color="$uiBaseSecondary" textAlign="center">
+              To keep your account secure, Exa App uses passkeys, a passwordless authentication method protected by your
+              device biometric verification.
+            </Text>
+          </View>
+        </View>
+
+        <View alignItems="stretch" alignSelf="stretch">
+          <View flexDirection="row" alignSelf="stretch" justifyContent="center">
+            <Text fontSize={ms(11)} color="$uiNeutralPlaceholder">
+              By continuing, I accept the
+            </Text>
+            <Text fontSize={ms(11)} color="$interactiveBaseBrandDefault">
+              {" "}
+              Terms & Conditions
+            </Text>
+          </View>
+
+          <View>
+            <View flexDirection="row" alignSelf="stretch">
+              <ActionButton
+                flex={1}
+                marginTop="$s4"
+                marginBottom="$s5"
+                isLoading={isPending || isConnecting}
+                loadingContent="Creating account..."
+                iconAfter={<Key size={ms(20)} color="$interactiveOnBaseBrandDefault" fontWeight="bold" />}
+                onPress={() => {
+                  createAccount();
+                }}
+              >
+                Create account
+              </ActionButton>
+            </View>
+
+            <View flexDirection="row" justifyContent="center">
+              <Pressable onPress={learnMore}>
+                <Text textAlign="center" fontSize={ms(13)} fontWeight="bold" color="$interactiveBaseBrandDefault">
+                  Learn more about passkeys
+                </Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
       </View>
     </SafeView>
--- src/components/onboarding/Success.tsx
+++ src/components/onboarding/Success.tsx
@@ -12,42 +12,38 @@ import Text from "../shared/Text";
 
 export default function Success() {
   return (
-    <SafeView
-      backgroundColor="$backgroundSoft"
-      padding="$s5"
-      justifyContent="space-between"
-      alignItems="stretch"
-      gap="$s5"
-    >
-      <View justifyContent="center" alignItems="center" flexGrow={1} flexShrink={1}>
-        <View width="100%" aspectRatio={1} justifyContent="center" alignItems="center" flexShrink={1}>
-          <View width="100%" height="100%" aspectRatio={1}>
-            <AccountCreatedBlob width="100%" height="100%" />
+    <SafeView backgroundColor="$backgroundSoft" justifyContent="space-between" alignItems="stretch" gap="$s5">
+      <View flex={1} padding="$s5">
+        <View justifyContent="center" alignItems="center" flexGrow={1} flexShrink={1}>
+          <View width="100%" aspectRatio={1} justifyContent="center" alignItems="center" flexShrink={1}>
+            <View width="100%" height="100%" aspectRatio={1}>
+              <AccountCreatedBlob width="100%" height="100%" />
+            </View>
+            <View width="100%" height="100%" aspectRatio={1} style={StyleSheet.absoluteFill}>
+              <AccountCreatedImage width="100%" height="100%" />
+            </View>
           </View>
-          <View width="100%" height="100%" aspectRatio={1} style={StyleSheet.absoluteFill}>
-            <AccountCreatedImage width="100%" height="100%" />
+          <View gap="$s5" justifyContent="center">
+            <Text emphasized title brand centered>
+              Account created successfully!
+            </Text>
           </View>
         </View>
-        <View gap="$s5" justifyContent="center">
-          <Text emphasized title brand centered>
-            Account created successfully!
-          </Text>
-        </View>
-      </View>
 
-      <View alignItems="stretch" alignSelf="stretch">
-        <View flexDirection="row" alignSelf="stretch">
-          <ActionButton
-            marginTop="$s4"
-            marginBottom="$s5"
-            onPress={() => {
-              // TODO Implement account setup
-              router.replace("/(app)");
-            }}
-            iconAfter={<ArrowRight color="$interactiveOnBaseBrandDefault" />}
-          >
-            Start account setup
-          </ActionButton>
+        <View>
+          <View flexDirection="row" alignSelf="stretch">
+            <ActionButton
+              marginTop="$s4"
+              marginBottom="$s5"
+              onPress={() => {
+                // TODO Implement account setup
+                router.replace("/(app)");
+              }}
+              iconAfter={<ArrowRight color="$interactiveOnBaseBrandDefault" />}
+            >
+              Start account setup
+            </ActionButton>
+          </View>
         </View>
       </View>
     </SafeView>
