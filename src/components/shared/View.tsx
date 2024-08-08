3e4c35055d4c71574175422c577477e04a7f882e
Guillermo Diéguez <guillermodieguez@outlook.com>
2024-08-08 14:57:24 -0300

    ✨ app: add tamagui view component

---
+++ src/components/shared/View.tsx
@@ -0,0 +1,35 @@
+import type { ViewProps } from "tamagui";
+import { styled, View } from "tamagui";
+
+export interface ViewProperties extends ViewProps {
+  padded?: boolean;
+  fullScreen?: boolean;
+  tab?: boolean;
+  smallPadding?: boolean;
+}
+
+export default styled(View, {
+  variants: {
+    fullScreen: {
+      true: {
+        width: "100%",
+        height: "100%",
+      },
+    },
+    padded: {
+      true: {
+        padding: "$s5",
+      },
+    },
+    tab: {
+      true: {
+        paddingBottom: 0,
+      },
+    },
+    smallPadding: {
+      true: {
+        padding: "$s3",
+      },
+    },
+  },
+});
