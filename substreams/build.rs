d746efce936dc77c16996b9fdec845afd275a6d0
danilo neves cruz <cruzdanilo@gmail.com>
2024-08-13 16:05:19 -0300

    🔨 substreams: generate entrypoint abi

--- substreams/build.rs
+++ substreams/build.rs
@@ -1,7 +1,10 @@
 use anyhow::{Error, Ok, Result};
 use std::process::Command;
+use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), Error> {
+  Abigen::new("EntryPoint", "abi/entrypoint.json")?.generate()?.write_to_file("src/abi/entrypoint.rs")?;
+
   Command::new("substreams").arg("protogen").status().expect("substreams protogen failed");
 
   Ok(())
