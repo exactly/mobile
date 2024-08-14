0927062f796b1352a602c08c08306476eeafce7c
danilo neves cruz <cruzdanilo@gmail.com>
2024-08-14 10:57:55 -0300

    🔨 substreams: add build script dependencies

--- substreams/build.rs
+++ substreams/build.rs
@@ -3,6 +3,10 @@ use std::process::Command;
 use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), Error> {
+  println!("cargo::rerun-if-changed=abi");
+  println!("cargo::rerun-if-changed=exa.proto");
+  println!("cargo::rerun-if-changed=substreams.yaml");
+
   Abigen::new("EntryPoint", "abi/entrypoint.json")?.generate()?.write_to_file("src/abi/entrypoint.rs")?;
   Abigen::new("ERC20", "abi/erc20.json")?.generate()?.write_to_file("src/abi/erc20.rs")?;
 
