928f8df01bb30b95694903c51d1fbf87d8e0afea
danilo neves cruz <cruzdanilo@gmail.com>
2024-08-14 13:14:13 -0300

    ⚡️ substreams: optimize account extraction

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -16,10 +16,15 @@ mod pb;
 pub fn map_accounts(block: Block) -> Result<Accounts, Error> {
   Ok(Accounts {
     accounts: block
-      .events::<AccountDeployed>(&[&hex!("5FF137D4b0FDCD49DcA30c7CF57E578a026d2789")])
-      .filter_map(|(event, _)| match event.factory.as_slice() {
-        hex!("6E1b5A67adD32E8dC034c23b8022b54821ED297b") => Some(event.sender),
-        _ => None,
+      .logs()
+      .filter_map(|log| {
+        if log.address() != hex!("5FF137D4b0FDCD49DcA30c7CF57E578a026d2789") {
+          return None;
+        }
+        AccountDeployed::match_and_decode(log).and_then(|event| match event.factory.as_slice() {
+          hex!("6E1b5A67adD32E8dC034c23b8022b54821ED297b") => Some(event.sender),
+          _ => None,
+        })
       })
       .collect(),
   })
