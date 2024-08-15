66f4211281e0af094af51cb235fd6cd78138fb4f
danilo neves cruz <cruzdanilo@gmail.com>
2024-08-15 10:39:57 -0300

    🔨 substreams: assert protogen success

--- substreams/build.rs
+++ substreams/build.rs
@@ -10,7 +10,7 @@ fn main() -> Result<(), Error> {
   Abigen::new("EntryPoint", "abi/entrypoint.json")?.generate()?.write_to_file("src/abi/entrypoint.rs")?;
   Abigen::new("ERC20", "abi/erc20.json")?.generate()?.write_to_file("src/abi/erc20.rs")?;
 
-  Command::new("substreams").arg("protogen").status().expect("substreams protogen failed");
+  assert!(Command::new("substreams").arg("protogen").status().expect("protogen failed").success());
 
   Ok(())
 }
