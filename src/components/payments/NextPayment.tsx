687ae0412458a56efc4e622995d26a89d2164e5b
guillermo dieguez <guillermodieguez@outlook.com>
2024-08-17 16:12:38 -0300

    ✨ app: implement rollover

--- src/components/payments/NextPayment.tsx
+++ src/components/payments/NextPayment.tsx
@@ -1,12 +1,20 @@
 import { Coins, FileText, IterationCw } from "@tamagui/lucide-icons";
 import { formatDistanceToNow, intlFormat } from "date-fns";
-import React from "react";
+import React, { useCallback } from "react";
 import { Pressable } from "react-native";
 import { ms } from "react-native-size-matters";
-import { zeroAddress } from "viem";
-import { useAccount } from "wagmi";
+import { maxUint256, zeroAddress } from "viem";
+import { useAccount, useWriteContract } from "wagmi";
 
-import { previewerAddress, useReadPreviewerExactly } from "../../generated/contracts";
+import {
+  debtManagerAddress,
+  marketUSDCAddress,
+  previewerAddress,
+  useReadPreviewerExactly,
+  useSimulateDebtManagerRollFixedToFloating,
+  useSimulateMarketApprove,
+} from "../../generated/contracts";
+import WAD from "../../utils/WAD";
 import Button from "../shared/Button";
 import Text from "../shared/Text";
 import View from "../shared/View";
@@ -37,77 +45,126 @@ export default function NextPayment() {
       }
     }
   }
-  const maturity = usdDue.keys().next().value as bigint;
-  const { previewValue, position } = usdDue.values().next().value as { previewValue: bigint; position: bigint };
+  const maturity = usdDue.keys().next().value as bigint | undefined;
+  const duePayment = usdDue.get(maturity ?? 0n);
+
+  const { data: approveSimulation } = useSimulateMarketApprove({
+    address: marketUSDCAddress,
+    args: [debtManagerAddress, maxUint256],
+  });
+  const { data: rolloverSimulation } = useSimulateDebtManagerRollFixedToFloating({
+    address: debtManagerAddress,
+    args: [marketUSDCAddress, maturity ?? 0n, maxUint256, WAD],
+  });
+
+  const { writeContract: approve } = useWriteContract();
+  const { writeContract: rollover } = useWriteContract();
+
+  const approveMarket = useCallback(() => {
+    if (!approveSimulation) throw new Error("no approve simulation");
+    approve(approveSimulation.request);
+  }, [approve, approveSimulation]);
+  const rollFixedToFloating = useCallback(() => {
+    if (!rolloverSimulation) throw new Error("no rollover simulation");
+    rollover(rolloverSimulation.request);
+  }, [rollover, rolloverSimulation]);
   return (
     <View backgroundColor="$backgroundSoft" borderRadius="$r3" padding="$s4" gap="$s5">
-      <View>
-        <View flexDirection="row" alignItems="center" justifyContent="space-between">
-          {maturity && (
-            <Text emphasized headline>
-              Due in {formatDistanceToNow(Number(maturity) * 1000)}
-            </Text>
-          )}
-          <Pressable>
-            <View flexDirection="row" gap="$s2" alignItems="center">
-              <Text emphasized footnote color="$interactiveTextBrandDefault">
-                Statement
+      {maturity ? (
+        <>
+          <View>
+            <View flexDirection="row" alignItems="center" justifyContent="space-between">
+              <Text emphasized headline>
+                Due in {formatDistanceToNow(Number(maturity) * 1000)}
               </Text>
-              <FileText size={14} color="$interactiveTextBrandDefault" fontWeight="bold" />
+              <Pressable>
+                <View flexDirection="row" gap="$s2" alignItems="center">
+                  <Text emphasized footnote color="$interactiveTextBrandDefault">
+                    Statement
+                  </Text>
+                  <FileText size={14} color="$interactiveTextBrandDefault" fontWeight="bold" />
+                </View>
+              </Pressable>
             </View>
-          </Pressable>
-        </View>
-        <Text footnote color="$uiNeutralSecondary">
-          {intlFormat(new Date(Number(maturity) * 1000), { dateStyle: "medium" }).toUpperCase()}
-        </Text>
-      </View>
-      <View gap="$s5">
-        <View flexDirection="column" justifyContent="center" alignItems="center">
-          <Text textAlign="center" fontFamily="$mono" fontSize={ms(40)} fontWeight="bold" overflow="hidden">
-            {(Number(previewValue) / 1e18).toLocaleString(undefined, {
-              style: "currency",
-              currency: "USD",
-            })}
+            <Text footnote color="$uiNeutralSecondary">
+              {intlFormat(new Date(Number(maturity) * 1000), { dateStyle: "medium" }).toUpperCase()}
+            </Text>
+          </View>
+          {duePayment && (
+            <View gap="$s5">
+              <View flexDirection="column" justifyContent="center" alignItems="center">
+                <Text textAlign="center" fontFamily="$mono" fontSize={ms(40)} fontWeight="bold" overflow="hidden">
+                  {(Number(duePayment.previewValue) / 1e18).toLocaleString(undefined, {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </Text>
+              </View>
+              <View flexDirection="row" gap="$s3" justifyContent="center">
+                <Text subHeadline strikeThrough color="$uiNeutralSecondary">
+                  {(Number(duePayment.position) / 1e18).toLocaleString(undefined, {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </Text>
+                <Text
+                  pill
+                  caption2
+                  padding="$s2"
+                  backgroundColor="$interactiveBaseSuccessSoftDefault"
+                  color="$uiSuccessSecondary"
+                >
+                  {(Number(WAD - (duePayment.previewValue * WAD) / duePayment.position) / 1e18).toLocaleString(
+                    undefined,
+                    {
+                      style: "percent",
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    },
+                  )}{" "}
+                  OFF
+                </Text>
+              </View>
+              <View
+                flexDirection="row"
+                display="flex"
+                gap={ms(10)}
+                justifyContent="center"
+                alignItems="center"
+                paddingVertical={ms(10)}
+              >
+                <Button contained main spaced halfWidth iconAfter={<Coins color="$interactiveOnBaseBrandDefault" />}>
+                  Pay
+                </Button>
+                <Button
+                  outlined
+                  main
+                  spaced
+                  halfWidth
+                  iconAfter={
+                    <IterationCw
+                      color={rolloverSimulation ? "$interactiveOnBaseBrandSoft" : "$interactiveOnDisabled"}
+                    />
+                  }
+                  onPress={rollFixedToFloating}
+                  disabled={!rolloverSimulation}
+                >
+                  Rollover
+                </Button>
+              </View>
+              <Button outlined main spaced onPress={approveMarket} disabled={!approveSimulation}>
+                Approve
+              </Button>
+            </View>
+          )}
+        </>
+      ) : (
+        <View>
+          <Text textAlign="center" subHeadline color="$uiNeutralSecondary">
+            There are no fixed payments due.
           </Text>
         </View>
-        <View flexDirection="row" gap="$s3" justifyContent="center">
-          <Text subHeadline strikeThrough color="$uiNeutralSecondary">
-            {(Number(position) / 1e18).toLocaleString(undefined, {
-              style: "currency",
-              currency: "USD",
-            })}
-          </Text>
-          <Text
-            pill
-            caption2
-            padding="$s2"
-            backgroundColor="$interactiveBaseSuccessSoftDefault"
-            color="$uiSuccessSecondary"
-          >
-            {(Number(10n ** 18n - (previewValue * 10n ** 18n) / position) / 1e18).toLocaleString(undefined, {
-              style: "percent",
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}{" "}
-            OFF
-          </Text>
-        </View>
-        <View
-          flexDirection="row"
-          display="flex"
-          gap={ms(10)}
-          justifyContent="center"
-          alignItems="center"
-          paddingVertical={ms(10)}
-        >
-          <Button contained main spaced halfWidth iconAfter={<Coins color="$interactiveOnBaseBrandDefault" />}>
-            Pay
-          </Button>
-          <Button outlined main spaced halfWidth iconAfter={<IterationCw color="$interactiveOnBaseBrandSoft" />}>
-            Rollover
-          </Button>
-        </View>
-      </View>
+      )}
     </View>
   );
 }
