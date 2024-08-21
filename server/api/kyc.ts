b6fce8bca030632a7d790ea635630f23a694d446
guillermo dieguez <guillermodieguez@outlook.com>
2024-08-21 16:46:01 -0300

    ✨ server: add support for native kyc

--- server/api/kyc.ts
+++ server/api/kyc.ts
@@ -1,5 +1,7 @@
+import { vValidator } from "@hono/valibot-validator";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { object, optional, string } from "valibot";
 
 import database, { credentials } from "../database/index";
 import auth from "../middleware/auth";
@@ -22,21 +24,34 @@ app.get("/", async (c) => {
   return c.json(true);
 });
 
-app.post("/", async (c) => {
-  const credentialId = c.get("credentialId");
-  const credential = await database.query.credentials.findFirst({
-    columns: { id: true, kycId: true },
-    where: eq(credentials.id, credentialId),
-  });
-  if (!credential) return c.text("credential not found", 404);
-  let inquiryId = credential.kycId;
-  if (!inquiryId) {
-    const { data } = await createInquiry();
-    await database.update(credentials).set({ kycId: data.id }).where(eq(credentials.id, credentialId));
-    inquiryId = data.id;
-  }
-  const { meta } = await generateOTL(inquiryId);
-  return c.json(meta["one-time-link"]);
-});
+app.post(
+  "/",
+  vValidator("json", object({ inquiryId: optional(string()) }), ({ success }, c) => {
+    if (!success) return c.text("invalid body", 400);
+  }),
+  async (c) => {
+    const credentialId = c.get("credentialId");
+    const credential = await database.query.credentials.findFirst({
+      columns: { id: true, kycId: true },
+      where: eq(credentials.id, credentialId),
+    });
+    if (!credential) return c.text("credential not found", 404);
+    const { inquiryId } = c.req.valid("json");
+    if (!credential.kycId) {
+      const { kycId, result } = await (inquiryId
+        ? getInquiry(inquiryId).then(({ data }) => {
+            return { kycId: inquiryId, result: data.attributes.status === "approved" };
+          })
+        : createInquiry().then(async ({ data }) => {
+            const { meta } = await generateOTL(data.id);
+            return { kycId: data.id, result: meta["one-time-link"] };
+          }));
+      if (!kycId) return c.text("kyc not found", 404);
+      if (!result) return c.text("kyc not approved", 403);
+      await database.update(credentials).set({ kycId }).where(eq(credentials.id, credentialId));
+      return c.json(result);
+    }
+  },
+);
 
 export default app;
