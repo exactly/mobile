7bb3dca660fddf2e2430e8d0daf12fc41f94f6dc
danilo neves cruz <cruzdanilo@gmail.com>
2024-08-22 21:00:32 -0300

    ✨ server: handle account transfers

--- server/hooks/alchemy.ts
+++ server/hooks/alchemy.ts
@@ -1,11 +1,19 @@
-import { Address, Hash, Hex } from "@exactly/common/types";
+import chain, { auditorAbi, auditorAddress, marketAbi, wethAddress } from "@exactly/common/generated/chain";
+import { Address } from "@exactly/common/types";
 import { vValidator } from "@hono/valibot-validator";
 import { captureException, setContext } from "@sentry/node";
 import createDebug from "debug";
+import { inArray } from "drizzle-orm";
 import { Hono } from "hono";
 import { validator } from "hono/validator";
 import { createHmac } from "node:crypto";
 import * as v from "valibot";
+import { getAddress, isAddress } from "viem";
+import { optimism } from "viem/chains";
+
+import database, { credentials } from "../database";
+import publicClient from "../utils/publicClient";
+import redis from "../utils/redis";
 
 if (!process.env.ALCHEMY_SIGNING_KEY) throw new Error("missing alchemy signing key");
 const signingKey = process.env.ALCHEMY_SIGNING_KEY;
@@ -30,39 +38,27 @@ app.post(
   vValidator(
     "json",
     v.object({
-      webhookId: v.string(),
-      id: v.string(),
-      createdAt: v.pipe(v.string(), v.isoTimestamp()),
       type: v.literal("ADDRESS_ACTIVITY"),
       event: v.object({
-        network: v.string(),
+        network: v.literal(chain.id === optimism.id ? "OPT_MAINNET" : "OPT_SEPOLIA"),
         activity: v.array(
-          v.object({
-            blockNum: v.string(),
-            hash: v.string(),
-            fromAddress: Address,
-            toAddress: Address,
-            value: v.number(),
-            erc721TokenId: v.optional(Hex),
-            erc1155Metadata: v.optional(v.string()),
-            asset: v.string(),
-            category: v.string(),
-            rawContract: v.object({ rawValue: Hex, address: v.optional(Address), decimals: v.number() }),
-            typeTraceAddress: v.optional(v.string()),
-            log: v.optional(
+          v.intersect([
+            v.object({ fromAddress: Address, toAddress: Address }),
+            v.variant("category", [
               v.object({
-                address: Address,
-                topics: v.array(Hash),
-                data: Hex,
-                blockNumber: Hex,
-                transactionHash: Hash,
-                transactionIndex: Hex,
-                blockHash: Hash,
-                logIndex: Hex,
-                removed: v.boolean(),
+                category: v.picklist(["external", "internal"]),
+                asset: v.literal("ETH"),
+                rawContract: v.object({ address: v.undefined() }),
+                value: v.number(),
               }),
-            ),
-          }),
+              v.object({
+                category: v.picklist(["token", "erc20", "erc721", "erc1155"]),
+                asset: v.string(),
+                rawContract: v.object({ address: Address }),
+                value: v.optional(v.number()),
+              }),
+            ]),
+          ]),
         ),
       }),
     }),
@@ -75,8 +71,46 @@ app.post(
       if (debug.enabled) debug(JSON.stringify(result.output));
     },
   ),
-  (c) => {
-    c.req.valid("json");
+  async (c) => {
+    const { activity } = c.req.valid("json").event;
+    const transfers = activity.filter(
+      ({ category, value }) => category !== "erc721" && category !== "erc1155" && value,
+    );
+    const accounts = await database.query.credentials
+      .findMany({
+        columns: { account: true, publicKey: true },
+        where: inArray(credentials.account, [...new Set(transfers.map(({ toAddress }) => toAddress))]),
+      })
+      .then((result) =>
+        Object.fromEntries(result.map(({ account, publicKey }) => [getAddress(account), publicKey] as const)),
+      );
+    await Promise.all(
+      transfers.map(async ({ toAddress: account, rawContract }) => {
+        if (!accounts[account]) return;
+        const asset = rawContract.address ? getAddress(rawContract.address) : wethAddress;
+        await redis.hgetall(`${String(chain.id)}:${asset}`).then(async (found) => {
+          if (found.market && isAddress(found.market) && !Number.isNaN(Number(found.index))) {
+            return { address: found.market, index: Number(found.index) };
+          }
+          const markets = await publicClient.readContract({
+            address: auditorAddress,
+            functionName: "allMarkets",
+            abi: auditorAbi,
+          });
+          return Object.fromEntries(
+            await Promise.all(
+              markets.map(async (address, index) => {
+                const underlying = await publicClient.readContract({ address, functionName: "asset", abi: marketAbi });
+                redis
+                  .hset(`${String(chain.id)}:${underlying}`, { market: address, index })
+                  .catch((error: unknown) => captureException(error));
+                return [underlying, { address, index }] as const;
+              }),
+            ),
+          )[asset];
+        });
+      }),
+    );
     return c.json({});
   },
 );
