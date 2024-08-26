e180270b65495d89319c37209c85a3deddf3950f
guillermo dieguez <guillermodieguez@outlook.com>
2024-08-26 09:49:27 -0300

    🎨 app: export symbol to icon map as util

--- src/components/home/AssetList.tsx
+++ src/components/home/AssetList.tsx
@@ -7,17 +7,8 @@ import { zeroAddress } from "viem";
 import { useAccount } from "wagmi";
 
 import { useReadPreviewerExactly } from "../../generated/contracts";
+import assetLogos from "../../utils/assetLogos";
 import Text from "../shared/Text";
-
-const symbolToIcon: Record<string, string> = {
-  ETH: "https://assets.smold.app/api/token/1/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/logo.svg",
-  OP: "https://assets.smold.app/api/token/10/0x4200000000000000000000000000000000000042/logo.svg",
-  DAI: "https://assets.smold.app/api/token/10/0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1/logo.svg",
-  WBTC: "https://assets.smold.app/api/token/10/0x68f180fcCe6836688e9084f035309E29Bf0A2095/logo.svg",
-  wstETH: "https://assets.smold.app/api/token/10/0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb/logo.svg",
-  USDC: "https://assets.smold.app/api/token/10/0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85/logo.svg",
-  "USDC.e": "https://assets.smold.app/api/token/10/0x7F5c764cBc14f9669B88837ca1490cCa17c31607/logo.svg",
-};
 
 export default function AssetList() {
   const { address } = useAccount();
@@ -49,7 +40,7 @@ export default function AssetList() {
             <View flexDirection="row" gap={ms(10)} flex={1} alignItems="center">
               <Image
                 source={{
-                  uri: symbolToIcon[symbol],
+                  uri: assetLogos[symbol as keyof typeof assetLogos],
                 }}
                 width={ms(32)}
                 height={ms(32)}
