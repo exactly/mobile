0647b8e4acf5992216f3568c14f3f361f89212bc
guillermo dieguez <guillermodieguez@outlook.com>
2024-08-27 17:24:26 -0300

    ✨ app: implement plugin upgrade

--- src/components/settings/ContractUtils.tsx
+++ src/components/settings/ContractUtils.tsx
@@ -1,18 +1,27 @@
-import { auditorAddress, marketUSDCAddress, previewerAddress, usdcAddress } from "@exactly/common/generated/chain";
+import {
+  exaPluginAbi,
+  exaPluginAddress,
+  marketUSDCAddress,
+  previewerAddress,
+  usdcAddress,
+} from "@exactly/common/generated/chain";
+import { useMutation } from "@tanstack/react-query";
 import { setStringAsync } from "expo-clipboard";
 import React, { useCallback, useState } from "react";
 import { ms } from "react-native-size-matters";
 import { View, Spinner } from "tamagui";
-import { maxUint256, zeroAddress } from "viem";
-import { useAccount, useBalance, useWriteContract } from "wagmi";
+import { encodeAbiParameters, encodeFunctionData, getAbiItem, keccak256, maxUint256, zeroAddress } from "viem";
+import { useAccount, useWriteContract } from "wagmi";
 
 import {
+  upgradeableModularAccountAbi,
+  useReadExaPluginPluginManifest,
+  useReadIAccountLoupeGetInstalledPlugins,
   useReadPreviewerExactly,
-  useSimulateAuditorEnterMarket,
-  useSimulateMarketApprove,
   useSimulateMarketBorrowAtMaturity,
-  useSimulateMarketDeposit,
+  useSimulateUpgradeableModularAccountUninstallPlugin,
 } from "../../generated/contracts";
+import { accountClient } from "../../utils/alchemyConnector";
 import handleError from "../../utils/handleError";
 import Button from "../shared/Button";
 import Input from "../shared/Input";
@@ -26,11 +35,17 @@ function copyHash(hash: string | undefined) {
 export default function ContractUtils() {
   const [borrowAmount, setBorrowAmount] = useState<number>(0);
   const { address } = useAccount();
-  const { data: balanceUSDC } = useBalance({ token: usdcAddress, address });
+
   const { data: markets } = useReadPreviewerExactly({
     address: previewerAddress,
     account: address,
     args: [address ?? zeroAddress],
+  });
+  const { data: installedPlugins } = useReadIAccountLoupeGetInstalledPlugins({
+    address: address ?? zeroAddress,
+  });
+  const { data: pluginManifest } = useReadExaPluginPluginManifest({
+    address: exaPluginAddress,
   });
 
   const marketUSDC = markets?.find((market) => market.asset === usdcAddress);
@@ -38,112 +53,77 @@ export default function ContractUtils() {
   const firstMaturity = marketUSDC?.fixedPools[0]?.maturity ?? 0n;
   const borrowAssets = BigInt(borrowAmount * 10 ** (marketUSDC?.decimals ?? 6));
 
-  const { data: approveUSDCSimulation } = useSimulateMarketApprove({
-    address: usdcAddress,
-    args: [marketUSDCAddress, balanceUSDC?.value ?? 0n],
-    query: { enabled: !!address && !!balanceUSDC && balanceUSDC.value > 0n },
-  });
-  const { data: enterUSDCSimulation } = useSimulateAuditorEnterMarket({
-    address: auditorAddress,
-    args: [marketUSDCAddress],
-    query: { enabled: !!address },
-  });
-  const { data: depositUSDCSimulation } = useSimulateMarketDeposit({
-    address: marketUSDCAddress,
-    args: [balanceUSDC?.value ?? 0n, address ?? zeroAddress],
-    query: { enabled: !!address && !!balanceUSDC && balanceUSDC.value > 0n },
-  });
   const { data: borrowUSDCSimulation } = useSimulateMarketBorrowAtMaturity({
     address: marketUSDCAddress,
     args: [firstMaturity, borrowAssets, maxUint256, address ?? zeroAddress, address ?? zeroAddress],
     query: { enabled: !!address && !!floatingUSDCDeposit && floatingUSDCDeposit > 0n },
   });
+  const { data: uninstallPluginSimulation } = useSimulateUpgradeableModularAccountUninstallPlugin({
+    address,
+    args: [installedPlugins?.[0] ?? zeroAddress, "0x", "0x"],
+    query: { enabled: !!address && !!installedPlugins },
+  });
 
-  const { writeContract: approve, data: approveHash, isPending: isApproving, error: approveError } = useWriteContract();
-  const {
-    writeContract: enter,
-    data: enterMarketHash,
-    isPending: isSending,
-    error: enterMarketError,
-  } = useWriteContract();
-  const {
-    writeContract: deposit,
-    data: depositHash,
-    isPending: isDepositing,
-    error: depositError,
-  } = useWriteContract();
   const { writeContract: borrow, data: borrowHash, isPending: isBorrowing, error: borrowError } = useWriteContract();
 
-  const approveUSDC = useCallback(() => {
-    if (!approveUSDCSimulation) throw new Error("no approve simulation");
-    approve(approveUSDCSimulation.request);
-  }, [approve, approveUSDCSimulation]);
-  const enterUSDC = useCallback(() => {
-    if (!enterUSDCSimulation) throw new Error("no enter market simulation");
-    enter(enterUSDCSimulation.request);
-  }, [enterUSDCSimulation, enter]);
-  const depositUSDC = useCallback(() => {
-    if (!depositUSDCSimulation) throw new Error("no deposit simulation");
-    deposit(depositUSDCSimulation.request);
-  }, [deposit, depositUSDCSimulation]);
   const borrowUSDC = useCallback(() => {
     if (!borrowUSDCSimulation) throw new Error("no borrow simulation");
     borrow(borrowUSDCSimulation.request);
   }, [borrow, borrowUSDCSimulation]);
+
+  const {
+    data: updatePluginHash,
+    mutateAsync: upgradePlugin,
+    isPending,
+  } = useMutation({
+    mutationFn: async () => {
+      if (!accountClient) throw new Error("no account client");
+      if (!address) throw new Error("no account address");
+      if (!installedPlugins?.[0]) throw new Error("no installed plugin");
+      if (!uninstallPluginSimulation) throw new Error("no uninstall plugin simulation");
+      if (!pluginManifest) throw new Error("invalid manifest");
+      const hash = await accountClient.sendUserOperation({
+        uo: [
+          { target: address, value: 0n, data: encodeFunctionData(uninstallPluginSimulation.request) },
+          {
+            target: address,
+            value: 0n,
+            data: encodeFunctionData({
+              abi: upgradeableModularAccountAbi,
+              functionName: "installPlugin",
+              args: [
+                exaPluginAddress,
+                keccak256(
+                  encodeAbiParameters(getAbiItem({ abi: exaPluginAbi, name: "pluginManifest" }).outputs, [
+                    pluginManifest,
+                  ]),
+                ),
+                "0x",
+                [],
+              ],
+            }),
+          },
+        ],
+      });
+      return accountClient.waitForUserOperationTransaction(hash);
+    },
+  });
 
   return (
     <View gap="$s4">
       <Text fontSize={ms(16)} subHeadline fontWeight="bold">
         Exactly
       </Text>
-
-      {(isSending || isDepositing || isApproving || isBorrowing) && <Spinner color="$interactiveBaseBrandDefault" />}
-
-      {(enterMarketError ?? depositError ?? approveError ?? borrowError) && (
+      {(isBorrowing || isPending) && <Spinner color="$interactiveBaseBrandDefault" />}
+      {borrowError && (
         <Text color="$uiErrorPrimary" fontWeight="bold">
-          {enterMarketError
-            ? enterMarketError.message
-            : depositError
-              ? depositError.message
-              : approveError
-                ? approveError.message
-                : borrowError
-                  ? borrowError.message
-                  : "Error"}
+          {borrowError.message}
         </Text>
       )}
-
-      {approveHash && (
+      {updatePluginHash && (
         <View borderRadius="$r4" borderWidth={2} borderColor="$borderNeutralSoft" padding={ms(10)}>
           <Text textAlign="center" fontSize={ms(14)} fontFamily="$mono" width="100%" fontWeight="bold">
-            {approveHash}
-          </Text>
-        </View>
-      )}
-
-      <View flexDirection="row" gap="$s4">
-        <Button contained onPress={approveUSDC} disabled={!approveUSDCSimulation} padding={ms(10)} flex={1}>
-          Approve USDC
-        </Button>
-        {approveHash && (
-          <Button
-            outlined
-            borderRadius="$r2"
-            onPress={() => {
-              copyHash(approveHash);
-            }}
-            padding={ms(10)}
-            flex={1}
-          >
-            Copy
-          </Button>
-        )}
-      </View>
-
-      {enterMarketHash && (
-        <View borderRadius="$r4" borderWidth={2} borderColor="$borderNeutralSoft" padding={ms(10)}>
-          <Text textAlign="center" fontSize={ms(14)} fontFamily="$mono" width="100%" fontWeight="bold">
-            {enterMarketHash}
+            {updatePluginHash}
           </Text>
         </View>
       )}
@@ -151,19 +131,21 @@ export default function ContractUtils() {
       <View flexDirection="row" gap="$s4">
         <Button
           contained
-          onPress={enterUSDC}
-          disabled={balanceUSDC?.value === 0n || !enterUSDCSimulation}
+          onPress={() => {
+            upgradePlugin().catch(handleError);
+          }}
           padding={ms(10)}
+          disabled={!uninstallPluginSimulation || exaPluginAddress === installedPlugins?.[0]}
           flex={1}
         >
-          Enter USDC market
+          Upgrade Plugin
         </Button>
-        {enterMarketHash && (
+        {updatePluginHash && (
           <Button
             outlined
             borderRadius="$r2"
             onPress={() => {
-              copyHash(enterMarketHash);
+              copyHash(updatePluginHash);
             }}
             padding={ms(10)}
             flex={1}
@@ -172,40 +154,6 @@ export default function ContractUtils() {
           </Button>
         )}
       </View>
-
-      {depositHash && (
-        <View borderRadius="$r4" borderWidth={2} borderColor="$borderNeutralSoft" padding={ms(10)}>
-          <Text textAlign="center" fontSize={ms(14)} fontFamily="$mono" width="100%" fontWeight="bold">
-            {depositHash}
-          </Text>
-        </View>
-      )}
-
-      <View flexDirection="row" gap="$s4">
-        <Button
-          contained
-          onPress={depositUSDC}
-          disabled={balanceUSDC?.value === 0n || !depositUSDCSimulation}
-          padding={ms(10)}
-          flex={1}
-        >
-          Deposit USDC
-        </Button>
-        {depositHash && (
-          <Button
-            outlined
-            borderRadius="$r2"
-            onPress={() => {
-              copyHash(depositHash);
-            }}
-            padding={ms(10)}
-            flex={1}
-          >
-            Copy
-          </Button>
-        )}
-      </View>
-
       <View gap="$s4">
         <Text fontSize={ms(16)}>Borrow</Text>
         <Input
