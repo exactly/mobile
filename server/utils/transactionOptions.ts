e5bbd04758b450fb98db7759885e7eac518867cb
danilo neves cruz <cruzdanilo@gmail.com>
2024-09-03 18:22:49 -0300

    🔧 server: tweak tx gas options

--- server/utils/transactionOptions.ts
+++ server/utils/transactionOptions.ts
@@ -3,7 +3,7 @@ import chain from "@exactly/common/generated/chain";
 export default {
   chainId: chain.id,
   type: "eip1559",
-  maxFeePerGas: 1_000_000n,
+  maxFeePerGas: 1_000_000_000n,
   maxPriorityFeePerGas: 1_000_000n,
-  gas: 2_000_000n,
+  gas: 5_000_000n,
 } as const;
