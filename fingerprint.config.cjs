14824026bbe8703ae54486f4f6a3913e5d12c433
danilo neves cruz <cruzdanilo@gmail.com>
2024-09-12 11:51:23 -0300

    🔧 fingerprint: setup source skips and ignore

---
+++ fingerprint.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('expo-updates/fingerprint').Config} */
+module.exports = {
+  sourceSkips: [
+    "ExpoConfigAndroidPackage",
+    "ExpoConfigAssets",
+    "ExpoConfigIosBundleIdentifier",
+    "ExpoConfigNames",
+    "ExpoConfigRuntimeVersionIfString",
+    "ExpoConfigSchemes",
+    "ExpoConfigVersions",
+    "PackageJsonAndroidAndIosScriptsIfNotContainRun",
+  ],
+  ignorePaths: [".gitignore", "package.json", "patches/eslint-*"],
+};
