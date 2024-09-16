f60eff2fa6cf27f8bae60a63d88f1a2cda878058
guillermo dieguez <guillermodieguez@outlook.com>
2024-09-16 11:13:21 -0300

    ✨ app: add pull to refresh to activity

--- src/components/activity/Activity.tsx
+++ src/components/activity/Activity.tsx
@@ -1,19 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import React, { useMemo } from "react";
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import { ms } from "react-native-size-matters";
-import { Spinner } from "tamagui";
 
 import ActivityItem from "./ActivityItem";
+import handleError from "../../utils/handleError";
 import { getActivity } from "../../utils/server";
 import SafeView from "../shared/SafeView";
 import Text from "../shared/Text";
 import View from "../shared/View";
 
 export default function Activity() {
-  const { data: activity, isLoading } = useQuery({ queryKey: ["activity"], queryFn: getActivity });
-
+  const { data: activity, refetch, isFetching } = useQuery({ queryKey: ["activity"], queryFn: getActivity });
   const groupedActivity = useMemo(() => {
     if (!activity) return [];
     const groups: Record<string, typeof activity> = {};
@@ -24,7 +23,6 @@ export default function Activity() {
     }
     return Object.entries(groups).map(([date, purchases]) => ({ date, purchases }));
   }, [activity]);
-
   return (
     <SafeView fullScreen tab>
       <View fullScreen>
@@ -35,50 +33,52 @@ export default function Activity() {
             </Text>
           </View>
         </View>
-        <View gap="$s5" flex={1}>
-          {isLoading ? (
-            <View margin="$s5" justifyContent="center" alignItems="center">
-              <Spinner color="$interactiveBaseBrandDefault" />
-            </View>
-          ) : (
-            <FlatList
-              ListEmptyComponent={
-                <View
-                  margin="$s5"
-                  borderRadius="$r3"
-                  backgroundColor="$uiNeutralTertiary"
-                  padding="$s3_5"
-                  alignSelf="center"
-                >
-                  <Text textAlign="center" subHeadline color="$uiNeutralSecondary">
-                    There is no activity yet.
-                  </Text>
-                </View>
-              }
-              data={groupedActivity}
-              renderItem={({ item }) => {
-                const { date, purchases } = item;
-                return (
-                  <View key={date}>
-                    <View paddingHorizontal="$s5" paddingVertical="$s3" backgroundColor="$backgroundSoft">
-                      <Text subHeadline color="$uiNeutralSecondary">
-                        {date}
-                      </Text>
-                    </View>
-                    {purchases.map((purchase, index) => (
-                      <ActivityItem
-                        key={purchase.id}
-                        item={purchase}
-                        isFirst={index === 0}
-                        isLast={index === purchases.length - 1}
-                      />
-                    ))}
+        <View gap="$s5" height="100%">
+          <FlatList
+            refreshControl={
+              <RefreshControl
+                refreshing={isFetching}
+                onRefresh={() => {
+                  refetch().catch(handleError);
+                }}
+              />
+            }
+            ListEmptyComponent={
+              <View
+                margin="$s5"
+                borderRadius="$r3"
+                backgroundColor="$uiNeutralTertiary"
+                padding="$s3_5"
+                alignSelf="center"
+              >
+                <Text textAlign="center" subHeadline color="$uiNeutralSecondary">
+                  There is no activity yet.
+                </Text>
+              </View>
+            }
+            data={groupedActivity}
+            renderItem={({ item }) => {
+              const { date, purchases } = item;
+              return (
+                <View key={date}>
+                  <View paddingHorizontal="$s5" paddingVertical="$s3" backgroundColor="$backgroundSoft">
+                    <Text subHeadline color="$uiNeutralSecondary">
+                      {date}
+                    </Text>
                   </View>
-                );
-              }}
-              keyExtractor={(item) => item.date}
-            />
-          )}
+                  {purchases.map((purchase, index) => (
+                    <ActivityItem
+                      key={purchase.id}
+                      item={purchase}
+                      isFirst={index === 0}
+                      isLast={index === purchases.length - 1}
+                    />
+                  ))}
+                </View>
+              );
+            }}
+            keyExtractor={(item) => item.date}
+          />
         </View>
       </View>
     </SafeView>
