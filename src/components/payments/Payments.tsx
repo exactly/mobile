83af21f262485f4f76041f419f39a7b080350ee0
guillermo dieguez <guillermodieguez@outlook.com>
2024-09-17 11:48:32 -0300

    ✨ app: add pull to refresh to payments

--- src/components/payments/Payments.tsx
+++ src/components/payments/Payments.tsx
@@ -1,17 +1,25 @@
-import { marketUSDCAddress } from "@exactly/common/generated/chain";
+import { marketUSDCAddress, previewerAddress } from "@exactly/common/generated/chain";
 import React from "react";
+import { RefreshControl } from "react-native";
 import { ms } from "react-native-size-matters";
 import { ScrollView } from "tamagui";
+import { zeroAddress } from "viem";
 
 import NextPayment from "./NextPayment";
 import UpcomingPayments from "./UpcomingPayments";
+import { useReadPreviewerExactly } from "../../generated/contracts";
+import handleError from "../../utils/handleError";
 import useMarketAccount from "../../utils/useMarketAccount";
 import SafeView from "../shared/SafeView";
 import Text from "../shared/Text";
 import View from "../shared/View";
 
 export default function Payments() {
-  const { market } = useMarketAccount(marketUSDCAddress);
+  const { market, account } = useMarketAccount(marketUSDCAddress);
+  const { refetch, isFetching } = useReadPreviewerExactly({
+    address: previewerAddress,
+    args: [account ?? zeroAddress],
+  });
   let usdDue = 0n;
   if (market) {
     for (const { position } of market.fixedBorrowPositions.filter(({ previewValue }) => previewValue !== 0n)) {
@@ -20,7 +28,17 @@ export default function Payments() {
   }
   return (
     <SafeView fullScreen tab>
-      <ScrollView flex={1}>
+      <ScrollView
+        flex={1}
+        refreshControl={
+          <RefreshControl
+            refreshing={isFetching}
+            onRefresh={() => {
+              refetch().catch(handleError);
+            }}
+          />
+        }
+      >
         <View padded gap="$s5" backgroundColor="$backgroundSoft">
           <View flexDirection="row" gap={ms(10)} justifyContent="space-between" alignItems="center">
             <Text fontSize={ms(20)} fontWeight="bold">
