8eafa4ecb4dfa054b85a7eb129876d89405508b5
guillermo dieguez <guillermodieguez@outlook.com>
2024-09-20 17:46:40 -0300

    🎨 app: change supported assets ordering

--- src/utils/assetLogos.ts
+++ src/utils/assetLogos.ts
@@ -1,9 +1,9 @@
 export default {
-  ETH: "https://assets.smold.app/api/token/1/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/logo.svg",
-  OP: "https://assets.smold.app/api/token/10/0x4200000000000000000000000000000000000042/logo.svg",
-  DAI: "https://assets.smold.app/api/token/10/0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1/logo.svg",
-  WBTC: "https://assets.smold.app/api/token/10/0x68f180fcCe6836688e9084f035309E29Bf0A2095/logo.svg",
-  wstETH: "https://assets.smold.app/api/token/10/0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb/logo.svg",
   USDC: "https://assets.smold.app/api/token/10/0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85/logo.svg",
+  ETH: "https://assets.smold.app/api/token/1/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/logo.svg",
+  wstETH: "https://assets.smold.app/api/token/10/0x1F32b1c2345538c0c6f582fCB022739c4A194Ebb/logo.svg",
+  OP: "https://assets.smold.app/api/token/10/0x4200000000000000000000000000000000000042/logo.svg",
+  WBTC: "https://assets.smold.app/api/token/10/0x68f180fcCe6836688e9084f035309E29Bf0A2095/logo.svg",
+  DAI: "https://assets.smold.app/api/token/10/0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1/logo.svg",
   "USDC.e": "https://assets.smold.app/api/token/10/0x7F5c764cBc14f9669B88837ca1490cCa17c31607/logo.svg",
 } as const;
