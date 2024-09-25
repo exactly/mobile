455c95cc864c9e5e09ab7342c1bafe4d941aa9c1
danilo neves cruz <cruzdanilo@gmail.com>
2024-09-25 01:41:48 -0300

    ✅ server: group authenticated tests

--- server/test/api/activity.test.ts
+++ server/test/api/activity.test.ts
@@ -3,6 +3,7 @@ import "../mockDatabase";
 import "../mockDeployments";
 import "../mockSentry";
 
+import { Address } from "@exactly/common/types";
 import * as sentry from "@sentry/node";
 import { testClient } from "hono/testing";
 import { parse, type InferInput } from "valibot";
@@ -10,7 +11,7 @@ import { zeroAddress } from "viem";
 import { generatePrivateKey, privateKeyToAddress } from "viem/accounts";
 import { beforeAll, describe, expect, it, vi } from "vitest";
 
-import app, { CardTransaction } from "../../api/activity";
+import app, { CardActivity } from "../../api/activity";
 import database, { cards, credentials, transactions } from "../../database";
 
 const appClient = testClient(app);
@@ -46,50 +47,56 @@ describe("validation", () => {
   });
 });
 
-describe("card", () => {
-  const account = privateKeyToAddress(generatePrivateKey());
-  const payload: InferInput<typeof CardTransaction> = {
-    operation_id: "0",
-    data: {
-      created_at: new Date().toISOString(),
-      bill_amount: 100 / 3,
-      transaction_amount: (1111.11 * 100) / 3,
-      transaction_currency_code: "ARS",
-      merchant_data: { name: "Merchant", country: "ARG", city: "Buenos Aires", state: "CABA" },
-    },
-  };
+describe("authenticated", () => {
+  const account = parse(Address, privateKeyToAddress(generatePrivateKey()));
 
   beforeAll(async () => {
     await database
       .insert(credentials)
       .values([{ id: account, publicKey: new Uint8Array(), account, factory: zeroAddress }]);
-    await database.insert(cards).values([{ id: "card", credentialId: account, lastFour: "1234" }]);
-    await database.insert(transactions).values([{ id: "0", cardId: "card", hash: "0x0", payload }]);
   });
 
-  it("returns the card transaction", async () => {
-    const response = await appClient.index.$get(
-      { query: { include: "card" } },
-      { headers: { "test-credential-id": account } },
-    );
+  describe("card", () => {
+    const payload: InferInput<typeof CardActivity> = {
+      operation_id: "0",
+      data: {
+        created_at: new Date().toISOString(),
+        bill_amount: 100 / 3,
+        transaction_amount: (1111.11 * 100) / 3,
+        transaction_currency_code: "ARS",
+        merchant_data: { name: "Merchant", country: "ARG", city: "Buenos Aires", state: "CABA" },
+      },
+    };
 
-    expect(response.status).toBe(200);
-    await expect(response.json()).resolves.toStrictEqual([parse(CardTransaction, payload)]);
-  });
+    beforeAll(async () => {
+      await database.insert(cards).values([{ id: "card", credentialId: account, lastFour: "1234" }]);
+      await database.insert(transactions).values([{ id: "0", cardId: "card", hash: "0x0", payload }]);
+    });
 
-  it("reports bad transaction", async () => {
-    await database.insert(transactions).values([{ id: "1", cardId: "card", hash: "0x1", payload: {} }]);
+    it("returns the card transaction", async () => {
+      const response = await appClient.index.$get(
+        { query: { include: "card" } },
+        { headers: { "test-credential-id": account } },
+      );
 
-    const captureException = vi.spyOn(sentry, "captureException").mockImplementationOnce(() => "");
+      expect(response.status).toBe(200);
+      await expect(response.json()).resolves.toStrictEqual([parse(CardActivity, payload)]);
+    });
 
-    const response = await appClient.index.$get(
-      { query: { include: "card" } },
-      { headers: { "test-credential-id": account } },
-    );
+    it("reports bad transaction", async () => {
+      await database.insert(transactions).values([{ id: "1", cardId: "card", hash: "0x1", payload: {} }]);
 
-    expect(captureException).toHaveBeenCalledOnce();
-    expect(captureException).toHaveBeenCalledWith(new Error("bad transaction"));
-    expect(response.status).toBe(200);
-    await expect(response.json()).resolves.toStrictEqual([parse(CardTransaction, payload)]);
+      const captureException = vi.spyOn(sentry, "captureException").mockImplementationOnce(() => "");
+
+      const response = await appClient.index.$get(
+        { query: { include: "card" } },
+        { headers: { "test-credential-id": account } },
+      );
+
+      expect(captureException).toHaveBeenCalledOnce();
+      expect(captureException).toHaveBeenCalledWith(new Error("bad transaction"));
+      expect(response.status).toBe(200);
+      await expect(response.json()).resolves.toStrictEqual([parse(CardActivity, payload)]);
+    });
   });
 });
