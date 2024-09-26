a963bdef8dc9a0c52c7472952feac167fb8327d4
guillermo dieguez <guillermodieguez@outlook.com>
2024-09-26 14:32:50 -0300

    💄 app: display daily penalties

--- src/components/payments/NextPayment.tsx
+++ src/components/payments/NextPayment.tsx
@@ -1,7 +1,7 @@
 import { marketUSDCAddress } from "@exactly/common/generated/chain";
 import { Coins } from "@tamagui/lucide-icons";
 import { useQuery } from "@tanstack/react-query";
-import { formatDistanceToNow, intlFormat } from "date-fns";
+import { formatDistance, intlFormat, isAfter } from "date-fns";
 import { router } from "expo-router";
 import React from "react";
 import { ms } from "react-native-size-matters";
@@ -26,18 +26,25 @@ export default function NextPayment() {
       usdDue.set(maturity, { previewValue: preview, position: positionValue });
     }
   }
-
   const maturity = usdDue.keys().next().value;
   const duePayment = usdDue.get(maturity ?? 0n);
-
+  const discount = duePayment ? Number(WAD - (duePayment.previewValue * WAD) / duePayment.position) / 1e18 : 0;
   return (
     <View backgroundColor="$backgroundSoft" borderRadius="$r3" padding="$s4" gap="$s5">
       {maturity ? (
         <>
           <View>
             <View flexDirection="row" alignItems="center" justifyContent="space-between">
-              <Text emphasized headline>
-                Due in {formatDistanceToNow(Number(maturity) * 1000)}
+              <Text
+                emphasized
+                headline
+                color={
+                  isAfter(new Date(Number(maturity) * 1000), new Date()) ? "$uiNeutralPrimary" : "$uiErrorSecondary"
+                }
+              >
+                {isAfter(new Date(Number(maturity) * 1000), new Date())
+                  ? `Due in ${formatDistance(new Date(), new Date(Number(maturity) * 1000))}`
+                  : `${formatDistance(new Date(Number(maturity) * 1000), new Date())} past due`}
               </Text>
             </View>
             <Text footnote color="$uiNeutralSecondary">
@@ -48,31 +55,35 @@ export default function NextPayment() {
             <View gap="$s5">
               <View flexDirection="column" justifyContent="center" alignItems="center" gap="$s4">
                 {!hidden && (
-                  <Text
-                    pill
-                    caption2
-                    padding="$s2"
-                    backgroundColor="$interactiveBaseSuccessSoftDefault"
-                    color="$uiSuccessSecondary"
-                  >
-                    {`PAY NOW AND SAVE `}
-                    {(Number(WAD - (duePayment.previewValue * WAD) / duePayment.position) / 1e18).toLocaleString(
-                      undefined,
-                      {
+                  <>
+                    <Text
+                      pill
+                      caption2
+                      padding="$s2"
+                      backgroundColor={
+                        discount > 0 ? "$interactiveBaseSuccessSoftDefault" : "$interactiveBaseErrorSoftDefault"
+                      }
+                      color={discount > 0 ? "$uiSuccessSecondary" : "$uiErrorSecondary"}
+                    >
+                      {discount > 0 ? "PAY NOW AND SAVE " : "DAILY PENALTIES "}
+                      {(discount > 0 ? discount : discount * -1).toLocaleString(undefined, {
                         style: "percent",
                         minimumFractionDigits: 2,
                         maximumFractionDigits: 2,
-                      },
-                    )}
+                      })}
+                    </Text>
+                  </>
+                )}
+
+                {discount > 0 && (
+                  <Text sensitive body strikeThrough color="$uiNeutralSecondary">
+                    {(Number(duePayment.position) / 1e18).toLocaleString(undefined, {
+                      style: "currency",
+                      currency: "USD",
+                      currencyDisplay: "narrowSymbol",
+                    })}
                   </Text>
                 )}
-                <Text sensitive body strikeThrough color="$uiNeutralSecondary">
-                  {(Number(duePayment.position) / 1e18).toLocaleString(undefined, {
-                    style: "currency",
-                    currency: "USD",
-                    currencyDisplay: "narrowSymbol",
-                  })}
-                </Text>
                 <Text
                   sensitive
                   textAlign="center"
@@ -80,6 +91,9 @@ export default function NextPayment() {
                   fontSize={ms(40)}
                   fontWeight="bold"
                   overflow="hidden"
+                  color={
+                    isAfter(new Date(Number(maturity) * 1000), new Date()) ? "$uiNeutralPrimary" : "$uiErrorSecondary"
+                  }
                 >
                   {(Number(duePayment.previewValue) / 1e18).toLocaleString(undefined, {
                     style: "currency",
