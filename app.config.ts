af50ed491563f5868e11ef92a900b326055ef978
danilo neves cruz <cruzdanilo@gmail.com>
2024-10-02 12:52:57 -0300

    🔨 codegen: move release to common

--- app.config.ts
+++ app.config.ts
@@ -8,8 +8,8 @@ import type withCamera from "expo-camera/plugin/build/withCamera";
 import type { FontProps } from "expo-font/plugin/build/withFonts";
 import type { OneSignalPluginProps } from "onesignal-expo-plugin/types/types";
 
+import release from "./common/generated/release.js";
 import metadata from "./package.json";
-import release from "./src/generated/release.js";
 import versionCode from "./src/generated/versionCode.js";
 
 const { Mode } =
