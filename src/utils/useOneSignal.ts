40198691a449e0126b82bf0959f330e28b45225d
guillermo dieguez <guillermodieguez@outlook.com>
2024-10-02 19:54:51 -0300

    🐛 app: fix onesignal usage during ssr
    co-authored-by: danilo neves cruz <cruzdanilo@gmail.com>

--- src/utils/useOneSignal.ts
+++ src/utils/useOneSignal.ts
@@ -1,19 +1,20 @@
 import { useState, useEffect } from "react";
 import { Platform } from "react-native";
-import type OneSignalNative from "react-native-onesignal";
-import type OneSignalWeb from "react-onesignal";
+import type * as OneSignalNative from "react-native-onesignal";
+import type * as OneSignalWeb from "react-onesignal";
 
 import handleError from "./handleError";
 
 const appId = process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID;
 
 const { initialization, login, logout } = (
-  Platform.OS === "web"
+  Platform.OS === "web" && globalThis.window // eslint-disable-line @typescript-eslint/no-unnecessary-condition
     ? () => {
-        const OneSignal = (require("react-onesignal") as { default: typeof OneSignalWeb }).default; // eslint-disable-line @typescript-eslint/no-require-imports, unicorn/prefer-module
+        const { default: OneSignal } = require("react-onesignal") as typeof OneSignalWeb; // eslint-disable-line @typescript-eslint/no-require-imports, unicorn/prefer-module
         return {
-          initialization:
-            appId === undefined ? Promise.resolve() : OneSignal.init({ appId, allowLocalhostAsSecureOrigin: __DEV__ }),
+          initialization: appId
+            ? OneSignal.init({ appId, allowLocalhostAsSecureOrigin: __DEV__ }).catch(handleError)
+            : Promise.resolve(),
           login: (userId: string) => OneSignal.login(userId),
           logout: () => OneSignal.logout(),
         };
@@ -22,7 +23,7 @@ const { initialization, login, logout } = (
         const { OneSignal } = require("react-native-onesignal") as typeof OneSignalNative; // eslint-disable-line @typescript-eslint/no-require-imports, unicorn/prefer-module
         return {
           initialization: (() => {
-            if (appId !== undefined) OneSignal.initialize(appId);
+            if (appId) OneSignal.initialize(appId);
             return Promise.resolve();
           })(),
           login: (userId: string) => {
@@ -39,20 +40,16 @@ const { initialization, login, logout } = (
 
 export default function useOneSignal({ userId }: { userId?: string } = {}) {
   const [initialized, setInitialized] = useState(false);
-
   useEffect(() => {
     initialization
       .then(() => {
         setInitialized(true);
       })
       .catch(handleError);
-
     if (userId && initialized) login(userId).catch(handleError);
-
     return () => {
       if (userId) logout().catch(handleError);
     };
   }, [userId, initialized]);
-
   return initialized;
 }
