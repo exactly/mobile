480318ef9e0c668f5ad6c35d50d1d137d695a420
guillermo dieguez <guillermodieguez@outlook.com>
2024-10-09 15:34:19 -0300

    🧑‍💻 app: improve handle error interface

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -2,5 +2,5 @@ import { captureException } from "@sentry/react-native";
 
 export default function handleError(error: unknown) {
   console.error(error); // eslint-disable-line no-console
-  captureException(error);
+  return captureException(error);
 }
