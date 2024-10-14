0b36e0d027c98d6bae45f0a781845db2f18d1a6b
guillermo dieguez <guillermodieguez@outlook.com>
2024-10-14 15:09:45 -0300

    💄 app: improve camera permissions interface

--- src/components/send-funds/Qr.tsx
+++ src/components/send-funds/Qr.tsx
@@ -2,29 +2,94 @@ import { Address } from "@exactly/common/validation";
 import { ArrowLeft, BoxSelect, SwitchCamera } from "@tamagui/lucide-icons";
 import { CameraView, useCameraPermissions } from "expo-camera";
 import { router } from "expo-router";
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+import React, { useRef, useState } from "react";
+import { Linking, StyleSheet } from "react-native";
 import { ms } from "react-native-size-matters";
-import { useWindowDimensions } from "tamagui";
+import { useWindowDimensions, XStack } from "tamagui";
 import { safeParse } from "valibot";
 
 import handleError from "../../utils/handleError";
 import Button from "../shared/Button";
+import Text from "../shared/Text";
 import View from "../shared/View";
 
 export default function Qr() {
+  const cameraReference = useRef<CameraView>(null);
+  const { height, width } = useWindowDimensions();
   const [cameraFacing, setCameraFacing] = useState<"front" | "back">("back");
   const [permission, requestPermission] = useCameraPermissions();
-  const { height, width } = useWindowDimensions();
-  if (!permission?.granted && permission?.canAskAgain) {
-    requestPermission().catch((error: unknown) => {
-      handleError(error);
-      router.back();
-    });
+
+  if (!permission) {
+    return <View fullScreen backgroundColor="$backgroundSoft" />;
   }
+
+  if (!permission.granted) {
+    if (!permission.canAskAgain) {
+      return (
+        <View fullScreen justifyContent="center" alignItems="center" backgroundColor="$backgroundSoft">
+          <XStack
+            position="absolute"
+            borderRadius="$r_0"
+            backgroundColor="transparent"
+            alignItems="center"
+            top="$s4"
+            left="$s4"
+            padding="$s3"
+            onPress={() => {
+              router.back();
+            }}
+            gap="$s2"
+          >
+            <ArrowLeft size={ms(24)} color="white" />
+            <Text headline>Back</Text>
+          </XStack>
+          <Button
+            onPress={() => {
+              Linking.openSettings().catch(handleError);
+            }}
+          >
+            Go to Settings
+          </Button>
+        </View>
+      );
+    }
+    return (
+      <View fullScreen justifyContent="center" alignItems="center" backgroundColor="$backgroundSoft">
+        <XStack
+          position="absolute"
+          borderRadius="$r_0"
+          backgroundColor="transparent"
+          alignItems="center"
+          top="$s4"
+          left="$s4"
+          padding="$s3"
+          onPress={() => {
+            router.back();
+          }}
+          gap="$s2"
+        >
+          <ArrowLeft size={ms(24)} color="white" />
+          <Text headline>Back</Text>
+        </XStack>
+        <Button
+          onPress={() => {
+            requestPermission().catch((error: unknown) => {
+              handleError(error);
+              router.back();
+            });
+          }}
+          outlined
+        >
+          Allow camera usage
+        </Button>
+      </View>
+    );
+  }
+
   return (
-    <View fullScreen>
+    <View fullScreen backgroundColor="$backgroundSoft">
       <CameraView
+        ref={cameraReference}
         barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
         onBarcodeScanned={({ data: receiver }) => {
           const result = safeParse(Address, receiver);
@@ -34,7 +99,6 @@ export default function Qr() {
         }}
         facing={cameraFacing}
         style={styles.cameraView}
-        autofocus="on"
       />
       <Button
         position="absolute"
