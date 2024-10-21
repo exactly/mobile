26af1aefaa65417c362dca0999b4b8a9073a1522
guillermo dieguez <guillermodieguez@outlook.com>
2024-10-21 17:21:08 -0300

    💫 app: add asset logo loading transition

--- src/components/shared/AssetLogo.tsx
+++ src/components/shared/AssetLogo.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import { Skeleton } from "moti/skeleton";
+import React, { useState } from "react";
+import { Appearance } from "react-native";
+import { FadeIn, FadeOut } from "react-native-reanimated";
 import { SvgUri, type UriProps } from "react-native-svg";
 
+import AnimatedView from "./AnimatedView";
 import View from "./View";
 
 export default function AssetLogo({ ...properties }: UriProps) {
+  const [loading, setLoading] = useState(true);
+
   return (
     <View borderRadius="$r_0" overflow="hidden">
-      <SvgUri {...properties} />
+      {loading && (
+        <Skeleton
+          radius="round"
+          colorMode={Appearance.getColorScheme() ?? "light"}
+          height={Number(properties.height)}
+          width={Number(properties.width)}
+        />
+      )}
+      <AnimatedView
+        alignItems="center"
+        justifyContent="center"
+        display={loading ? "none" : "flex"}
+        entering={FadeIn}
+        exiting={FadeOut}
+        width={Number(properties.width)}
+        height={Number(properties.height)}
+      >
+        <SvgUri
+          {...properties}
+          onLoad={() => {
+            setLoading(false);
+          }}
+        />
+      </AnimatedView>
     </View>
   );
 }
