673a0c0b3fcc1559fdf0468c872103538b494362
guillermo dieguez <guillermodieguez@outlook.com>
2024-10-28 17:39:43 -0300

    ✨ app: add info badge component

---
+++ src/components/shared/InfoBadge.tsx
@@ -0,0 +1,64 @@
+import { ChevronRight, AlertTriangle } from "@tamagui/lucide-icons";
+import React from "react";
+import { Pressable } from "react-native";
+import { ms } from "react-native-size-matters";
+import { View, Text, Spinner } from "tamagui";
+
+export default function InfoBadge({
+  title,
+  actionText,
+  loading,
+  onPress,
+}: {
+  title: string;
+  actionText: string;
+  loading?: boolean;
+  onPress?: () => void;
+}) {
+  return (
+    <View
+      borderRadius="$r6"
+      flexDirection="row"
+      backgroundColor="$interactiveBaseInformationSoftDefault"
+      justifyContent="space-between"
+      alignItems="center"
+      gap={ms(10)}
+      flex={1}
+    >
+      <View
+        padding={ms(25)}
+        backgroundColor="$interactiveBaseInformationDefault"
+        justifyContent="center"
+        alignItems="center"
+        borderTopLeftRadius="$r6"
+        borderBottomLeftRadius="$r6"
+        width="20%"
+        height="100%"
+      >
+        <AlertTriangle size={ms(32)} color="$interactiveOnBaseInformationDefault" />
+      </View>
+
+      <View gap={ms(10)} padding={ms(25)} flex={1}>
+        <Text fontSize={ms(15)} color="$interactiveOnBaseInformationSoft">
+          {title}
+        </Text>
+        <Pressable
+          onPress={() => {
+            onPress?.();
+          }}
+        >
+          <View flexDirection="row" gap={ms(2)} alignItems="center">
+            <Text color="$interactiveOnBaseInformationSoft" fontSize={ms(15)} lineHeight={18} fontWeight="bold">
+              {actionText}
+            </Text>
+            {loading ? (
+              <Spinner color="$interactiveOnBaseInformationSoft" />
+            ) : (
+              <ChevronRight size={ms(14)} color="$interactiveOnBaseInformationSoft" fontWeight="bold" />
+            )}
+          </View>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
