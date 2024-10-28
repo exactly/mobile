acdd7c54eb02724aeba7f665b0c9147a92116ab7
guillermo dieguez <guillermodieguez@outlook.com>
2024-10-28 17:39:57 -0300

    ✨ app: create plugin upgrade component

---
+++ src/components/shared/PluginUpgrade.tsx
@@ -0,0 +1,79 @@
+import { exaPluginAbi, exaPluginAddress } from "@exactly/common/generated/chain";
+import { useMutation } from "@tanstack/react-query";
+import React from "react";
+import { encodeAbiParameters, encodeFunctionData, getAbiItem, keccak256, zeroAddress } from "viem";
+import { useAccount } from "wagmi";
+
+import InfoBadge from "./InfoBadge";
+import {
+  upgradeableModularAccountAbi,
+  useReadExaPluginPluginManifest,
+  useReadUpgradeableModularAccountGetInstalledPlugins,
+  useSimulateUpgradeableModularAccountUninstallPlugin,
+} from "../../generated/contracts";
+import { accountClient } from "../../utils/alchemyConnector";
+import handleError from "../../utils/handleError";
+
+export default function PluginUpgrade() {
+  const { address } = useAccount();
+  const { data: installedPlugins, refetch: refetchInstalledPlugins } =
+    useReadUpgradeableModularAccountGetInstalledPlugins({
+      address: address ?? zeroAddress,
+    });
+  const { data: pluginManifest } = useReadExaPluginPluginManifest({ address: exaPluginAddress });
+  const { data: uninstallPluginSimulation } = useSimulateUpgradeableModularAccountUninstallPlugin({
+    address,
+    args: [installedPlugins?.[0] ?? zeroAddress, "0x", "0x"],
+    query: { enabled: !!address && !!installedPlugins },
+  });
+
+  const { mutateAsync: updatePlugin, isPending: isUpdating } = useMutation({
+    mutationFn: async () => {
+      if (!accountClient) throw new Error("no account client");
+      if (!address) throw new Error("no account address");
+      if (!installedPlugins?.[0]) throw new Error("no installed plugin");
+      if (!uninstallPluginSimulation) throw new Error("no uninstall plugin simulation");
+      if (!pluginManifest) throw new Error("invalid manifest");
+      const hash = await accountClient.sendUserOperation({
+        uo: [
+          { target: address, value: 0n, data: encodeFunctionData(uninstallPluginSimulation.request) },
+          {
+            target: address,
+            value: 0n,
+            data: encodeFunctionData({
+              abi: upgradeableModularAccountAbi,
+              functionName: "installPlugin",
+              args: [
+                exaPluginAddress,
+                keccak256(
+                  encodeAbiParameters(getAbiItem({ abi: exaPluginAbi, name: "pluginManifest" }).outputs, [
+                    pluginManifest,
+                  ]),
+                ),
+                "0x",
+                [],
+              ],
+            }),
+          },
+        ],
+      });
+      return accountClient.waitForUserOperationTransaction(hash);
+    },
+    onSuccess: async () => {
+      await refetchInstalledPlugins();
+    },
+  });
+
+  if (installedPlugins?.[0] === exaPluginAddress) return null;
+
+  return (
+    <InfoBadge
+      title="An account upgrade is required to access the latest features."
+      actionText="Upgrade account now"
+      loading={isUpdating}
+      onPress={() => {
+        updatePlugin().catch(handleError);
+      }}
+    />
+  );
+}
