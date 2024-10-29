ad0729666f2bf2b68810683b2d9628ad6e1f5e44
itofarina <farinalvaro@gmail.com>
2024-10-29 11:32:49 -0300

    ✨ contracts: integrate lifi on cross repay

--- contracts/test/ExaAccountFactory.t.sol
+++ contracts/test/ExaAccountFactory.t.sol
@@ -18,7 +18,7 @@ import { OwnersLib } from "webauthn-owner-plugin/OwnersLib.sol";
 import { WebauthnOwnerPlugin } from "webauthn-owner-plugin/WebauthnOwnerPlugin.sol";
 
 import { ExaAccountFactory, ExaAccountInitialized } from "../src/ExaAccountFactory.sol";
-import { ExaPlugin, IBalancerVault, IInstallmentsRouter, IVelodromeFactory } from "../src/ExaPlugin.sol";
+import { ExaPlugin, IBalancerVault, IInstallmentsRouter } from "../src/ExaPlugin.sol";
 import { IAuditor, IMarket } from "../src/IExaAccount.sol";
 import { IssuerChecker } from "../src/IssuerChecker.sol";
 import { KeeperFeeModel } from "../src/KeeperFeeModel.sol";
@@ -39,7 +39,6 @@ contract ExaAccountFactoryTest is Test {
       IMarket(address(new MockERC4626(new MockERC20()))),
       IBalancerVault(address(this)),
       IInstallmentsRouter(address(this)),
-      IVelodromeFactory(address(this)),
       IssuerChecker(address(this)),
       address(this),
       KeeperFeeModel(address(this))
