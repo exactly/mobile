d91f97379dc6ab29fe1a393584b05e18f695d96e
danilo neves cruz <cruzdanilo@gmail.com>
2024-10-29 18:57:30 +0000

    🔨 contracts: setup installments previewer deployment

--- contracts/test/InstallmentsPreviewer.t.sol
+++ contracts/test/InstallmentsPreviewer.t.sol
@@ -3,18 +3,29 @@ pragma solidity ^0.8.0;
 
 import { ForkTest } from "./Fork.t.sol";
 
+import { LibString } from "solady/utils/LibString.sol";
+
+import { DeployInstallmentsPreviewer } from "../script/InstallmentsPreviewer.s.sol";
 import { ICollectableMarket, InstallmentsPreviewer } from "../src/InstallmentsPreviewer.sol";
 import { DeployProtocol } from "./mocks/Protocol.s.sol";
 
 contract InstallmentsPreviewerTest is ForkTest {
+  using LibString for address;
+
   ICollectableMarket internal exaUSDC;
   InstallmentsPreviewer internal previewer;
 
   function setUp() external {
+    vm.setEnv("DEPLOYER_ADDRESS", address(this).toHexString());
+
     DeployProtocol p = new DeployProtocol();
     p.run();
     exaUSDC = ICollectableMarket(address(p.exaUSDC()));
-    previewer = new InstallmentsPreviewer(exaUSDC);
+    vm.setEnv("PROTOCOL_MARKETUSDC_ADDRESS", address(exaUSDC).toHexString());
+
+    DeployInstallmentsPreviewer ip = new DeployInstallmentsPreviewer();
+    ip.run();
+    previewer = ip.installmentsPreviewer();
   }
 
   // solhint-disable func-name-mixedcase
