e74f84615fbe4f96c42b8df535b88032efc1c8ac
danilo neves cruz <cruzdanilo@gmail.com>
2024-12-05 20:16:12 +0700

    🚧

--- src/components/settings/Beta.tsx
+++ src/components/settings/Beta.tsx
@@ -1,6 +1,10 @@
+import { optimism } from "@alchemy/aa-core";
+import { getRoutes, getStepTransaction } from "@lifi/sdk";
+import { captureException } from "@sentry/react-native";
 import { ArrowLeft } from "@tamagui/lucide-icons";
+import { useQuery } from "@tanstack/react-query";
 import { router, useRouter } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { Pressable } from "react-native";
 import { ms } from "react-native-size-matters";
 import { ScrollView } from "tamagui";
@@ -12,6 +16,29 @@ import View from "../../components/shared/View";
 
 export default function Beta() {
   const { canGoBack } = useRouter();
+  const { data: routes } = useQuery({
+    queryKey: ["lifi", "swap"],
+    queryFn: async () => {
+      const lifi = await getRoutes({
+        fromChainId: optimism.id,
+        toChainId: optimism.id,
+        fromTokenAddress: "0x68f180fcCe6836688e9084f035309E29Bf0A2095",
+        toTokenAddress: "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
+        fromAddress: "0x261968232C3541758Ee8d47a59FC275f1D55deA4",
+        fromAmount: "40000",
+      });
+      const [route] = lifi.routes;
+      if (!route) {
+        captureException(new Error("bad route"), { contexts: { lifi: { ...lifi } } });
+        return;
+      }
+      return Promise.all(route.steps.map((step) => getStepTransaction(step)));
+    },
+  });
+  useEffect(() => {
+    if (!routes) return;
+    console.log(...routes); // eslint-disable-line no-console -- // TODO remove
+  }, [routes]);
   return (
     <SafeView fullScreen tab>
       <View fullScreen padded gap="$s5">
