e8acc9ae8355518364041298164bcf69974a4bdc
guillermo dieguez <guillermodieguez@outlook.com>
2024-12-23 15:52:56 -0300

    🚧

--- src/components/payments/NextPayment.tsx
+++ src/components/payments/NextPayment.tsx
@@ -1,15 +1,18 @@
-import { marketUSDCAddress } from "@exactly/common/generated/chain";
+import { exaPluginAbi, marketUSDCAddress } from "@exactly/common/generated/chain";
 import { WAD } from "@exactly/lib";
-import { Coins, Info } from "@tamagui/lucide-icons";
+import { Coins, Info, RefreshCcw } from "@tamagui/lucide-icons";
 import { useQuery } from "@tanstack/react-query";
 import { format, formatDistance, isAfter } from "date-fns";
 import { router } from "expo-router";
-import React from "react";
+import React, { useCallback } from "react";
 import { Pressable } from "react-native";
 import { ms } from "react-native-size-matters";
 import { XStack } from "tamagui";
+import { useWriteContract } from "wagmi";
 
+import { useSimulateExaPluginRollDebt } from "../../generated/contracts";
 import handleError from "../../utils/handleError";
+import queryClient from "../../utils/queryClient";
 import useIntercom from "../../utils/useIntercom";
 import useMarketAccount from "../../utils/useMarketAccount";
 import Button from "../shared/Button";
@@ -19,7 +22,7 @@ import View from "../shared/View";
 export default function NextPayment() {
   const { presentArticle } = useIntercom();
   const { data: hidden } = useQuery<boolean>({ queryKey: ["settings", "sensitive"] });
-  const { market: USDCMarket } = useMarketAccount(marketUSDCAddress);
+  const { market: USDCMarket, account: address, queryKey } = useMarketAccount(marketUSDCAddress);
   const usdDue = new Map<bigint, { previewValue: bigint; position: bigint }>();
   if (USDCMarket) {
     const { fixedBorrowPositions, usdPrice, decimals } = USDCMarket;
@@ -33,6 +36,47 @@ export default function NextPayment() {
   const maturity = usdDue.keys().next().value;
   const duePayment = usdDue.get(maturity ?? 0n);
   const discount = duePayment ? Number(WAD - (duePayment.previewValue * WAD) / duePayment.position) / 1e18 : 0;
+
+  // TODO implement
+  const fixedPools = USDCMarket?.fixedPools.map((pool) => pool.maturity) ?? [];
+  const index = maturity ? fixedPools.indexOf(maturity) : 0;
+  const nextMaturity = fixedPools.length > 0 ? fixedPools[index + 1] : 0n;
+
+  // TODO implement
+  const repayMaturity = maturity ?? 0n; // The maturity of the fixed pool that the position is being rolled from.
+  const borrowMaturity = nextMaturity ?? 0n; // The maturity of the fixed pool that the position is being rolled to.
+  const maxRepayAssets = duePayment ? duePayment.position : 0n; // Max amount of debt that the account is willing to accept to be repaid.
+  const maxBorrowAssets = duePayment ? duePayment.position : 0n; // Max amount of debt that the account is willing to accept to be borrowed.
+  const percentage = WAD; // 100% expressed in 18 decimals.
+
+  const { data: rolloverSimulation, error: rolloverSimulationError } = useSimulateExaPluginRollDebt({
+    args: [repayMaturity, borrowMaturity, maxRepayAssets, maxBorrowAssets, percentage],
+    query: { enabled: !!maturity },
+    address,
+  });
+
+  const {
+    data: rolloverHash,
+    writeContract: rollover,
+    isPending: isRolloverPending,
+    isSuccess: isRolloverSuccess,
+    error: rolloverError,
+  } = useWriteContract({
+    mutation: {
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey }).catch(handleError);
+      },
+    },
+  });
+
+  const rollDebt = useCallback(() => {
+    if (!rolloverSimulation) throw new Error("no rollover simulation");
+    rollover(rolloverSimulation.request);
+  }, [rollover, rolloverSimulation]);
+
+  // TODO remove
+  console.log(rolloverSimulationError);
+
   return (
     <View backgroundColor="$backgroundSoft" paddingTop="$s8">
       {maturity ? (
@@ -130,6 +174,17 @@ export default function NextPayment() {
                   iconAfter={<Coins color="$interactiveOnBaseBrandDefault" strokeWidth={2.5} />}
                 >
                   Pay
+                </Button>
+                <Button
+                  spaced
+                  main
+                  halfWidth
+                  onPress={rollDebt}
+                  backgroundColor="$interactiveBaseBrandSoftDefault"
+                  color="$interactiveOnBaseBrandSoft"
+                  iconAfter={<RefreshCcw color="$interactiveOnBaseBrandSoft" strokeWidth={2.5} />}
+                >
+                  Rollover
                 </Button>
               </View>
             </View>
