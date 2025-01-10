9e40128798975a934acba9d3761ffb144280a88e
danilo neves cruz <cruzdanilo@gmail.com>
2025-01-11 00:42:34 +0700

    🔧 changesets: filter ignored packages in commit message

--- .changeset/commit.js
+++ .changeset/commit.js
@@ -3,6 +3,7 @@ module.exports = {
   getVersionMessage: ({ releases }) =>
     Promise.resolve(
       `🔖 release: ${releases
+        .filter(({ type }) => type !== "none")
         .map(({ name, newVersion }) => `${name.replace(/^@exactly\//, "")}@${newVersion}`)
         .reverse()
         .join(", ")}`,
