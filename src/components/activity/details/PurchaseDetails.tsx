d39fceb28ee733cc2b2008ac4f9472366fa850df
guillermo dieguez <guillermodieguez@outlook.com>
2025-01-17 11:54:13 -0300

    ✨ app: improve activity details

---
+++ src/components/activity/details/PurchaseDetails.tsx
@@ -0,0 +1,90 @@
+import shortenHex from "@exactly/common/shortenHex";
+import type { CreditActivity, DebitActivity, InstallmentsActivity } from "@exactly/server/api/activity";
+import { Copy } from "@tamagui/lucide-icons";
+import { useToastController } from "@tamagui/toast";
+import { format } from "date-fns";
+import { setStringAsync } from "expo-clipboard";
+import React from "react";
+import { Pressable } from "react-native";
+import { ms } from "react-native-size-matters";
+import { Separator, XStack, YStack } from "tamagui";
+
+import handleError from "../../../utils/handleError";
+import Text from "../../shared/Text";
+
+export default function PurchaseDetails({ item }: { item: CreditActivity | DebitActivity | InstallmentsActivity }) {
+  const toast = useToastController();
+  return (
+    <YStack gap="$s4">
+      <YStack gap="$s4">
+        <Text emphasized headline>
+          Purchase details
+        </Text>
+        <Separator height={1} borderColor="$borderNeutralSoft" />
+      </YStack>
+      <YStack gap="$s3_5">
+        <XStack justifyContent="space-between">
+          <Text emphasized footnote color="$uiNeutralSecondary">
+            Amount
+          </Text>
+          <Text callout color="$uiNeutralPrimary">
+            {Number(item.amount).toLocaleString(undefined, { maximumFractionDigits: 8, minimumFractionDigits: 0 })}
+            &nbsp;{item.currency}
+          </Text>
+        </XStack>
+        <XStack justifyContent="space-between">
+          <Text emphasized footnote color="$uiNeutralSecondary">
+            ID
+          </Text>
+          <Pressable
+            onPress={() => {
+              setStringAsync(item.id).catch(handleError);
+              toast.show("Operation ID copied!", {
+                native: true,
+                duration: 1000,
+                burntOptions: { haptic: "success" },
+              });
+            }}
+            hitSlop={ms(15)}
+          >
+            <XStack gap="$s3">
+              <Text callout color="$uiNeutralPrimary">
+                {shortenHex(item.id)}
+              </Text>
+              <Copy size={ms(20)} color="$uiNeutralPrimary" />
+            </XStack>
+          </Pressable>
+        </XStack>
+        <XStack justifyContent="space-between">
+          <Text emphasized footnote color="$uiNeutralSecondary">
+            Exchange rate
+          </Text>
+          <Text callout color="$uiNeutralPrimary">
+            1 USD&nbsp;=&nbsp;
+            {Number(item.amount / item.usdAmount).toLocaleString(undefined, {
+              maximumFractionDigits: 8,
+              minimumFractionDigits: 0,
+            })}
+            &nbsp;{item.currency}
+          </Text>
+        </XStack>
+        <XStack justifyContent="space-between">
+          <Text emphasized footnote color="$uiNeutralSecondary">
+            Date
+          </Text>
+          <Text callout color="$uiNeutralPrimary">
+            {format(item.timestamp, "yyyy-MM-dd")}
+          </Text>
+        </XStack>
+        <XStack justifyContent="space-between">
+          <Text emphasized footnote color="$uiNeutralSecondary">
+            Time
+          </Text>
+          <Text callout color="$uiNeutralPrimary">
+            {format(item.timestamp, "HH:mm:ss")}
+          </Text>
+        </XStack>
+      </YStack>
+    </YStack>
+  );
+}
--- src/components/activity/details/TransactionDetails.tsx
+++ src/components/activity/details/TransactionDetails.tsx
@@ -1,12 +1,12 @@
 import chain from "@exactly/common/generated/chain";
 import shortenHex from "@exactly/common/shortenHex";
 import { Copy, SquareArrowOutUpRight } from "@tamagui/lucide-icons";
+import { useToastController } from "@tamagui/toast";
 import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import { setStringAsync } from "expo-clipboard";
 import { openBrowserAsync } from "expo-web-browser";
 import React from "react";
-import { Alert } from "react-native";
 import { ms } from "react-native-size-matters";
 import { Separator, XStack, YStack } from "tamagui";
 
@@ -17,7 +17,7 @@ import Text from "../../shared/Text";
 
 export default function TransactionDetails() {
   const { data: item } = useQuery<ActivityItem>({ queryKey: ["activity", "details"] });
-  if (!item || item.type === "card") return null;
+  const toast = useToastController();
   return (
     <YStack gap="$s4">
       <YStack gap="$s4">
@@ -27,26 +27,6 @@ export default function TransactionDetails() {
         <Separator height={1} borderColor="$borderNeutralSoft" />
       </YStack>
       <YStack gap="$s3_5">
-        {item.type === "sent" && (
-          <XStack justifyContent="space-between">
-            <Text emphasized footnote color="$uiNeutralSecondary">
-              To
-            </Text>
-            <XStack
-              alignItems="center"
-              gap="$s3"
-              onPress={() => {
-                setStringAsync(item.receiver).catch(handleError);
-                Alert.alert("Copied", "The receiver's address has been copied to the clipboard.");
-              }}
-            >
-              <Text callout textDecorationLine="underline">
-                {shortenHex(item.receiver)}
-              </Text>
-              <Copy size={ms(20)} color="$uiNeutralSecondary" />
-            </XStack>
-          </XStack>
-        )}
         <XStack justifyContent="space-between">
           <Text emphasized footnote color="$uiNeutralSecondary">
             Network fee
@@ -66,39 +46,69 @@ export default function TransactionDetails() {
             <OptimismImage height={ms(20)} width={ms(20)} />
           </XStack>
         </XStack>
-        <XStack justifyContent="space-between">
-          <Text emphasized footnote color="$uiNeutralSecondary">
-            Date
-          </Text>
-          <Text callout color="$uiNeutralPrimary">
-            {format(item.timestamp, "yyyy-MM-dd")}
-          </Text>
-        </XStack>
-        <XStack justifyContent="space-between">
-          <Text emphasized footnote color="$uiNeutralSecondary">
-            Time
-          </Text>
-          <Text callout color="$uiNeutralPrimary">
-            {format(item.timestamp, "HH:mm:ss")}
-          </Text>
-        </XStack>
-        <XStack justifyContent="space-between">
-          <Text emphasized footnote color="$uiNeutralSecondary">
-            Transaction hash
-          </Text>
-          <XStack
-            alignItems="center"
-            gap="$s3"
-            onPress={() => {
-              openBrowserAsync(`${chain.blockExplorers?.default.url}/tx/${item.transactionHash}`).catch(handleError);
-            }}
-          >
-            <Text textDecorationLine="underline" callout color="$uiNeutralPrimary">
-              {shortenHex(item.transactionHash)}
+        {item && item.type === "sent" && (
+          <XStack justifyContent="space-between">
+            <Text emphasized footnote color="$uiNeutralSecondary">
+              To
             </Text>
-            <SquareArrowOutUpRight size={ms(20)} color="$uiNeutralSecondary" />
+            <XStack
+              alignItems="center"
+              gap="$s3"
+              onPress={() => {
+                setStringAsync(item.receiver).catch(handleError);
+                toast.show("Address copied!", {
+                  native: true,
+                  duration: 1000,
+                  burntOptions: { haptic: "success" },
+                });
+              }}
+            >
+              <Text callout textDecorationLine="underline">
+                {shortenHex(item.receiver)}
+              </Text>
+              <Copy size={ms(20)} color="$uiNeutralSecondary" />
+            </XStack>
           </XStack>
-        </XStack>
+        )}
+        {item?.timestamp && item.type !== "card" && (
+          <>
+            <XStack justifyContent="space-between">
+              <Text emphasized footnote color="$uiNeutralSecondary">
+                Date
+              </Text>
+              <Text callout color="$uiNeutralPrimary">
+                {format(item.timestamp, "yyyy-MM-dd")}
+              </Text>
+            </XStack>
+            <XStack justifyContent="space-between">
+              <Text emphasized footnote color="$uiNeutralSecondary">
+                Time
+              </Text>
+              <Text callout color="$uiNeutralPrimary">
+                {format(item.timestamp, "HH:mm:ss")}
+              </Text>
+            </XStack>
+          </>
+        )}
+        {item?.transactionHash && (
+          <XStack justifyContent="space-between">
+            <Text emphasized footnote color="$uiNeutralSecondary">
+              Transaction hash
+            </Text>
+            <XStack
+              alignItems="center"
+              gap="$s3"
+              onPress={() => {
+                openBrowserAsync(`${chain.blockExplorers?.default.url}/tx/${item.transactionHash}`).catch(handleError);
+              }}
+            >
+              <Text textDecorationLine="underline" callout color="$uiNeutralPrimary">
+                {shortenHex(item.transactionHash)}
+              </Text>
+              <SquareArrowOutUpRight size={ms(20)} color="$uiNeutralSecondary" />
+            </XStack>
+          </XStack>
+        )}
       </YStack>
     </YStack>
   );
