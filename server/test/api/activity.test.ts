515fcf794d4af0cd97e4c9e014507a560ae71eca
danilo neves cruz <cruzdanilo@gmail.com>
2025-01-29 21:50:08 +0700

    ✅ server: improve test assertion range

--- server/test/api/activity.test.ts
+++ server/test/api/activity.test.ts
@@ -189,7 +189,12 @@ describe("authenticated", () => {
 
       expect(response.status).toBe(200);
       await expect(response.json()).resolves.toMatchObject([
-        { amount: expect.closeTo(420, 0.01), currency: "USDC", type: "repay", usdAmount: expect.closeTo(420, 0.01) }, // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+        {
+          amount: expect.closeTo(419.5, 0.01), // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+          currency: "USDC",
+          type: "repay",
+          usdAmount: expect.closeTo(419.5, 0.01), // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+        },
         { amount: expect.closeTo(81, 0.5), currency: "USDC", type: "repay", usdAmount: expect.closeTo(81, 0.5) }, // eslint-disable-line @typescript-eslint/no-unsafe-assignment
       ]);
     });
