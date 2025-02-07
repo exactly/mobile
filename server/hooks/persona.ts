50c79b32105f3fc24b937575bcb6ef020b237b2e
nfmelendez <nfmelendez@gmail.com>
2025-02-07 12:04:55 -0300

    🐛 server: fix session parsing and use last one

--- server/hooks/persona.ts
+++ server/hooks/persona.ts
@@ -2,53 +2,77 @@ import { vValidator } from "@hono/valibot-validator";
 import { captureException } from "@sentry/node";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { array, length, literal, object, pipe, string, variant, type InferInput } from "valibot";
+import type { InferOutput } from "valibot";
+import { array, ip, isoTimestamp, literal, looseObject, object, pipe, safeParse, string, transform } from "valibot";
 
 import database, { credentials } from "../database/index";
 import { createUser } from "../utils/panda";
 import { headerValidator } from "../utils/persona";
 
-const IpAddress = object({
-  type: literal("inquiry-session"),
-  attributes: object({ status: literal("active"), ipAddress: string() }),
-});
-
-const Payload = object({
-  data: object({
-    attributes: object({
-      payload: object({
-        data: object({
-          id: string(),
-          attributes: object({
-            status: literal("approved"),
-            referenceId: string(),
-            fields: object({
-              selectedCountryCode: object({ value: pipe(string(), length(2)) }),
-              inputSelect: object({ value: string() }),
-              annualSalary: object({ value: string() }),
-              expectedMonthlyVolume: object({ value: string() }),
-              accountPurpose: object({ value: string() }),
-            }),
-          }),
-        }),
-        included: array(variant("type", [IpAddress, object({ type: string() })])),
-      }),
-    }),
+const Session = pipe(
+  object({
+    type: literal("inquiry-session"),
+    attributes: object({ createdAt: pipe(string(), isoTimestamp()), ipAddress: pipe(string(), ip()) }),
   }),
-});
+  transform((x) => {
+    return {
+      ...x,
+      attributes: {
+        createdAt: x.attributes.createdAt,
+        IPAddress: x.attributes.ipAddress,
+      },
+    };
+  }),
+);
 
 export default new Hono().post(
   "/",
   headerValidator(),
-  vValidator("json", Payload, (validation, c) => {
-    if (!validation.success) {
-      captureException(new Error("bad persona"), { contexts: { validation } });
-      return c.json(
-        validation.issues.map((issue) => `${issue.path?.map((p) => p.key).join("/")} ${issue.message}`),
-        400,
-      );
-    }
-  }),
+  vValidator(
+    "json",
+    object({
+      data: object({
+        attributes: object({
+          payload: object({
+            data: object({
+              id: string(),
+              attributes: object({
+                status: literal("approved"),
+                referenceId: string(),
+                fields: object({
+                  inputSelect: object({ value: string() }),
+                  annualSalary: object({ value: string() }),
+                  expectedMonthlyVolume: object({ value: string() }),
+                  accountPurpose: object({ value: string() }),
+                }),
+              }),
+            }),
+            included: pipe(
+              array(looseObject({ type: string() })),
+              transform((incl) => {
+                return incl
+                  .reduce<InferOutput<typeof Session>[]>((sessions, item) => {
+                    const s = safeParse(Session, item);
+                    if (s.success) return [...sessions, s.output];
+                    return sessions;
+                  }, [])
+                  .sort((a, b) => a.attributes.createdAt.localeCompare(b.attributes.createdAt));
+              }),
+            ),
+          }),
+        }),
+      }),
+    }),
+    (validation, c) => {
+      if (!validation.success) {
+        captureException(new Error("bad persona"), { contexts: { validation } });
+        return c.json(
+          validation.issues.map((issue) => `${issue.path?.map((p) => p.key).join("/")} ${issue.message}`),
+          400,
+        );
+      }
+    },
+  ),
   async (c) => {
     const payload = c.req.valid("json");
     const {
@@ -59,14 +83,14 @@ export default new Hono().post(
       included,
     } = payload.data.attributes.payload;
 
-    const session = included.find((x): x is InferInput<typeof IpAddress> => x.type === "inquiry-session");
-    if (!session) return c.json("no active inquiry session", 400);
+    const session = included[0];
+    if (!session) return c.json("no inquiry session", 400);
 
     const { id } = await createUser({
       accountPurpose: fields.accountPurpose.value,
       annualSalary: fields.annualSalary.value,
       expectedMonthlyVolume: fields.expectedMonthlyVolume.value,
-      ipAddress: session.attributes.ipAddress,
+      ipAddress: session.attributes.IPAddress,
       isTermsOfServiceAccepted: true,
       occupation: fields.inputSelect.value,
       personaShareToken,
