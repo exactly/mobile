e0a523e0ac329b29bc043a35299aede89596c262
danilo neves cruz <cruzdanilo@gmail.com>
2025-02-12 21:23:42 +0700

    ⬆️ dependencies: upgrade all

--- server/instrument.cjs
+++ server/instrument.cjs
@@ -8,7 +8,6 @@ init({
   tracesSampleRate: process.env.NODE_ENV === "production" ? 1 : 0.01,
   profilesSampleRate: process.env.NODE_ENV === "production" ? 1 : 0.01,
   attachStacktrace: true,
-  autoSessionTracking: true,
   maxValueLength: 8192,
   normalizeDepth: 69,
   integrations: [nodeProfilingIntegration(), extraErrorDataIntegration({ depth: 69 })],
