223c2d4c27167919e308cdf3035872ef84d108fe
guillermo dieguez <guillermodieguez@outlook.com>
2025-02-14 13:23:33 -0300

    ⚗️ app: remove plugin upgrade limitation

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -27,6 +27,7 @@ import useAsset from "../../utils/useAsset";
 import useIntercom from "../../utils/useIntercom";
 import InfoAlert from "../shared/InfoAlert";
 import LatestActivity from "../shared/LatestActivity";
+import PluginUpgrade from "../shared/PluginUpgrade";
 import SafeView from "../shared/SafeView";
 import Text from "../shared/Text";
 import View from "../shared/View";
@@ -220,6 +221,7 @@ export default function Card() {
                     }}
                   />
                 )}
+                <PluginUpgrade />
                 <ExaCard
                   revealing={isRevealing || isGeneratingCard}
                   frozen={cardDetails?.status === "FROZEN"}
