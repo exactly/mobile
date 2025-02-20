76a3eb417b9b947a89cfd9db2be55400bad04b80
nfmelendez <nfmelendez@gmail.com>
2025-02-20 11:55:55 -0300

    ✨ server: split collectors by provider

--- server/utils/cryptomate.ts
+++ server/utils/cryptomate.ts
@@ -3,6 +3,7 @@ import { Address, Hash } from "@exactly/common/validation";
 import removeAccents from "remove-accents";
 import * as v from "valibot";
 import { privateKeyToAccount } from "viem/accounts";
+import { optimism } from "viem/chains";
 
 import { track } from "./segment";
 import { issuerCheckerAddress } from "../generated/contracts";
@@ -76,6 +77,12 @@ async function request<TInput, TOutput, TIssue extends v.BaseIssue<unknown>>(
   if (!response.ok) throw new Error(`${response.status} ${await response.text()}`);
   return v.parse(schema, await response.json());
 }
+
+export const collectors: Address[] = (
+  {
+    [optimism.id]: ["0x0f25bA5b8B0BA4Ff4dF645fDE030652da60BabA6", "0x471e5F3428D5C50543072c817a9D0CcBa8ed7D5F"],
+  }[chain.id] ?? ["0xDb90CDB64CfF03f254e4015C4F705C3F3C834400"]
+).map((address) => v.parse(Address, address));
 
 const CreateCardRequest = v.object({
   card_holder_name: v.string(),
