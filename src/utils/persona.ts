a88ee30b4ddb5665a13c96ed4dc369790bd19ec9
nfmelendez <nfmelendez@gmail.com>
2025-02-24 15:49:32 -0300

    🔧 app: set persona template for panda

--- src/utils/persona.ts
+++ src/utils/persona.ts
@@ -1,14 +1,27 @@
+import { upgradeableModularAccountAbi } from "@exactly/common/generated/chain";
+import latestExaPlugin from "@exactly/common/latestExaPlugin";
 import type { Passkey } from "@exactly/common/validation";
 import { router } from "expo-router";
 import { Platform } from "react-native";
 import { Environment, Inquiry } from "react-native-persona";
+import { zeroAddress } from "viem";
 
 import handleError from "./handleError";
+import publicClient from "./publicClient";
 import queryClient from "./queryClient";
 import { getKYCLink } from "./server";
+import { accountClient } from "../utils/alchemyConnector";
 
-export const templateId = "itmpl_8uim4FvD5P3kFpKHX37CW817"; // cspell:disable-line
 export const environment = __DEV__ ? Environment.SANDBOX : Environment.PRODUCTION;
+
+export async function getTemplateId() {
+  const [exaPlugin] = await publicClient.readContract({
+    address: accountClient?.account.address ?? zeroAddress,
+    abi: upgradeableModularAccountAbi,
+    functionName: "getInstalledPlugins",
+  });
+  return exaPlugin === latestExaPlugin ? "itmpl_1igCJVqgf3xuzqKYD87HrSaDavU2" : "itmpl_8uim4FvD5P3kFpKHX37CW817"; // cspell:disable-line
+}
 
 export async function createInquiry(passkey: Passkey) {
   if (Platform.OS === "web") {
@@ -17,7 +30,7 @@ export async function createInquiry(passkey: Passkey) {
     return;
   }
 
-  Inquiry.fromTemplate(templateId)
+  Inquiry.fromTemplate(await getTemplateId())
     .environment(environment)
     .referenceId(passkey.credentialId)
     .onCanceled(() => {
