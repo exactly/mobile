482cd76edcd1cf006cf380e30593e3fbe57eeb5c
danilo neves cruz <cruzdanilo@gmail.com>
2025-02-28 14:59:33 +0700

    ✨ common: add proposal type enum

---
+++ .changeset/modern-streets-drum.md
@@ -0,0 +1,5 @@
+---
+"@exactly/common": patch
+---
+
+✨ add proposal type enum
---
+++ common/ProposalType.ts
@@ -0,0 +1,11 @@
+enum ProposalType {
+  None,
+  BorrowAtMaturity,
+  CrossRepayAtMaturity,
+  Redeem,
+  RepayAtMaturity,
+  RollDebt,
+  Swap,
+  Withdraw,
+}
+export default ProposalType;
