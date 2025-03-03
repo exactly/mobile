95bbda8a2c725e6174759502165bb383ea747a73
guillermo dieguez <guillermodieguez@outlook.com>
2025-03-03 14:05:53 -0300

    🔥 app: remove unused components from card screen

--- src/components/card/exa-card/CardContents.tsx
+++ src/components/card/exa-card/CardContents.tsx
@@ -1,6 +1,6 @@
 import { marketUSDCAddress, previewerAddress } from "@exactly/common/generated/chain";
 import { borrowLimit, withdrawLimit } from "@exactly/lib";
-import { ChevronRight, Dot, Loader, LockKeyhole, Snowflake } from "@tamagui/lucide-icons";
+import { Loader, LockKeyhole, Snowflake } from "@tamagui/lucide-icons";
 import React from "react";
 import { Platform } from "react-native";
 import { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
@@ -20,13 +20,11 @@ export default function CardContents({
   disabled,
   frozen,
   revealing,
-  lastFour,
 }: {
   isCredit: boolean;
   disabled: boolean;
   frozen: boolean;
   revealing: boolean;
-  lastFour?: string;
 }) {
   const { address } = useAccount();
   const { data: markets } = useReadPreviewerExactly({ address: previewerAddress, args: [address ?? zeroAddress] });
@@ -48,79 +46,60 @@ export default function CardContents({
     >
       <YStack height="100%" justifyContent="space-between" alignItems="flex-start">
         <AnimatePresence exitBeforeEnter>
-          {disabled ? (
-            <LockKeyhole size={ms(40)} strokeWidth={2} color="white" />
-          ) : revealing ? (
-            <AnimatedView style={rStyle}>
-              <Loader size={ms(40)} strokeWidth={2} color="white" />
-            </AnimatedView>
-          ) : frozen ? (
-            <Snowflake size={ms(40)} strokeWidth={2} color="white" />
-          ) : isCredit ? (
-            <View
-              key="credit"
-              animation="moderate"
-              enterStyle={{ opacity: 0, transform: [{ translateX: -100 }] }} // eslint-disable-line react-native/no-inline-styles
-              exitStyle={{ opacity: 0, transform: [{ translateX: -100 }] }} // eslint-disable-line react-native/no-inline-styles
-              transform={[{ translateX: 0 }]}
-            >
-              <Text sensitive color="white" title maxFontSizeMultiplier={1}>
-                {(markets ? Number(borrowLimit(markets, marketUSDCAddress)) / 1e6 : 0).toLocaleString(undefined, {
-                  style: "currency",
-                  currency: "USD",
-                  currencyDisplay: "narrowSymbol",
-                })}
-              </Text>
+          <YStack>
+            {disabled ? (
+              <LockKeyhole size={ms(40)} strokeWidth={2} color="white" />
+            ) : revealing ? (
+              <AnimatedView style={rStyle}>
+                <Loader size={ms(40)} strokeWidth={2} color="white" />
+              </AnimatedView>
+            ) : frozen ? (
+              <Snowflake size={ms(40)} strokeWidth={2} color="white" />
+            ) : isCredit ? (
               <View
+                key="credit"
                 animation="moderate"
-                enterStyle={{ opacity: 0, transform: [{ translateX: 25 }] }} // eslint-disable-line react-native/no-inline-styles
-                exitStyle={{ opacity: 0, transform: [{ translateX: 25 }] }} // eslint-disable-line react-native/no-inline-styles
+                enterStyle={{ opacity: 0, transform: [{ translateX: -100 }] }} // eslint-disable-line react-native/no-inline-styles
+                exitStyle={{ opacity: 0, transform: [{ translateX: -100 }] }} // eslint-disable-line react-native/no-inline-styles
                 transform={[{ translateX: 0 }]}
               >
-                <Text color="white" emphasized caption maxFontSizeMultiplier={1}>
-                  CREDIT LIMIT
+                <Text sensitive color="white" title maxFontSizeMultiplier={1}>
+                  {(markets ? Number(borrowLimit(markets, marketUSDCAddress)) / 1e6 : 0).toLocaleString(undefined, {
+                    style: "currency",
+                    currency: "USD",
+                    currencyDisplay: "narrowSymbol",
+                  })}
                 </Text>
+                <View>
+                  <Text color="white" emphasized caption maxFontSizeMultiplier={1}>
+                    SPENDING LIMIT
+                  </Text>
+                </View>
               </View>
-            </View>
-          ) : (
-            <View
-              key="debit"
-              animation="moderate"
-              enterStyle={{ opacity: 0, transform: [{ translateX: 100 }] }} // eslint-disable-line react-native/no-inline-styles
-              exitStyle={{ opacity: 0, transform: [{ translateX: 100 }] }} // eslint-disable-line react-native/no-inline-styles
-              transform={[{ translateX: 0 }]}
-            >
-              <Text sensitive color="white" title maxFontSizeMultiplier={1}>
-                {(markets ? Number(withdrawLimit(markets, marketUSDCAddress)) / 1e6 : 0).toLocaleString(undefined, {
-                  style: "currency",
-                  currency: "USD",
-                  currencyDisplay: "narrowSymbol",
-                })}
-              </Text>
+            ) : (
               <View
+                key="debit"
                 animation="moderate"
-                enterStyle={{ opacity: 0, transform: [{ translateX: -25 }] }} // eslint-disable-line react-native/no-inline-styles
-                exitStyle={{ opacity: 0, transform: [{ translateX: -25 }] }} // eslint-disable-line react-native/no-inline-styles
+                enterStyle={{ opacity: 0, transform: [{ translateX: 100 }] }} // eslint-disable-line react-native/no-inline-styles
+                exitStyle={{ opacity: 0, transform: [{ translateX: 100 }] }} // eslint-disable-line react-native/no-inline-styles
                 transform={[{ translateX: 0 }]}
               >
-                <Text color="white" emphasized caption maxFontSizeMultiplier={1}>
-                  DEBIT LIMIT
+                <Text sensitive color="white" title maxFontSizeMultiplier={1}>
+                  {(markets ? Number(withdrawLimit(markets, marketUSDCAddress)) / 1e6 : 0).toLocaleString(undefined, {
+                    style: "currency",
+                    currency: "USD",
+                    currencyDisplay: "narrowSymbol",
+                  })}
                 </Text>
+                <View>
+                  <Text color="white" emphasized caption maxFontSizeMultiplier={1}>
+                    SPENDING LIMIT
+                  </Text>
+                </View>
               </View>
-            </View>
-          )}
+            )}
+          </YStack>
         </AnimatePresence>
-        <XStack gap="$s2" alignItems="center" justifyContent="flex-start">
-          <XStack gap="$s1">
-            {Array.from({ length: 4 }).map((_, index) => (
-              <Dot key={index} color="white" size={ms(10)} strokeWidth={10} />
-            ))}
-          </XStack>
-          <Text color="white" callout fontFamily="$mono" letterSpacing={2} maxFontSizeMultiplier={1}>
-            {lastFour}
-          </Text>
-          <ChevronRight size={ms(20)} strokeWidth={2} color="white" />
-        </XStack>
       </YStack>
       <XStack animation="moderate" position="absolute" right={0} left={0} top={0} bottom={0} justifyContent="flex-end">
         <Card
