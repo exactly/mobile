3c44fff8c504dd0737cf8fb43ba816a29a874e16
danilo neves cruz <cruzdanilo@gmail.com>
2025-03-06 17:58:25 -0300

    ✅ server: fix anvil setup script order

--- server/test/anvil.ts
+++ server/test/anvil.ts
@@ -81,8 +81,6 @@ export default async function setup({ provide }: TestProject) {
     shell.env.PROTOCOL_INSTALLMENTSROUTER_ADDRESS = installmentsRouter.contractAddress; // cspell:disable-line
     await $(shell)`forge script test/mocks/Mocks.s.sol
       --unlocked ${deployer} --rpc-url ${foundry.rpcUrls.default.http[0]} --broadcast --slow --skip-simulation`;
-    await $(shell)`forge script script/ExaPreviewer.s.sol
-      --unlocked ${deployer} --rpc-url ${foundry.rpcUrls.default.http[0]} --broadcast --slow --skip-simulation`;
     await $(shell)`forge script script/IssuerChecker.s.sol
       --unlocked ${deployer} --rpc-url ${foundry.rpcUrls.default.http[0]} --broadcast --slow --skip-simulation`;
   }
@@ -111,14 +109,10 @@ export default async function setup({ provide }: TestProject) {
       --unlocked ${deployer} --rpc-url ${foundry.rpcUrls.default.http[0]} --broadcast --slow --skip-simulation`;
     await $(shell)`forge script script/Refunder.s.sol
       --unlocked ${deployer} --rpc-url ${foundry.rpcUrls.default.http[0]} --broadcast --slow --skip-simulation`;
+    await $(shell)`forge script script/ExaPreviewer.s.sol
+      --unlocked ${deployer} --rpc-url ${foundry.rpcUrls.default.http[0]} --broadcast --slow --skip-simulation`;
   }
 
-  const [exaPreviewer] = parse(
-    object({
-      transactions: tuple([object({ contractName: literal("ExaPreviewer"), contractAddress: Address })]),
-    }),
-    await import(`@exactly/plugin/broadcast/ExaPreviewer.s.sol/${foundry.id}/run-latest.json`),
-  ).transactions;
   const [proposalManager] = parse(
     object({
       transactions: tuple([object({ contractName: literal("ProposalManager"), contractAddress: Address })]),
@@ -130,6 +124,12 @@ export default async function setup({ provide }: TestProject) {
       transactions: tuple([object({ contractName: literal("Refunder"), contractAddress: Address })]),
     }),
     await import(`@exactly/plugin/broadcast/Refunder.s.sol/${foundry.id}/run-latest.json`),
+  ).transactions;
+  const [exaPreviewer] = parse(
+    object({
+      transactions: tuple([object({ contractName: literal("ExaPreviewer"), contractAddress: Address })]),
+    }),
+    await import(`@exactly/plugin/broadcast/ExaPreviewer.s.sol/${foundry.id}/run-latest.json`),
   ).transactions;
 
   if (initialize) {
