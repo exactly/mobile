b845ec63aaf5091bf5a1d7c5b753629f906f922b
danilo neves cruz <cruzdanilo@gmail.com>
2025-03-07 00:04:27 -0300

    🤡 server: improve sentry test reporter

--- server/test/mocks/sentry.ts
+++ server/test/mocks/sentry.ts
@@ -5,11 +5,14 @@ const contexts = new Map<string, unknown>();
 
 vi.mock("@sentry/node", async () => ({
   ...(await import("@sentry/node")),
-  captureException(...arguments_: unknown[]) {
+  captureException(exception: unknown, hint?: { level?: string; contexts?: Record<string, unknown> }) {
+    const { contexts: localContexts, ...rest } = hint ?? {};
+    if (localContexts) for (const [key, value] of Object.entries(localContexts)) contexts.set(key, value);
     for (const [key, value] of contexts) {
       console.log(key, inspect(value, false, null, true)); // eslint-disable-line no-console
     }
-    console.log(...arguments_.map((value) => inspect(value, false, null, true))); // eslint-disable-line no-console
+    for (const [key, value] of Object.entries(rest)) console.log(key, inspect(value, false, null, true)); // eslint-disable-line no-console
+    console.log(exception instanceof Error ? exception.stack : exception); // eslint-disable-line no-console
   },
   setContext: (key: string, value: unknown) => contexts.set(key, value),
 }));
