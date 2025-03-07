53de9ad956f9a00554456e19bad571e4a386d499
danilo neves cruz <cruzdanilo@gmail.com>
2025-03-07 09:18:41 -0300

    ✨ server: extend keeper transaction sending

---
+++ .changeset/rude-friends-grab.md
@@ -0,0 +1,5 @@
+---
+"@exactly/server": patch
+---
+
+✨ extend keeper transaction sending
--- server/utils/keeper.ts
+++ server/utils/keeper.ts
@@ -1,9 +1,24 @@
 import alchemyAPIKey from "@exactly/common/alchemyAPIKey";
 import chain from "@exactly/common/generated/chain";
 import { Hash } from "@exactly/common/validation";
+import { captureException, setContext, startSpan } from "@sentry/node";
 import { parse } from "valibot";
-import { createWalletClient, http, nonceManager } from "viem";
+import {
+  BaseError,
+  ContractFunctionRevertedError,
+  createWalletClient,
+  getContractError,
+  http,
+  nonceManager,
+  RawContractError,
+  type Prettify,
+  type TransactionReceipt,
+  type WriteContractParameters,
+} from "viem";
 import { privateKeyToAccount } from "viem/accounts";
+
+import publicClient from "./publicClient";
+import traceClient from "./traceClient";
 
 if (!chain.rpcUrls.alchemy?.http[0]) throw new Error("missing alchemy rpc url");
 
@@ -16,4 +31,72 @@ export default createWalletClient({
     }),
     { nonceManager },
   ),
-});
+}).extend(extender);
+
+export function extender(keeper: ReturnType<typeof createWalletClient>) {
+  return {
+    exaSend: async (
+      spanOptions: Prettify<{ name: string; op: string } & Omit<Parameters<typeof startSpan>[0], "name" | "op">>,
+      call: Prettify<Pick<WriteContractParameters, "address" | "functionName" | "args" | "abi">>,
+      options?: {
+        onHash?: (hash: Hash) => Promise<unknown>;
+        ignore?: string[] | ((reason: string) => Promise<TransactionReceipt | boolean | undefined>);
+      },
+    ) =>
+      startSpan(spanOptions, async () => {
+        try {
+          const { request: writeRequest } = await startSpan({ name: "eth_call", op: "tx.simulate" }, () =>
+            publicClient.simulateContract({
+              account: keeper.account,
+              type: "eip1559",
+              maxFeePerGas: 1_000_000_000n,
+              maxPriorityFeePerGas: 1_000_000n,
+              gas: 5_000_000n,
+              ...call,
+            }),
+          );
+          const {
+            abi: _,
+            account: __,
+            address: ___,
+            ...request
+          } = { from: writeRequest.account.address, to: writeRequest.address, ...writeRequest };
+          setContext("tx", { request });
+          const hash = await startSpan({ name: "deploy account", op: "tx.send" }, () =>
+            keeper.writeContract(writeRequest),
+          );
+          setContext("tx", { hash, request });
+          await options?.onHash?.(hash);
+          const receipt = await startSpan({ name: "tx.wait", op: "tx.wait" }, () =>
+            publicClient.waitForTransactionReceipt({ hash }),
+          );
+          setContext("tx", { hash, request, receipt });
+          const trace = await traceClient.traceTransaction(hash);
+          setContext("tx", { hash, request, receipt, trace });
+          if (receipt.status !== "success") {
+            // eslint-disable-next-line @typescript-eslint/only-throw-error -- returns error
+            throw getContractError(new RawContractError({ data: trace.output }), { ...call, args: call.args ?? [] });
+          }
+          return receipt;
+        } catch (error: unknown) {
+          if (options?.ignore) {
+            const reason =
+              error instanceof BaseError &&
+              error.cause instanceof ContractFunctionRevertedError &&
+              error.cause.data?.errorName
+                ? `${error.cause.data.errorName}(${error.cause.data.args?.map(String).join(",") ?? ""})`
+                : error instanceof Error
+                  ? error.message
+                  : String(error);
+            if (typeof options.ignore === "function") {
+              const ignore = await options.ignore(reason);
+              if (ignore === true) return null;
+              if (ignore) return ignore;
+            } else if (options.ignore.includes(reason)) return null;
+          }
+          captureException(error, { level: "error" });
+          throw error;
+        }
+      }),
+  };
+}
