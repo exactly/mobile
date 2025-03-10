dc308f9cfee5ffd121adee252b5154dd0f1525db
guillermo dieguez <guillermodieguez@outlook.com>
2025-03-10 18:45:23 -0300

    ✨ app: make exa card pressable

--- src/components/card/exa-card/ExaCard.tsx
+++ src/components/card/exa-card/ExaCard.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { Pressable } from "react-native";
 import Animated from "react-native-reanimated";
 import { YStack } from "tamagui";
 
@@ -11,18 +12,26 @@ interface ExaCardProperties {
   disabled?: boolean;
   revealing: boolean;
   frozen: boolean;
+  onPress?: () => void;
 }
 
-export default function ExaCard({ disabled = false, revealing, frozen }: ExaCardProperties) {
+export default function ExaCard({ disabled = false, revealing, frozen, onPress }: ExaCardProperties) {
   const { data: card } = useQuery({ queryKey: ["card", "details"], queryFn: getCard });
-
   const isDebit = card?.mode === 0;
-
   return (
     <AnimatedYStack width="100%" borderRadius="$r4" borderWidth={0}>
-      <View zIndex={3} backgroundColor="black" borderColor="black" borderRadius="$r4" borderWidth={1} overflow="hidden">
-        <CardContents isCredit={!isDebit} disabled={disabled} frozen={frozen} revealing={revealing} />
-      </View>
+      <Pressable onPress={onPress}>
+        <View
+          zIndex={3}
+          backgroundColor="black"
+          borderColor="black"
+          borderRadius="$r4"
+          borderWidth={1}
+          overflow="hidden"
+        >
+          <CardContents isCredit={!isDebit} disabled={disabled} frozen={frozen} revealing={revealing} />
+        </View>
+      </Pressable>
     </AnimatedYStack>
   );
 }
