cafdc67e5945c8248b0027b55e6ef40be8239bda
guillermo dieguez <guillermodieguez@outlook.com>
2025-03-10 11:03:43 -0300

    💬 app: update payment activity literals

--- src/components/activity/ActivityItem.tsx
+++ src/components/activity/ActivityItem.tsx
@@ -51,7 +51,7 @@ export default function ActivityItem({ item, isLast }: { item: Item; isLast: boo
               {type === "card" && item.merchant.name}
               {type === "received" && "Received"}
               {type === "sent" && "Sent"}
-              {type === "repay" && "Debt Paid"}
+              {type === "repay" && "Protocol debt payment"}
               {type === "panda" && item.merchant.name}
             </Text>
             <Text caption color="$uiNeutralSecondary" numberOfLines={1}>
