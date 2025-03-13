b162106f0ca037bd878f8eeac0caa08c7fa088ce
guillermo dieguez <guillermodieguez@outlook.com>
2025-03-13 12:21:08 -0300

    ✨ app: support proposals on latest plugin repays

--- src/components/pay-later/TransactionDetails.tsx
+++ src/components/pay-later/TransactionDetails.tsx
@@ -1,17 +1,13 @@
 import chain from "@exactly/common/generated/chain";
-import shortenHex from "@exactly/common/shortenHex";
-import { SquareArrowOutUpRight } from "@tamagui/lucide-icons";
 import { format } from "date-fns";
-import { openBrowserAsync } from "expo-web-browser";
 import React from "react";
 import { ms } from "react-native-size-matters";
 import { Separator, XStack, YStack } from "tamagui";
 
 import OptimismImage from "../../assets/images/optimism.svg";
-import handleError from "../../utils/handleError";
 import Text from "../shared/Text";
 
-export default function TransactionDetails({ transactionHash }: { transactionHash: string }) {
+export default function TransactionDetails() {
   return (
     <YStack gap="$s4">
       <YStack gap="$s4">
@@ -55,23 +51,6 @@ export default function TransactionDetails({ transactionHash }: { transactionHas
           <Text callout color="$uiNeutralPrimary">
             {format(new Date(), "HH:mm:ss")}
           </Text>
-        </XStack>
-        <XStack justifyContent="space-between">
-          <Text emphasized footnote color="$uiNeutralSecondary">
-            Transaction hash
-          </Text>
-          <XStack
-            alignItems="center"
-            gap="$s3"
-            onPress={() => {
-              openBrowserAsync(`${chain.blockExplorers?.default.url}/tx/${transactionHash}`).catch(handleError);
-            }}
-          >
-            <Text textDecorationLine="underline" callout color="$uiNeutralPrimary">
-              {shortenHex(transactionHash)}
-            </Text>
-            <SquareArrowOutUpRight size={ms(20)} color="$uiNeutralSecondary" />
-          </XStack>
         </XStack>
       </YStack>
     </YStack>
