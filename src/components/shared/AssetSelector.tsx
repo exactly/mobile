8649b78ef660fed1d35db69c330bb7feee9a157c
guillermo dieguez <guillermodieguez@outlook.com>
2025-03-13 15:35:35 -0300

    ♻️ app: extract account assets

--- src/components/shared/AssetSelector.tsx
+++ src/components/shared/AssetSelector.tsx
@@ -1,7 +1,6 @@
-import chain, { previewerAddress } from "@exactly/common/generated/chain";
+import { previewerAddress } from "@exactly/common/generated/chain";
 import { Address } from "@exactly/common/validation";
 import { withdrawLimit } from "@exactly/lib";
-import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "moti/skeleton";
 import React, { useState } from "react";
 import { Appearance, Image } from "react-native";
@@ -9,41 +8,17 @@ import { ms, vs } from "react-native-size-matters";
 import { ToggleGroup, YStack } from "tamagui";
 import { safeParse } from "valibot";
 import { zeroAddress } from "viem";
-import { optimism } from "viem/chains";
 import { useAccount } from "wagmi";
 
 import AssetLogo from "./AssetLogo";
 import { useReadPreviewerExactly } from "../../generated/contracts";
 import assetLogos from "../../utils/assetLogos";
-import { getTokenBalances } from "../../utils/lifi";
+import useAccountAssets from "../../utils/useAccountAssets";
 import Text from "../shared/Text";
 import View from "../shared/View";
 
-export interface ProtocolAsset {
-  type: "protocol";
-  symbol: string;
-  assetName: string;
-  floatingDepositAssets: bigint;
-  decimals: number;
-  usdValue: number;
-  market: `0x${string}`;
-}
-
-export interface ExternalAsset {
-  type: "external";
-  name: string;
-  symbol: string;
-  logoURI?: string;
-  address: string;
-  amount?: bigint;
-  priceUSD: string;
-  decimals: number;
-  usdValue: number;
-}
-
 export default function AssetSelector({
   onSubmit,
-  useExternalAssets = false,
 }: {
   positions?: {
     symbol: string;
@@ -54,44 +29,14 @@ export default function AssetSelector({
     market: string;
   }[];
   onSubmit: (market: Address, isExternalAsset: boolean) => void;
-  useExternalAssets?: boolean;
 }) {
   const [selectedMarket, setSelectedMarket] = useState<Address | undefined>();
   const { address: account } = useAccount();
   const { data: markets } = useReadPreviewerExactly({ address: previewerAddress, args: [account ?? zeroAddress] });
 
-  const { data: externalAssets, isPending: isExternalAssetsPending } = useQuery({
-    queryKey: ["externalAssets", account],
-    queryFn: async () => {
-      if (chain.id !== optimism.id || !account) return [];
-      const balances = await getTokenBalances(account);
-      return balances.filter(
-        ({ address }) => markets && !markets.some(({ market }) => address.toLowerCase() === market.toLowerCase()),
-      );
-    },
-    enabled: !!account && useExternalAssets,
-  });
+  const { accountAssets, externalAssets, isPending: isAccountAssetsPending } = useAccountAssets();
 
-  const protocol = (markets ?? [])
-    .map((market) => ({
-      ...market,
-      symbol: market.symbol.slice(3) === "WETH" ? "ETH" : market.symbol.slice(3),
-      usdValue: markets
-        ? Number((withdrawLimit(markets, market.market) * market.usdPrice) / BigInt(10 ** market.decimals)) / 1e18
-        : 0,
-      type: "protocol",
-    }))
-    .filter(({ floatingDepositAssets }) => floatingDepositAssets > 0) as ProtocolAsset[];
-
-  const external = (externalAssets ?? []).map((externalAsset) => ({
-    ...externalAsset,
-    usdValue: (Number(externalAsset.priceUSD) * Number(externalAsset.amount ?? 0n)) / 10 ** externalAsset.decimals,
-    type: "external",
-  })) as ExternalAsset[];
-
-  const combinedAssets = [...protocol, ...external].sort((a, b) => Number(b.usdValue) - Number(a.usdValue));
-
-  if (combinedAssets.length === 0) {
+  if (accountAssets.length === 0) {
     return (
       <Text textAlign="center" emphasized footnote color="$uiNeutralSecondary">
         No available assets.
@@ -110,12 +55,12 @@ export default function AssetSelector({
           const market = safeParse(Address, value);
           if (!market.success) return;
           setSelectedMarket(market.output);
-          const isExternalAsset = useExternalAssets && externalAssets?.some(({ address }) => address === market.output);
-          onSubmit(market.output, isExternalAsset ?? false);
+          const isExternalAsset = externalAssets.some(({ address }) => address === market.output);
+          onSubmit(market.output, isExternalAsset);
         }}
         value={selectedMarket}
       >
-        {combinedAssets.map((item) => {
+        {accountAssets.map((item) => {
           if (item.type === "external") {
             const { name, symbol, logoURI, address, amount, priceUSD, decimals, usdValue } = item;
             return (
@@ -180,10 +125,16 @@ export default function AssetSelector({
                   borderRadius="$r3"
                 >
                   <View flexDirection="row" gap={ms(10)} alignItems="center" maxWidth="50%">
-                    <AssetLogo uri={assetLogos[symbol as keyof typeof assetLogos]} width={ms(32)} height={ms(32)} />
+                    <AssetLogo
+                      uri={
+                        assetLogos[symbol.slice(3) === "WETH" ? "ETH" : (symbol.slice(3) as keyof typeof assetLogos)]
+                      }
+                      width={ms(32)}
+                      height={ms(32)}
+                    />
                     <View gap="$s2" alignItems="flex-start" flexShrink={1}>
                       <Text fontSize={ms(15)} fontWeight="bold" color="$uiNeutralPrimary" numberOfLines={1}>
-                        {symbol}
+                        {symbol.slice(3) === "WETH" ? "ETH" : symbol.slice(3)}
                       </Text>
                       <Text fontSize={ms(12)} color="$uiNeutralSecondary" numberOfLines={1}>
                         {assetName === "Wrapped Ether" ? "Ether" : assetName}
@@ -211,7 +162,7 @@ export default function AssetSelector({
                           ),
                           useGrouping: false,
                         },
-                      )} ${symbol}`}
+                      )} ${symbol.slice(3) === "WETH" ? "ETH" : symbol.slice(3)}`}
                     </Text>
                   </View>
                 </View>
@@ -219,7 +170,7 @@ export default function AssetSelector({
             );
           }
         })}
-        {useExternalAssets && isExternalAssetsPending && (
+        {isAccountAssetsPending && (
           <View flexDirection="row" alignItems="center" width="100%">
             <Skeleton height={ms(50)} width="100%" colorMode={Appearance.getColorScheme() ?? "light"} />
           </View>
