c92ee3ccd9802acbd3a0e0b844aa2f379b4f43a1
guillermo dieguez <guillermodieguez@outlook.com>
2025-03-14 12:31:07 -0300

    🚧

--- src/utils/persona.ts
+++ src/utils/persona.ts
@@ -1,9 +1,10 @@
-import { upgradeableModularAccountAbi } from "@exactly/common/generated/chain";
+import { exaAccountFactoryAddress, upgradeableModularAccountAbi } from "@exactly/common/generated/chain";
 import latestExaPlugin from "@exactly/common/latestExaPlugin";
-import type { Passkey } from "@exactly/common/validation";
+import { Address, type Passkey } from "@exactly/common/validation";
 import { router } from "expo-router";
 import { Platform } from "react-native";
 import { Environment, Inquiry } from "react-native-persona";
+import { parse } from "valibot";
 import { zeroAddress } from "viem";
 
 import handleError from "./handleError";
@@ -13,20 +14,28 @@ import { getKYCLink } from "./server";
 import { accountClient } from "../utils/alchemyConnector";
 
 export const environment = __DEV__ ? Environment.SANDBOX : Environment.PRODUCTION;
+const PANDA_TEMPLATE = "itmpl_1igCJVqgf3xuzqKYD87HrSaDavU2";
+const CRYPTOMATE_TEMPLATE = "itmpl_8uim4FvD5P3kFpKHX37CW817";
 
 export async function getTemplateId() {
-  const [exaPlugin] = await publicClient.readContract({
-    address: accountClient?.account.address ?? zeroAddress,
-    abi: upgradeableModularAccountAbi,
-    functionName: "getInstalledPlugins",
-  });
-  return exaPlugin === latestExaPlugin ? "itmpl_1igCJVqgf3xuzqKYD87HrSaDavU2" : "itmpl_8uim4FvD5P3kFpKHX37CW817"; // cspell:disable-line
+  try {
+    const [exaPlugin] = await publicClient.readContract({
+      address: accountClient?.account.address ?? zeroAddress,
+      abi: upgradeableModularAccountAbi,
+      functionName: "getInstalledPlugins",
+    });
+    return exaPlugin === latestExaPlugin ? PANDA_TEMPLATE : CRYPTOMATE_TEMPLATE;
+  } catch {
+    return queryClient.getQueryData<Passkey>(["passkey"])?.factory === parse(Address, exaAccountFactoryAddress)
+      ? PANDA_TEMPLATE
+      : CRYPTOMATE_TEMPLATE;
+  }
 }
 
 export async function createInquiry(passkey: Passkey) {
   if (Platform.OS === "web") {
     const otl = await getKYCLink();
-    window.open(otl, "_blank");
+    window.open(otl);
     return;
   }
 
@@ -49,7 +58,7 @@ export async function createInquiry(passkey: Passkey) {
 export async function resumeInquiry(inquiryId: string, sessionToken: string) {
   if (Platform.OS === "web") {
     const otl = await getKYCLink();
-    window.open(otl, "_blank");
+    window.open(otl);
     return;
   }
 
