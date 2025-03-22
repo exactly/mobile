4390a4058c58f429eef294aa3f13d3ea8dbdedee
danilo neves cruz <cruzdanilo@gmail.com>
2025-03-22 15:45:40 -0300

    🧑‍💻 server: disable drizzle kit output on tests

--- server/test/mocks/database.ts
+++ server/test/mocks/database.ts
@@ -9,7 +9,10 @@ vi.mock("../../database", async () => {
   const { default: _, ...original } = await import("../../database");
   const { pushSchema } = require("drizzle-kit/api") as typeof DrizzleKit; // eslint-disable-line @typescript-eslint/no-require-imports, unicorn/prefer-module
   const database = drizzle(new PGlite(undefined, { debug: 0 }), { schema });
+  const stdoutWrite = process.stdout.write; // eslint-disable-line @typescript-eslint/unbound-method
+  process.stdout.write = () => true;
   const { apply } = await pushSchema(schema, database as unknown as PgliteDatabase);
   await apply();
+  process.stdout.write = stdoutWrite;
   return { ...original, default: database };
 });
