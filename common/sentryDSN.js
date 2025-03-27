fd1b56182344dba18438b91df9c14394d65595f3
danilo neves cruz <cruzdanilo@gmail.com>
2025-03-27 12:24:12 -0300

    🧑‍💻 common: allow empty sentry dsn

--- common/sentryDSN.js
+++ common/sentryDSN.js
@@ -3,7 +3,7 @@ const domain = require("@exactly/common/domain");
 const defaultDSN = "https://ac8875331e4cecd67dd0a7519a36dfeb@o1351734.ingest.us.sentry.io/4506186349674496";
 
 module.exports =
-  process.env.EXPO_PUBLIC_SENTRY_DSN || // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing -- ignore empty string
+  process.env.EXPO_PUBLIC_SENTRY_DSN ??
   {
     "web.exactly.app": defaultDSN,
     "sandbox.exactly.app": defaultDSN,
