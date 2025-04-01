9a96553f3dd7bd6778c26075f1cdc3106d257bd4
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-01 17:44:48 -0300

    📈 server: instrument public client http requests

--- server/utils/publicClient.ts
+++ server/utils/publicClient.ts
@@ -1,10 +1,41 @@
 import alchemyAPIKey from "@exactly/common/alchemyAPIKey";
 import chain from "@exactly/common/generated/chain";
+import { getActiveSpan } from "@sentry/node";
+import { inspect } from "node:util";
+import { array, object, optional, parse, string, unknown } from "valibot";
 import { createPublicClient, http } from "viem";
 
 if (!chain.rpcUrls.alchemy?.http[0]) throw new Error("missing alchemy rpc url");
 
 export default createPublicClient({
   chain,
-  transport: http(`${chain.rpcUrls.alchemy.http[0]}/${alchemyAPIKey}`, { batch: true }),
+  transport: http(`${chain.rpcUrls.alchemy.http[0]}/${alchemyAPIKey}`, {
+    batch: true,
+    async onFetchRequest(request) {
+      const span = getActiveSpan();
+      if (span) {
+        for (const [index, { method, params }] of parse(Requests, await request.json()).entries()) {
+          span.setAttribute(
+            `eth[${Date.now()}][${index}]`,
+            `${method}(${
+              params
+                ?.map((p) =>
+                  inspect(p, {
+                    colors: false,
+                    breakLength: Infinity,
+                    numericSeparator: true,
+                    maxStringLength: null,
+                    maxArrayLength: null,
+                    depth: null,
+                  }),
+                )
+                .join(", ") ?? ""
+            })`,
+          );
+        }
+      }
+    },
+  }),
 });
+
+const Requests = array(object({ method: string(), params: optional(array(unknown())) }));
