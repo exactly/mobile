1058a840764626a111c3003692bbe16ecd4965f3
guillermo dieguez <guillermodieguez@outlook.com>
2025-04-08 17:27:22 -0300

    💬 app: rename op mainnet chain name

---
+++ .changeset/cruel-bees-fold.md
@@ -0,0 +1,5 @@
+---
+"@exactly/mobile": patch
+---
+
+💬 rename op mainnet chain name
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./utils/polyfill";
 import "expo-router/entry";
+
+import chain from "@exactly/common/generated/chain";
+
+if (chain.id === 10) chain.name = `Optimism (${chain.name})`;
