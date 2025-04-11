5d38ef48f6047edbfd4ff3484397217353a898de
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-11 01:56:11 -0300

    🚧

--- server/test/utils/keeper.test.ts
+++ server/test/utils/keeper.test.ts
@@ -1,75 +1,26 @@
 import "../mocks/sentry";
 import "../mocks/deployments";
+import { keeperClient } from "../mocks/keeper"; // eslint-disable-line import/order -- must be imported early
 
-import { erc20Abi, padHex, WaitForTransactionReceiptTimeoutError, zeroHash } from "viem";
-import { privateKeyToAddress } from "viem/accounts";
-import * as actions from "viem/actions";
-import { beforeAll, describe, expect, inject, it, vi } from "vitest";
+import { describe, expect, inject, it, vi } from "vitest";
 
-import deriveAddress from "../../utils/deriveAddress";
+import { auditorAbi } from "../../generated/contracts";
 import keeper from "../../utils/keeper";
-import publicClient from "../../utils/publicClient";
 
-describe("with funds", () => {
-  const bob = privateKeyToAddress(padHex("0xb0b"));
-  const account = deriveAddress(inject("ExaAccountFactory"), { x: padHex(bob), y: zeroHash });
-
-  beforeAll(async () => {
-    await keeper.writeContract({
-      address: inject("USDC"),
-      abi: mockERC20Abi,
-      functionName: "mint",
-      args: [keeper.account.address, 400e6],
-    });
-  });
-
-  it("executes a contract function", async () => {
-    const onHash = vi.fn();
-    const receipt = await keeper.exaSend(
-      { name: "test transfer", op: "test.transfer", attributes: { account } },
-      {
-        address: inject("USDC"),
-        abi: [...erc20Abi],
-        functionName: "transfer",
-        args: [account, 100e6],
-      },
-      { onHash },
-    );
-
-    expect(onHash).toHaveBeenCalledOnce();
-    expect(receipt?.status).toBe("success");
-  });
-
-  it("executes a contract function after a timeout", async () => {
-    // vi.spyOn(actions, "sendRawTransaction").mockResolvedValue("0x0");
-    vi.spyOn(publicClient, "waitForTransactionReceipt").mockRejectedValueOnce(
-      new WaitForTransactionReceiptTimeoutError({ hash: "0x0" }),
-    );
-    const onHash = vi.fn();
+describe("fault tolerance", () => {
+  it("recovers if transaction is missing", async () => {
+    const sendRawTransaction = vi.spyOn(keeperClient, "sendRawTransaction");
+    sendRawTransaction.mockResolvedValueOnce("0x");
+    const onHash = vi.fn<() => void>();
     const k = keeper;
     const receipt = await k.exaSend(
-      { name: "test transfer", op: "test.transfer", attributes: { account } },
-      {
-        address: inject("USDC"),
-        abi: [...erc20Abi],
-        functionName: "transfer",
-        args: [account, 100e6],
-      },
+      { name: "test transfer", op: "test.transfer" },
+      { address: inject("Auditor"), abi: auditorAbi, functionName: "enterMarket", args: [inject("MarketUSDC")] },
       { onHash },
     );
 
     expect(onHash).toHaveBeenCalledOnce();
     expect(receipt?.status).toBe("success");
-    //expect(sendRawTransaction).toHaveBeenCalledTimes(2);
+    expect(sendRawTransaction).toHaveBeenCalledTimes(2);
   });
 });
-
-const mockERC20Abi = [
-  {
-    type: "function",
-    name: "mint",
-    inputs: [{ type: "address" }, { type: "uint256" }],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-];
--- server/utils/keeper.ts
+++ server/utils/keeper.ts
@@ -2,6 +2,7 @@ import alchemyAPIKey from "@exactly/common/alchemyAPIKey";
 import chain from "@exactly/common/generated/chain";
 import { Hash } from "@exactly/common/validation";
 import { captureException, startSpan, withScope } from "@sentry/node";
+import { setTimeout } from "node:timers/promises";
 import { parse } from "valibot";
 import {
   BaseError,
@@ -10,10 +11,12 @@ import {
   encodeFunctionData,
   getContractError,
   http,
+  keccak256,
   nonceManager,
   RawContractError,
   WaitForTransactionReceiptTimeoutError,
   withRetry,
+  type MaybePromise,
   type Prettify,
   type TransactionReceipt,
   type WriteContractParameters,
@@ -42,13 +45,14 @@ export function extender(keeper: ReturnType<typeof createWalletClient>) {
       spanOptions: Prettify<{ name: string; op: string } & Omit<Parameters<typeof startSpan>[0], "name" | "op">>,
       call: Prettify<Pick<WriteContractParameters, "address" | "functionName" | "args" | "abi">>,
       options?: {
-        onHash?: (hash: Hash) => Promise<unknown>;
+        onHash?: (hash: Hash) => MaybePromise<unknown>;
         ignore?: string[] | ((reason: string) => Promise<TransactionReceipt | boolean | undefined>);
       },
     ) =>
       withScope((scope) =>
         startSpan({ forceTransaction: true, ...spanOptions }, async (span) => {
           try {
+            scope.setContext("tx", { call });
             span.setAttributes({
               "tx.call": `${call.functionName}(${call.args?.map(String).join(", ") ?? ""})`,
               "tx.from": keeper.account?.address,
@@ -77,36 +81,51 @@ export function extender(keeper: ReturnType<typeof createWalletClient>) {
 
             const receipt = await withRetry(
               async () => {
-                const prepare = await startSpan({ name: "prepare transaction", op: "tx.prepare" }, () =>
+                const prepared = await startSpan({ name: "prepare transaction", op: "tx.prepare" }, () =>
                   keeper.prepareTransactionRequest({
                     account: keeper.account,
                     chain,
                     to: call.address,
-                    data: encodeFunctionData({ abi: call.abi, args: call.args, functionName: call.functionName }),
+                    data: encodeFunctionData(call),
                     ...txOptions,
                     nonceManager,
                   }),
                 );
-                scope.setContext("tx", { request, prepare });
+                scope.setContext("tx", { request, prepared });
+                span.setAttribute("tx.nonce", prepared.nonce);
                 const serializedTransaction = await startSpan({ name: "sign transaction", op: "tx.sign" }, () =>
-                  keeper.signTransaction(prepare),
+                  keeper.signTransaction(prepared),
                 );
-                scope.setContext("tx", { request, prepare, serializedTransaction });
+                const hash = keccak256(serializedTransaction);
+                scope.setContext("tx", { request, prepared, hash });
+                span.setAttribute("tx.hash", hash);
                 try {
-                  return await withRetry(
-                    async () => {
-                      const hash = await startSpan({ name: "send transaction", op: "tx.send" }, () =>
-                        keeper.sendRawTransaction({ serializedTransaction }),
-                      );
-
-                      span.setAttribute("tx.hash", hash);
-                      scope.setContext("tx", { hash, request, prepare, serializedTransaction });
-                      return await startSpan({ name: "wait for receipt", op: "tx.wait" }, () =>
-                        publicClient.waitForTransactionReceipt({ hash }),
-                      );
-                    },
-                    { shouldRetry: ({ error }) => error instanceof WaitForTransactionReceiptTimeoutError },
-                  );
+                  const controller = new AbortController();
+                  const [txReceipt] = await Promise.all([
+                    startSpan({ name: "wait for receipt", op: "tx.wait" }, () =>
+                      publicClient.waitForTransactionReceipt({ hash, confirmations: 0 }),
+                    ).finally(() => {
+                      controller.abort();
+                    }),
+                    (async () => {
+                      while (!controller.signal.aborted) {
+                        await Promise.allSettled([
+                          startSpan({ name: "send transaction", op: "tx.send" }, () =>
+                            keeper.sendRawTransaction({ serializedTransaction }),
+                          ),
+                          setTimeout(6666, null, { signal: controller.signal }),
+                        ]);
+                      }
+                    })(),
+                    (async () => {
+                      try {
+                        await options?.onHash?.(hash);
+                      } catch (error) {
+                        captureException(error, { level: "error" });
+                      }
+                    })(),
+                  ]);
+                  return txReceipt;
                 } catch (error) {
                   if (error instanceof WaitForTransactionReceiptTimeoutError && keeper.account) {
                     captureException(new Error("bad nonce"), { level: "fatal" });
@@ -118,12 +137,10 @@ export function extender(keeper: ReturnType<typeof createWalletClient>) {
               { retryCount: 1, shouldRetry: ({ error }) => error instanceof WaitForTransactionReceiptTimeoutError },
             );
 
-            const hash = receipt.transactionHash;
-            await options?.onHash?.(hash);
             span.setStatus({ code: receipt.status === "success" ? 1 : 2, message: receipt.status });
-            scope.setContext("tx", { hash, request, receipt });
-            const trace = await traceClient.traceTransaction(hash);
-            scope.setContext("tx", { hash, request, receipt, trace });
+            scope.setContext("tx", { request, receipt });
+            const trace = await traceClient.traceTransaction(receipt.transactionHash);
+            scope.setContext("tx", { request, receipt, trace });
             if (receipt.status !== "success") {
               // eslint-disable-next-line @typescript-eslint/only-throw-error -- returns error
               throw getContractError(new RawContractError({ data: trace.output }), { ...call, args: call.args ?? [] });
