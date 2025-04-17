700352c0e5bcf7178add69922439c32e06e9d34c
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-17 14:12:29 -0300

    🧑‍💻 server: increase spotlight kill timeout

--- server/test/spotlight.ts
+++ server/test/spotlight.ts
@@ -6,7 +6,7 @@ const cwd = "node_modules/@exactly/.spotlight";
 export default async function setup() {
   await rm(cwd, { recursive: true, force: true });
   await mkdir(cwd, { recursive: true });
-  const subprocess = $({ cwd, env: { SPOTLIGHT_CAPTURE: "1" } })`spotlight`;
+  const subprocess = $({ cwd, env: { SPOTLIGHT_CAPTURE: "1" }, forceKillAfterDelay: 33_333 })`spotlight`;
 
   return function teardown() {
     subprocess.kill();
