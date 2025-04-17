117178d26093ae46fb98f64d9b707aac396c3b55
danilo neves cruz <cruzdanilo@gmail.com>
2025-04-17 12:20:33 -0300

    🔧 fingerprint: ignore config and script changes

--- fingerprint.config.cjs
+++ fingerprint.config.cjs
@@ -1,14 +1,5 @@
 /** @type {import('@expo/fingerprint').Config} */
 module.exports = {
-  sourceSkips: [
-    "ExpoConfigAndroidPackage",
-    "ExpoConfigAssets",
-    "ExpoConfigIosBundleIdentifier",
-    "ExpoConfigNames",
-    "ExpoConfigRuntimeVersionIfString",
-    "ExpoConfigSchemes",
-    "ExpoConfigVersions",
-    "PackageJsonAndroidAndIosScriptsIfNotContainRun",
-  ],
-  ignorePaths: [".gitignore", "package.json", "patches/eslint-*", "src/generated/versionCode.js"],
+  sourceSkips: ["ExpoConfigAll", "GitIgnore", "PackageJsonScriptsAll"],
+  ignorePaths: ["package.json", "patches/eslint-*", "src/generated/versionCode.js"],
 };
